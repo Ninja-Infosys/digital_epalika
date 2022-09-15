488d4c4d547c30689ce4fa017555380672290574
manoj <manojoli667@gmail.com>
2022-09-15 11:10:13 +0545

    updates

--- Modules/Circular/Http/Controllers/RegistrationController.php
+++ Modules/Circular/Http/Controllers/RegistrationController.php
@@ -5,12 +5,18 @@ namespace Modules\Circular\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Gate;
 use Modules\Circular\Entities\Registration;
 
 class RegistrationController extends Controller
 {
     public function index()
     {
+        abort_if(Gate::denies('registration_access'),
+            403,
+            'You are not allowed to registration access'
+        );
+
         $registrations = Registration::latest()->get();
 
         return view('circular::registration.index', compact('registrations'));
@@ -18,31 +24,55 @@ class RegistrationController extends Controller
 
     public function create()
     {
+        abort_if(Gate::denies('registration_create'),
+            403,
+            'You are not allowed to registration create'
+        );
+
         return view('circular::registration.create');
     }
 
     public function store(Request $request)
     {
-        //
+        abort_if(Gate::denies('registration_create'),
+            403,
+            'You are not allowed to registration create'
+        );
     }
 
     public function show(Registration $registration)
     {
+        abort_if(Gate::denies('registration_access'),
+            403,
+            'You are not allowed to registration access'
+        );
+
         return view('circular::registration.show');
     }
 
     public function edit(Registration $registration)
     {
+        abort_if(Gate::denies('registration_edit'),
+            403,
+            'You are not allowed to registration edit'
+        );
+
         return view('circular::registration.edit');
     }
 
     public function update(Request $request, Registration $registration)
     {
-        //
+        abort_if(Gate::denies('registration_edit'),
+            403,
+            'You are not allowed to registration edit'
+        );
     }
 
     public function destroy(Registration $registration)
     {
-        //
+        abort_if(Gate::denies('registration_delete'),
+            403,
+            'You are not allowed to registration delete'
+        );
     }
 }
