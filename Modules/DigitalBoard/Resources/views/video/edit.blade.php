da8b96a5e8b26b6116f754d55c1e81b599d00b5b
Yekdev Rokaya <yekdevrc@gmail.com>
2022-09-15 17:06:45 +0545

    update

---
+++ Modules/DigitalBoard/Resources/views/video/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item">
+                            <a href="{{route('admin.dashboard')}}">
+                                <i class="fa fa-home"></i> गृहपृष्ठ
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="{{route('admin.digitalBoard.video.index')}}">डिजिटल बोर्ड</a>
+                        </li>
+                        <li class="breadcrumb-item active">भिडियो सम्पादन गर्नुहोस्</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">भिडियो</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <h4 class="header-title">भिडियो सम्पादन गर्नुहोस्</h4>
+                        <a href="{{route('admin.digitalBoard.video.index')}}" class="btn btn-sm btn-outline-primary">
+                            <i class="fa fa-list"></i> सुची
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('admin.digitalBoard.video.update', $video)}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('put')
+                        <div class="row">
+                            <div class="col-md-6 mb-2">
+                                <label for="title" class="form-label">भिडियो शिर्षक </label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    value="{{old('title', $video->title)}}"
+                                    class="form-control @error('title') is-invalid @enderror"
+                                    id="title"
+                                    placeholder="Video Title"
+                                />
+                                @error('title')
+                                <div class="invalid-feedback">{{$message}}</div>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-2">
+                                <label for="video" class="form-label">भिडियो  *</label>
+                                <input
+                                    type="file"
+                                    name="video"
+                                    class="form-control @error('video') is-invalid @enderror"
+                                    accept="video/*"
+                                    id="video"
+                                />
+                                @error('video')
+                                <div class="invalid-feedback">{{$message}}</div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">
+                            Update
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
