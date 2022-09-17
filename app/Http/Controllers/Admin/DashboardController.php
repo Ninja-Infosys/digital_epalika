27a4e7d1b59477d0be58862108d7049535770316
ShiShiR RaI <rphantom05@gmail.com>
2022-09-17 23:15:47 +0545

    Menus command and auto update file

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Nwidart\Modules\Facades\Module;
 
 class DashboardController extends Controller
 {
