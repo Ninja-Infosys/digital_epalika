2198c085bd4635979d817a4e6f0001b767c93a4f
manoj <manojoli667@gmail.com>
2022-09-22 18:38:11 +0545

    updates

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
           integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
           crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @livewireStyles
 </head>
 <body>
 @include('frontend.partials.header')
@@ -38,4 +39,5 @@
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 @stack('styles')
+@livewireScripts
 </html>
