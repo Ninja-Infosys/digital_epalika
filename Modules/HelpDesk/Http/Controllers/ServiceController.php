3037352de12177dbcf581710bc1fb43692d30b72
Yekdev Rokaya <yekdevrc@gmail.com>
2022-09-22 11:20:23 +0545

    update

--- Modules/HelpDesk/Http/Controllers/ServiceController.php
+++ Modules/HelpDesk/Http/Controllers/ServiceController.php
@@ -59,7 +59,7 @@ class ServiceController extends Controller
             }
         });
 
-        toast('Service Created Successfully', 'success');
+        toast('सेवा सफलतापूर्वक सिर्जना गरियो', 'success');
 
         return back();
     }
