ca190a026c5bdc6ae98e9b6cac25f40cb5ec93db
Ninja <bijaygamuwatharu@gmail.com>
2022-09-22 18:37:52 +0545

    help-desk: complete

--- Modules/DigitalBoard/Resources/views/frontend/services/service.blade.php
+++ Modules/DigitalBoard/Resources/views/frontend/services/service.blade.php
@@ -1,4 +1,4 @@
-@extends('frontend.layouts.master')
+@extends('digitalboard::layouts.master')
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
---
+++ Modules/HelpDesk/Http/Controllers/FrontController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\HelpDesk\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\HelpDesk\Entities\Service;
+
+class FrontController extends Controller
+{
+    public function showServiceDetail(Service $service)
+    {
+        return view('digitalboard::frontend.services.details', compact('service'));
+    }
+}
