2d601a979879ea695be6f86c1055c1e260040f9f
Yekdev Rokaya <yekdevrc@gmail.com>
2022-09-22 12:42:28 +0545

    update

--- Modules/HelpDesk/Http/Requests/Service/UpdateServiceRequest.php
+++ Modules/HelpDesk/Http/Requests/Service/UpdateServiceRequest.php
@@ -32,4 +32,25 @@ class UpdateServiceRequest extends FormRequest
             'serviceEmployees.*.employee' => ['required']
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'branch_id' => 'शाखा आवश्यक छ',
+            'service_name.required' => 'सेवाको नाम आवश्यक छ',
+            'time_taken.required' => 'लाग्ने समय आबश्यक छ ',
+            'responsible_officer.required' => 'जिम्मेवार कर्मचारी आबश्यक छ ',
+            'office.required' => 'कोठा नम्बर/कार्यालय आबश्यक छ ',
+            'photo.image' => 'फोटो हुनुपर्छ ',
+            'email.required' => 'इमेल अनिबार्य छ ',
+            'email.email' => 'इमेल फर्ममा हुनुपर्छ ',
+            'phone.required' => 'फोन नम्बर अनिबार्य छ ',
+            'serviceDocuments.required' => 'आबश्यक कागजात अनिबार्य छ ',
+            'serviceDocuments.*.description.required' => 'कागजात आबश्यक छ ',
+            'serviceProcesses.required' => 'उपलब्ध गराउने प्रक्रिया अनिबार्य छ ',
+            'serviceProcesses.*.description.required' => 'उपलब्ध गराउने प्रक्रिया आबश्यक छ ',
+            'serviceEmployees.required' => 'सेवा दिने कर्मचारीहरुको नाम अनिबार्य छ ',
+            'serviceEmployees.*.employee' => 'कर्मचारी आबश्यक छ '
+        ];
+    }
 }
