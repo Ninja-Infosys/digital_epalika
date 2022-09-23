c31f49f00436e07d6c7217d3e48623d7ba062869
ShiShiR RaI <rphantom05@gmail.com>
2022-09-23 15:40:15 +0545

    Registration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 use Modules\EMap\Http\Controllers\OrganizationAuthController;
 
@@ -17,41 +18,54 @@ use Modules\EMap\Http\Controllers\OrganizationAuthController;
 //Route::get('/', function () {
 //    return redirect(route('admin.dashboard'));
 //});
-Route::get('/', function () {
-    return view('frontend.home');
-});
-Route::get('/static/category', function(){
+Route::get('/', [FrontController::class, 'index'])->name('welcome');
+Route::get('/static/category', function () {
     return view('frontend.static.category.category');
 });
-Route::get('/static/category', function(){
+Route::get('/static/category', function () {
     return view('frontend.static.category.index');
 });
-Route::get('/static/contact', function(){
+Route::get('/static/contact', function () {
     return view('frontend.static.contact.index');
 });
-Route::get('/static/representative', function(){
+Route::get('/static/representative', function () {
+    return view('frontend.static.representive.representive');
+});
+Route::get('/static/gallery/audio', function () {
+    return view('frontend.static.gallery.audio.index');
+});
+Route::get('/static/gallery/photo', function () {
+    return view('frontend.static.gallery.photo.index');
+});
+Route::get('/static/gallery/photo', function () {
+    return view('frontend.static.gallery.photo.single-photo');
+});
+Route::get('/static/gallery/video', function () {
+    return view('frontend.static.gallery.video.index');
+});
+Route::get('/static/representative', function () {
     return view('frontend.static.representative.index');
 });
-Route::get('/static/employee', function(){
+Route::get('/static/employee', function () {
     return view('frontend.static.employee.index');
 });
 
-Route::get('/static/gallery/photo', function(){
+Route::get('/static/photo', function () {
     return view('frontend.static.gallery.photo.photo');
 });
-Route::get('/static/gallery/audio', function(){
+Route::get('/static/audio', function () {
     return view('frontend.static.gallery.audio.audio');
 });
-Route::get('/static/gallery/video', function(){
+Route::get('/static/video', function () {
     return view('frontend.static.gallery.video.video');
 });
-Route::get('/static/employee', function(){
+Route::get('/static/employee', function () {
     return view('frontend.static.employee.employee');
 });
-Route::get('/static/representive', function(){
+Route::get('/static/representive', function () {
     return view('frontend.static.representive.representive');
 });
-Route::get('/static/gallery/photo/single-photo', function (){
+Route::get('/static/gallery/photo/single-photo', function () {
     return view('frontend.static.gallery.photo.single-photo.single-photo');
 });
 
