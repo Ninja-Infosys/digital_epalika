aaa097f54b4e64dae3c4f5554c98cb6ee8bd512c
manoj <manojoli667@gmail.com>
2022-09-23 17:09:11 +0545

    updates

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Frontend\BaseController;
 use App\Models\ExecutiveMeeting\MeetingDetail;
 use App\Models\ExecutiveMeeting\MunicipalMeetingDecision;
 use Illuminate\Http\Request;
 use Modules\DigitalBoard\Entities\Employee;
 use Modules\DigitalBoard\Entities\Notice;
 
-class FrontController extends BaseController
+class FrontController extends Controller
 {
     public function index()
     {
@@ -21,7 +20,7 @@ class FrontController extends BaseController
         $news = $mixedNotices->where('type', 'News')->limit(3)->get();
 
         $meetingDetails = MunicipalMeetingDecision::with('meetingDetail')->whereHas('meetingDetail', function ($query) {
-            $query->orderByDesc('meeting_date');
+            $query->orderBy('meeting_date');
         })->get();
 
         return view('frontend.home', compact('employees', 'notices', 'news', 'meetingDetails'));
