98844e132a85f52e66c89050762c80af840b4071
ShiShiR RaI <rphantom05@gmail.com>
2022-09-23 18:30:00 +0545

    Registration

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,7 +10,7 @@
     </div>
 
     <div class="row">
-        <div class="col-md-6 col-xl-3">
+        <div class="col-md-6 col-xl-2">
             <div class="widget-rounded-circle card">
                 <div class="card-body">
                     <div class="row">
@@ -20,7 +20,7 @@
                         <div class="col-6">
                             <div class="text-end">
                                 <h3 class="text-dark mt-1">
-                                    1
+                                    {{$user_count}}
                                 </h3>
                                 <p class="text-muted mb-1 text-truncate">
                                     प्रयोगकर्ताहरु
@@ -28,12 +28,205 @@
                             </div>
                         </div>
                     </div>
-{{--                    <x-date-component--}}
-{{--                        :data="[ 'name_ne'=>'xyz','label_ne'=>'xyz', 'name_en'=>'abc', 'label_en'=>'abc']"/>--}}
-{{--                    <x-date-component--}}
-{{--                        :data="[ 'name_ne'=>'sad','label_ne'=>'asdsa', 'name_en'=>'asfew', 'label_en'=>'ascassa']"/>--}}
 
-                    @livewire('date-livewire')
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-user fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$grievance_user_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    गुनासो गर्ने प्रयोगकर्ताहरु
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-clipboard fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$notice_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    सूचना
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-newspaper fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$news_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    समाचार
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-users fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$registration_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    प्रयोगकर्ताहरु
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-user fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$dispatch_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    गुनासो गर्ने प्रयोगकर्ताहरु
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-clipboard fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$unseen_grievance_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    सूचना
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-newspaper fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$replied_grievance_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    समाचार
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-clipboard fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$investigated_grievance_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    सूचना
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <i class="fa fa-newspaper fa-4x"></i>
+                        </div>
+                        <div class="col-6">
+                            <div class="text-end">
+                                <h3 class="text-dark mt-1">
+                                    {{$closed_grievance_count}}
+                                </h3>
+                                <p class="text-muted mb-1 text-truncate">
+                                    समाचार
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
