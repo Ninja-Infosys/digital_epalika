e93d45d296cff5e7317b7f5f41172dfdbdf43cd1
Tekchan Khatri <tekam7755@gmail.com>
2022-09-25 15:54:47 +0545

    businessRegistration

--- Modules/BusinessRegistration/Database/Migrations/2022_09_25_100559_create_registered_businesses_table.php
+++ Modules/BusinessRegistration/Database/Migrations/2022_09_25_100559_create_registered_businesses_table.php
@@ -10,6 +10,11 @@ return new class extends Migration
     {
         Schema::create('registered_businesses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('business_detail_id')->nullable()->constrained()->nullOnDelete()->onUpdate('no action');
+            $table->string('registration_no')->nullable();
+            $table->string('business_name')->nullable();
+            $table->string('registration_date')->nullable();
+            $table->string('active')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
