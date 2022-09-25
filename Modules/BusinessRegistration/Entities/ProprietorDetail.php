c608b0b45768933710970c3cefc0b66b3a3ca89f
Tekchan Khatri <tekam7755@gmail.com>
2022-09-25 14:30:07 +0545

    businessRegistration

---
+++ Modules/BusinessRegistration/Entities/ProprietorDetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\BusinessRegistration\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProprietorDetail extends Model
+{
+    use HasFactory,SoftDeletes;
+
+   protected $dates = [
+       'created_at',
+       'updated_at',
+       'deleted_at'
+   ];
+
+   protected $fillable = [
+
+   ];
+}
