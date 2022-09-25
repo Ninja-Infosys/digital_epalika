a2c4510904623b53de5519167deeffaab9d55aa4
Tekchan Khatri <tekam7755@gmail.com>
2022-09-25 16:52:43 +0545

    businessRegistration

--- Modules/BusinessRegistration/Http/Livewire/RegistrationForm.php
+++ Modules/BusinessRegistration/Http/Livewire/RegistrationForm.php
@@ -2,10 +2,21 @@
 
 namespace Modules\BusinessRegistration\Http\Livewire;
 
+use App\Models\Address\Province;
 use Livewire\Component;
 
 class RegistrationForm extends Component
 {
+
+    public $provices = [];
+    public $districts = [];
+    public $localBodies = [];
+
+
+    public function mount()
+    {
+        $this->provices = Province::all();
+    }
     public function render()
     {
         return view('businessregistration::livewire.registration-form');
