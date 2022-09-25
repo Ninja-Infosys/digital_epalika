bd4a1c0f2f5bd42850276369c00114102987be5c
Tekchan Khatri <tekam7755@gmail.com>
2022-09-25 14:03:45 +0545

    businessRegistration

--- Modules/BusinessRegistration/Http/Controllers/Frontend/FrontendController.php
+++ Modules/BusinessRegistration/Http/Controllers/Frontend/FrontendController.php
@@ -8,40 +8,6 @@ use App\Http\Controllers\Controller;
 
 class FrontendController extends Controller
 {
-    public function index()
-    {
-        return view('businessregistration::index');
-    }
-
-    public function create()
-    {
-        return view('businessregistration::create');
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show($id)
-    {
-        return view('businessregistration::show');
-    }
-
-    public function edit($id)
-    {
-        return view('businessregistration::edit');
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
-    }
 
     public function businessRegistration()
     {
