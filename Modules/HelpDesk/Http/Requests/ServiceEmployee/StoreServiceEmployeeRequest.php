9852b6b715b6c32bcfa8ec473b23a9dca6de0072
Yekdev Rokaya <yekdevrc@gmail.com>
2022-09-25 11:17:56 +0545

    update

--- Modules/HelpDesk/Http/Requests/ServiceEmployee/StoreServiceEmployeeRequest.php
+++ Modules/HelpDesk/Http/Requests/ServiceEmployee/StoreServiceEmployeeRequest.php
@@ -22,4 +22,15 @@ class StoreServiceEmployeeRequest extends FormRequest
             'position' => ['nullable', 'integer'],
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'employee_name.required' => 'कर्मचारी नाम आवश्यक छ',
+            'photo.image' => 'फोटो फर्ममा हुनुपर्छ ',
+            'email.email' => 'इमेल फर्ममा हुनुपर्छ ',
+            'designation.required' => 'पद आबश्यक छ ',
+            'position.integer' => 'स्थिति अंकमा हुनुपर्छ '
+        ];
+    }
 }
