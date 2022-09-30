70b5fa21c2097ad01341f590e676fb940bef161c
Ninja <bijaygamuwatharu@gmail.com>
2022-09-30 13:46:21 +0545

    front end UI completed

--- routes/web.php
+++ routes/web.php
@@ -19,70 +19,24 @@ use Modules\EMap\Http\Controllers\OrganizationAuthController;
 //    return redirect(route('admin.dashboard'));
 //});
 Route::get('/', [FrontController::class, 'index'])->name('welcome');
-Route::get('/static/category', function () {
-    return view('frontend.static.category.category');
-});
-Route::get('/static/category', function () {
-    return view('frontend.static.category.index');
-});
+Route::get('introduction', [FrontController::class, 'introduction'])->name('introduction');
+Route::get('category', [FrontController::class, 'category'])->name('category');
 Route::get('contact', [FrontController::class, 'contact'])->name('contact');
-Route::get('/static/representative', function () {
-    return view('frontend.static.representive.representive');
-});
-Route::get('/static/gallery/audio', function () {
-    return view('frontend.static.gallery.audio.index');
-});
-Route::get('/static/gallery/photo', function () {
-    return view('frontend.static.gallery.photo.index');
-});
-Route::get('/static/gallery/photo', function () {
-    return view('frontend.static.gallery.photo.single-photo');
-});
-Route::get('/static/gallery/video', function () {
-    return view('frontend.static.gallery.video.index');
-});
-Route::get('/static/representative', function () {
-    return view('frontend.static.representative.index');
-});
-Route::get('/static/employee', function () {
-    return view('frontend.static.employee.index');
-});
+Route::get('representative', [FrontController::class, 'representative'])->name('representative');
+Route::get('audio', [FrontController::class, 'audio'])->name('audio');
+Route::get('photo', [FrontController::class, 'photo'])->name('photo');
+Route::get('single-photo', [FrontController::class, 'single_photo'])->name('single-photo');
+Route::get('video', [FrontController::class, 'video'])->name('video');
+Route::get('employee', [FrontController::class, 'employee'])->name('employee');
+Route::get('executive', [FrontController::class, 'executive'])->name('executive');
+Route::get('single-executive', [FrontController::class, 'single_executive'])->name('single-executive');
+Route::get('service-details', [FrontController::class, 'service_details'])->name('service-details');
 
-Route::get('/static/photo', function () {
-    return view('frontend.static.gallery.photo.photo');
-});
-Route::get('/static/audio', function () {
-    return view('frontend.static.gallery.audio.audio');
-});
-Route::get('/static/video', function () {
-    return view('frontend.static.gallery.video.video');
-});
-Route::get('/static/employee', function () {
-    return view('frontend.static.employee.employee');
-});
-Route::get('/static/representive', function () {
-    return view('frontend.static.representive.representive');
-});
-Route::get('/static/gallery/photo/single-photo', function () {
-    return view('frontend.static.gallery.photo.single-photo.single-photo');
-});
+
 Route::get('/static/notice', [FrontController::class, 'notice'])->name('notice');
 Route::get('/static/single-notice/{notice}', [FrontController::class, 'singleNotice'])->name('single-notice');
-Route::get('/static/executive', function () {
-    return view('frontend.static.executive-board.index');
-});
-Route::get('/static/single-executive', function () {
-    return view('frontend.static.executive-board.single-executive-board');
-});
-Route::get('/popup', function () {
-    return view('frontend.static.chat.popup');
-});
-Route::get('/chat', function () {
-    return view('frontend.static.chat.chat');
-});
-Route::get('/service-details', function () {
-    return view('frontend.static.chat.service');
-});
+
+
 
 
 
