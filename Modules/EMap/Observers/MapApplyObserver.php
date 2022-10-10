9beac12f1212c18044a166291760e4ae5e03e4c3
manoj <manojoli667@gmail.com>
2022-10-10 10:43:28 +0545

    map apply

--- Modules/EMap/Observers/MapApplyObserver.php
+++ Modules/EMap/Observers/MapApplyObserver.php
@@ -10,7 +10,7 @@ class MapApplyObserver
     public function creating(MapApply $mapApply)
     {
         $mapApply->organization_id = auth('organization')->user()->id;
-        $mapApply->registration_no = ($mapApply->fiscalYear->title ?? '') . '/' . time();
+        $mapApply->registration_no = ($officeSetting->fiscalYear->title ?? '') . '/' . time();
         $mapApply->registration_date = now();
     }
 }
