3997e40d0e475e48360af7b6139805ad876c963b
Ninja <bijaygamuwatharu@gmail.com>
2022-10-11 10:10:51 +0545

    application pages

---
+++ Modules/EMap/Resources/views/organization/clients/map/application/map_acceptance.blade.php
@@ -0,0 +1,17 @@
+@extends('emap::organization.layouts.master')
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            <div class="card mb_30">
+                <div class="card-header p-3">
+                    <div class="main-title d-flex justify-content-between">
+                        <h3 class="mb-0">सेवाग्राही:fghfg</h3>
+                        <a href="" class="btn btn-primary btn-sm">
+                            <i class="fa fa-list"></i> सेवाग्राही सुची
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- Modules/EMap/Routes/web.php
+++ Modules/EMap/Routes/web.php
@@ -11,7 +11,6 @@
 |
 */
 
-
 use Modules\EMap\Http\Controllers\OrganizationAuthController;
 
 Route::prefix('organization')->as('organization.')->group(function () {
@@ -21,13 +20,16 @@ Route::prefix('organization')->as('organization.')->group(function () {
     Route::get('logout', [OrganizationAuthController::class, 'logout'])->name('logout');
     Route::get('{organization}/invitation', [OrganizationAuthController::class, 'invitation'])->name('invitation');
     Route::view('dashboard', 'emap::organization.dashboard');
+    Route::view('mapacceptance', 'emap::organization.application.map_acceptance');
     Route::get('/password/create', [OrganizationAuthController::class, 'create'])->name('password.create')->middleware([  'password.check']);
     Route::post('/password/store', [OrganizationAuthController::class, 'store'])->name('password.store')->middleware(['password.check']);
 });
+
 Route::view('/e-map', 'emap::frontend.e-map.index')->name('e-map');
 Route::view('/downloads', 'emap::frontend.e-map.downloads.downloads');
 Route::view('/e-help', 'emap::frontend.e-map.e-help.e-help');
 Route::view('/notice', 'emap::frontend.e-map.notice.notice');
 Route::view('/register', 'emap::frontend.e-map.register.register-form');
 Route::view('/track', 'emap::frontend.e-map.track.track');
+Route::view('/track', 'emap::organization.e-map.track.track');
 
