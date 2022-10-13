e7a2b3371026c5c95f6ee24984ac64a67ae0ac56
Tekchan Khatri <tekam7755@gmail.com>
2022-10-13 12:24:43 +0545

    businessRegistration

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Notifications\DatabaseNotification;
-use Illuminate\Notifications\Notification;
 
 class NotificationController extends Controller
 {
