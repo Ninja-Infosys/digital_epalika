0b36b22a64ebd81a103418a1245b19a17753e1f1
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-10-13 16:57:58 +0545

    small changes menu list

--- resources/views/admin/layouts/menu_list.blade.php
+++ resources/views/admin/layouts/menu_list.blade.php
@@ -93,15 +93,6 @@
     <div class="col-md-4 border">
         <a href="#">
             <div class="p-2 text-center">
-                <img src="{{asset('assets/backend/images/modules/paryogkarta-bhumika.png')}}"
-                     height="50" width="50">
-                <h4 class="p-1">प्रयोगकर्ता र भूमिका</h4>
-            </div>
-        </a>
-    </div>
-    <div class="col-md-4 border">
-        <a href="#">
-            <div class="p-2 text-center">
                 <img src="{{asset('assets/backend/images/modules/setting.png')}}"
                      height="50" width="50">
                 <h4 class="p-1">सेटिङ</h4>
