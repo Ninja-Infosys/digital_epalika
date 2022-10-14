edfdcd14249a4d5088b6d082824457d27ac77d1f
Ninja <bijaygamuwatharu@gmail.com>
2022-10-14 10:51:35 +0545

    wesite setting sidebar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -204,7 +204,6 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="header-title">Bar Chart</h4>
-
                     <div class="mt-4 chartjs-chart">
                         <canvas id="bar-chart-example" height="350" data-colors="#4a81d4,#e3eaef"></canvas>
                     </div>
@@ -217,11 +216,9 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="header-title">Pie Chart</h4>
-
                     <div class="mt-4 chartjs-chart">
                         <canvas id="pie-chart-example" height="350" class="mt-4" data-colors="#6658dd,#fa5c7c,#4fc6e1,#ebeff2"></canvas>
                     </div>
-
                 </div> <!-- end card-body-->
             </div> <!-- end card-->
         </div> <!-- end col -->
@@ -229,11 +226,9 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="header-title">Donut Chart</h4>
-
                     <div class="mt-4 chartjs-chart">
                         <canvas id="donut-chart-example" height="350" data-colors="#6c757d,#1abc9c,#ebeff2"></canvas>
                     </div>
-
                 </div> <!-- end card-body-->
             </div> <!-- end card-->
         </div> <!-- end col -->
@@ -255,7 +250,6 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="header-title">Radar Chart</h4>
-
                     <div class="mt-4 chartjs-chart">
                         <canvas id="radar-chart-example" height="350" data-colors="#39afd1,#a17fe0"></canvas>
                     </div>
