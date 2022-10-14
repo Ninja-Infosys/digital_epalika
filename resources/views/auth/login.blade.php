04595866e2cad40f73a320759159fcb6de947f47
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-10-14 12:38:07 +0545

    ui changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,88 +37,78 @@
             <div class="col-md-7">
                 <div class="card">
                     <div class="row">
-                        <div class="col-md-5 system_info">
+                        <div class="col-md-6 system_info">
                             <div class="logo">
                                 <img src="{{asset('images/np.png')}}" height="80" alt="Logo">
                             </div>
-                            <div class="title">
-                                <h4>
-                                    <b>{{$officeSetting->localBody->local_body??''}}</b> <br>
-                                    <span class="text-light">
-                                        {{$officeSetting->district->district??''}} <br>
-                                        {{$officeSetting->province->province??''}}, नेपाल
-                                    </span>
-                                </h4>
-                                <p>
-                                    डिजिटल पालिका ब्यबस्थापन प्रणालि
-                                    <br>
-                                    (Digital Palika Management System)
-                                </p>
+                            <div class="m-2 ">
+                                <h3 class="text-white">{{$officeSetting->localBody->local_body??''}}</h3>
+                                <h4 class="text-white pt-2">डिजिटल ई-पालिका</h4>
                             </div>
                         </div>
-                        <div class="col-md-7">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h2 class="text-center">Log In</h2>
-                                    <form action="{{route('login')}}" method="post">
+                        <div class="col-md-6">
+                            <div class="card card-body">
+                                    <h2 class="text-center">
+                                        <i class="fa fa-user-lock"></i>
+                                    </h2>
+                                    <form class="mt-1" action="{{route('login')}}" method="post">
                                         @csrf
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Email address</label>
+                                        <div class="mb-2">
+                                            <label for="email" class="form-label">
+                                                प्रयोगकर्ता इमेल
+                                            <span class="text-danger">*</span>
+                                            </label>
                                             <input
                                                 name="email"
                                                 class="form-control @error('email') is-invalid @enderror"
                                                 type="email"
                                                 value="{{old('email')}}"
                                                 id="email"
-                                                placeholder="Email Address"
+                                                placeholder="प्रयोगकर्ता इमेल"
                                             />
                                             @error('email')
                                             <div class="invalid-feedback">{{$message}}</div>
                                             @enderror
                                         </div>
                                         <div class="mb-3">
-                                            <label for="password" class="form-label">Password</label>
+                                            <label for="password" class="form-label">पासवर्ड
+                                                <span class="text-danger">*</span>
+                                            </label>
                                             <input
                                                 name="password"
                                                 class="form-control @error('password') is-invalid @enderror"
                                                 type="password"
                                                 id="password"
-                                                placeholder="Password"
+                                                placeholder="पासवर्ड"
                                             />
                                             @error('password')
                                             <div class="invalid-feedback">{{$message}}</div>
                                             @enderror
                                         </div>
-                                        <div class="mb-3">
-                                            <div class="text-center">
-                                                <button class="btn login-btn" type="submit">
+                                        <div class="button-list">
+                                                <button type="submit" class="btn btn-success waves-effect waves-light">
+                                                    <i class="fa fa-lock"></i>
                                                     साइन इन
                                                 </button>
-                                            </div>
+                                                <button type="reset" class="btn btn-danger waves-effect">
+                                                    <i class="fa fa-times-circle"></i>
+                                                    रिसेट</button>
                                         </div>
                                     </form>
-                                    <div class="row technical-support">
-                                        <p>
-                                            <b>प्राविधिक सहायता कक्ष:</b>
-                                            <br>
-                                            सम्पर्क नम्बर: 081-520361/9858042433
-                                            <br>
-                                            इमेल: ninjainfosys@gmail.com
-                                        </p>
-                                    </div>
+                                <div class="col-12 text-lg-end">
+                                        <a href="auth-recoverpw.html" class="ms-1">Forgot password?</a>
+                                </div>
+                                <div class="alert alert-info mt-3">
+                                    <h4 class="mb-1 text-decoration-underline">प्राविधिक सहायता कक्ष</h4>
+                                    <p>
+                                     <i class="fa fa-phone-alt"></i> : 081-520361/9858042433
+                                    </p>
+                                      <p>
+                                      <i class="fa fa-envelope"></i> : ninjainfosys@gmail.com
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-12 text-center">
-                        <p>
-                            <a href="auth-recoverpw.html" class="text-white-50 ms-1"
-                            >Forgot your password?</a
-                            >
-                        </p>
                     </div>
                 </div>
             </div>
@@ -126,12 +116,12 @@
     </div>
 </div>
 
-<footer class="footer footer-alt">
+<footer class="footer footer-alt bg-soft-main">
     2015 -
     <script>
         document.write(new Date().getFullYear());
     </script>
-    &copy; Design & Developed By <a href="#" class="text-white-50">Ninja Infosys</a>
+    &copy; Design & Developed by <a href="#" class="text-white text-decoration-underline">NINJA INFOSYS</a>
 </footer>
 
 <!-- Vendor js -->
