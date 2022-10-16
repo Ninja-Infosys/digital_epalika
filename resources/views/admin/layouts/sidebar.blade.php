5907afbdc13849522621aef1e508e55d7091d1fa
Ninja <bijaygamuwatharu@gmail.com>
2022-10-16 15:53:20 +0545

    main sidebar

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,6 @@
+<li class="{{request()->is('admin/dashboard') ? 'active' : ''}}">
+    <a href="{{route('admin.dashboard')}}">
+        <i class="fa fa-home"></i>
+        <span> ड्यासबोर्ड</span>
+    </a>
+</li>
