cf5db234a2b9b489f61aa908d87bc4259262e106
Ninja <bijaygamuwatharu@gmail.com>
2022-10-16 13:12:20 +0545

    dashboard

--- Modules/EMap/Resources/views/admin/dashboard.blade.php
+++ Modules/EMap/Resources/views/admin/dashboard.blade.php
@@ -26,7 +26,7 @@
                             <div class="row">
                                 <div class="col-6">
                                     <div class="avatar-lg rounded-circle bg-danger border-danger border">
-                                        <i class="fa fa-users font-22 avatar-title text-white"></i>
+                                        <i class="fa fa-sitemap font-22 avatar-title text-white"></i>
                                     </div>
                                 </div>
                                 <div class="col-6">
@@ -35,7 +35,7 @@
                                         2
                                     </span>
                                         </h3>
-                                        <p class="text-muted mb-1">जम्मा प्रयोगकर्ताहरु</p>
+                                        <p class="text-muted mb-1">जम्मा संगठन</p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
@@ -49,7 +49,7 @@
                             <div class="row">
                                 <div class="col-6">
                                     <div class="avatar-lg rounded-circle bg-secondary border-secondary border">
-                                        <i class="fa fa-clipboard-list font-22 avatar-title text-white"></i>
+                                        <i class="fa fa-map-marked font-22 avatar-title text-white"></i>
                                     </div>
                                 </div>
                                 <div class="col-6">
@@ -58,41 +58,20 @@
                                          3
                                     </span>
                                         </h3>
-                                        <p class="text-muted mb-1">जम्मा व्यवसायहरु</p>
+                                        <p class="text-muted mb-1">जम्मा नक्सा</p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
                         </div>
                     </div> <!-- end widget-rounded-circle-->
                 </div> <!-- end col-->
-
-                <div class="col-md-6 col-xl-3">
-                    <div class="widget-rounded-circle card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="avatar-lg rounded-circle bg-blue border-blue border">
-                                        <i class="fa fa-building font-22 avatar-title text-white"></i>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="text-end">
-                                        <h3 class="mt-1"><span data-plugin="counterup">4</span></h3>
-                                        <p class="text-muted mb-1">जम्मा दर्ता भएका व्यवसायहरु</p>
-                                    </div>
-                                </div>
-                            </div> <!-- end row-->
-                        </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
-
                 <div class="col-md-6 col-xl-3">
                     <div class="widget-rounded-circle card">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-6">
                                     <div class="avatar-lg rounded-circle bg-pink border-pink border">
-                                        <i class="fa fa-comment font-22 avatar-title text-white"></i>
+                                        <i class="fa fa-file font-22 avatar-title text-white"></i>
                                     </div>
                                 </div>
                                 <div class="col-6">
@@ -100,7 +79,7 @@
                                         <h3 class="mt-1"><span data-plugin="counterup">
                                        6
                                     </span></h3>
-                                        <p class="text-muted mb-1">जम्मा चलिरहेको गुनासोहरु</p>
+                                        <p class="text-muted mb-1">वार्षिक दर्ता नक्सा</p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
@@ -113,7 +92,7 @@
                             <div class="row">
                                 <div class="col-6">
                                     <div class="avatar-lg rounded-circle bg-primary border-primary border">
-                                        <i class="fa fa-building font-22 avatar-title text-white"></i>
+                                        <i class="fa fa-file font-22 avatar-title text-white"></i>
                                     </div>
                                 </div>
                                 <div class="col-6">
@@ -121,73 +100,81 @@
                                         <h3 class="mt-1"><span data-plugin="counterup">
                                         7
                                     </span></h3>
-                                        <p class="text-muted mb-1">जम्मा दर्ता भएका व्यवसायहरु </p>
+                                        <p class="text-muted mb-1">मासिक दर्ता नक्सा</p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
                         </div>
                     </div> <!-- end widget-rounded-circle-->
                 </div> <!-- end col-->
-                <div class="col-md-6 col-xl-3">
-                    <div class="widget-rounded-circle card">
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="avatar-lg rounded-circle bg-info border-info border">
-                                        <i class="fa fa-file-alt font-22 avatar-title text-white"></i>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="text-end">
-                                        <h3 class="mt-1"><span data-plugin="counterup">
-                                        7</span></h3>
-                                        <p class="text-muted mb-1">जम्मा चलानी पत्रहरु</p>
-                                    </div>
-                                </div>
-                            </div> <!-- end row-->
-                        </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
-                <div class="col-md-6 col-xl-3">
-                    <div class="widget-rounded-circle card">
+                            <h4 class="header-title">Line Chart</h4>
+                            <div class="mt-4 chartjs-chart">
+                                <canvas id="line-chart-example" height="350" data-colors="#1abc9c,#f1556c"></canvas>
+                            </div>
+                        </div> <!-- end card-body-->
+                    </div> <!-- end card-->
+                </div> <!-- end col -->
+                <div class="col-lg-6">
+                    <div class="card">
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="avatar-lg rounded-circle bg-success border-success border">
-                                        <i class="fa fa-file-alt font-22 avatar-title text-white"></i>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="text-end">
-                                        <h3 class="mt-1"><span data-plugin="counterup">
-                                        9</span></h3>
-                                        <p class="text-muted mb-1">जम्मा दर्ता पत्रहरु</p>
-                                    </div>
-                                </div>
-                            </div> <!-- end row-->
-                        </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
-                <div class="col-md-6 col-xl-3">
-                    <div class="widget-rounded-circle card">
+                            <h4 class="header-title">Bar Chart</h4>
+                            <div class="mt-4 chartjs-chart">
+                                <canvas id="bar-chart-example" height="350" data-colors="#4a81d4,#e3eaef"></canvas>
+                            </div>
+                        </div> <!-- end card-body-->
+                    </div> <!-- end card-->
+                </div> <!-- end col -->
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="avatar-lg rounded-circle bg-danger border-danger border">
-                                        <i class="fa fa-handshake font-22 avatar-title text-white"></i>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="text-end">
-                                        <h3 class="mt-1"><span data-plugin="counterup">
-                                        3</span></h3>
-                                        <p class="text-muted mb-1">जम्मा कार्यपालिका बैठकहरु</p>
-                                    </div>
-                                </div>
-                            </div> <!-- end row-->
-                        </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
+                            <h4 class="header-title">Pie Chart</h4>
+                            <div class="mt-4 chartjs-chart">
+                                <canvas id="pie-chart-example" height="350" class="mt-4" data-colors="#6658dd,#fa5c7c,#4fc6e1,#ebeff2"></canvas>
+                            </div>
+                        </div> <!-- end card-body-->
+                    </div> <!-- end card-->
+                </div> <!-- end col -->
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="header-title">Donut Chart</h4>
+                            <div class="mt-4 chartjs-chart">
+                                <canvas id="donut-chart-example" height="350" data-colors="#6c757d,#1abc9c,#ebeff2"></canvas>
+                            </div>
+                        </div> <!-- end card-body-->
+                    </div> <!-- end card-->
+                </div> <!-- end col -->
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="header-title">Polar area Chart</h4>
+
+                            <div class="mt-4 chartjs-chart">
+                                <canvas id="polar-chart-example" height="350" data-colors="#4a81d4,#fa5c7c,#4fc6e1,#ebeff2"> </canvas>
+                            </div>
+
+                        </div> <!-- end card-body-->
+                    </div> <!-- end card-->
+                </div> <!-- end col -->
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="header-title">Radar Chart</h4>
+                            <div class="mt-4 chartjs-chart">
+                                <canvas id="radar-chart-example" height="350" data-colors="#39afd1,#a17fe0"></canvas>
+                            </div>
+                        </div> <!-- end card-body-->
+                    </div> <!-- end card-->
+                </div> <!-- end col -->
             </div>
         </div>
     </div>
