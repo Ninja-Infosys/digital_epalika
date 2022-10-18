267f4bd1ca17fca5be27e73539633c2ea7b712b5
sush1l <sushilkc188@gmail.com>
2022-10-18 11:53:11 +0545

    form ui made

--- Modules/ListRegistration/Routes/admin.php
+++ Modules/ListRegistration/Routes/admin.php
@@ -6,3 +6,14 @@ use Modules\ListRegistration\Http\Controllers\Admin\ListRegistrationController;
 
 Route::get('dashboard', DashboardController::class)->name('dashboard');
 Route::resource('listRegistration', ListRegistrationController::class);
+
+Route::prefix('files')->as('files.')->group(function (){
+    Route::view('application-file', 'listregistration::admin.file.application_file')->name('application-file');
+    Route::view('notice', 'listregistration::admin.file.notice')->name('notice');
+    Route::view('application', 'listregistration::admin.file.application')->name('application');
+    Route::view('darta', 'listregistration::admin.file.darta')->name('darta');
+    Route::view('temporary', 'listregistration::admin.file.temporary')->name('temporary');
+    Route::view('taxclearance', 'listregistration::admin.file.taxclearance')->name('taxclearance');
+    Route::view('permission', 'listregistration::admin.file.permission')->name('permission');
+});
+
