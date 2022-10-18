310fcfc1d46391e7a1165fd29cd9f2a1ebd7fcf1
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-10-18 16:31:47 +0545

    notice board small changes

--- resources/views/frontend/digital_board.blade.php
+++ resources/views/frontend/digital_board.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <x-frontend.notice-vertical-slider-component/>
-</div>
+@extends('frontend.layouts.master')
+@section('content')
+    <main class="container-fluid">
+        <section class="news-section">
+            news
+        </section>
+        <section class="mid">
+            <div class="row">
+                <div class="col-md-6">
+                        <x-frontend.notice-vertical-slider-component/>
+                </div>
+                <div class="col-md-6">
+                   <x-frontend.employee-section-component/>
+                </div>
+            </div>
+        </section>
+        <section class="last">
+            <div class="row">
+                <div class="col-md-6">
+                    info desk
+                </div>
+                <div class="col-md-6">
+                    video desk
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
