2d6e998f182e9c277668c712ebca51341635ed05
Ninja <bijaygamuwatharu@gmail.com>
2022-10-19 10:37:20 +0545

    employee section

--- resources/views/components/frontend/employee-section-component.blade.php
+++ resources/views/components/frontend/employee-section-component.blade.php
@@ -1,26 +1,24 @@
-<section class="employee">
+<section class="employee pt-2">
     <div class="chiefPerson pb-4">
         <div class="card-employee rounded shadow d-flex mx-auto">
-            <div class="avatar avatar-lg">
+            <div class="avatar avatar-lg px-auto">
                 <img
                     src="{{asset('assets/frontend/image/submetro.jpg')}}"
                     alt="माननीय चन्द्र बहादुर शाही">
             </div>
-            <div class="textbox-01 mx-auto">
+            <div class="textbox-01 mx-auto my-auto">
                 <h6>माननीय चन्द्र बहादुर शाही</h6>
                 <p>मन्त्री</p>
             </div>
-
         </div>
     </div>
     <div class="seccondLevelEmployee d-flex justify-content-between pb-3">
         <div class="card-employee rounded shadow d-flex">
             <div class="avatar avatar-lg ">
-                <img
-                    src="{{asset('assets/frontend/image/submetro.jpg')}}"
+                <img src="{{asset('assets/frontend/image/submetro.jpg')}}"
                     alt="माननीय चन्द्र बहादुर शाही">
             </div>
-            <div class="textbox-01 mx-auto ">
+            <div class="textbox-01 my-auto mx-auto">
                 <h6>माननीय चन्द्र बहादुर शाही</h6>
                 <p>मन्त्री</p>
             </div>
@@ -33,33 +31,19 @@
                     src="{{asset('assets/frontend/image/submetro.jpg')}}"
                     alt="माननीय चन्द्र बहादुर शाही">
             </div>
-            <div class="textbox-01 mx-auto">
+            <div class="textbox-01 mx-auto my-auto">
                 <h6>माननीय चन्द्र बहादुर शाही</h6>
                 <p>मन्त्री</p>
             </div>
         </div>
     </div>
     <div class="lastLevelEmployee d-flex justify-content-between">
-
         <div class="card-employee rounded shadow d-flex">
             <div class="avatar avatar-lg">
-                <img
-                    src="{{asset('assets/frontend/image/submetro.jpg')}}"
+                <img src="{{asset('assets/frontend/image/submetro.jpg')}}"
                     alt="माननीय चन्द्र बहादुर शाही">
             </div>
-            <div class="textbox-01 mx-auto">
-                <h6>माननीय चन्द्र बहादुर शाही</h6>
-                <p>मन्त्री</p>
-            </div>
-        </div>
-
-        <div class="card-employee rounded shadow d-flex">
-            <div class="avatar avatar-lg">
-                <img
-                    src="{{asset('assets/frontend/image/submetro.jpg')}}"
-                    alt="माननीय चन्द्र बहादुर शाही">
-            </div>
-            <div class="textbox-01 mx-auto">
+            <div class="textbox-01 mx-auto px-auto">
                 <h6>माननीय चन्द्र बहादुर शाही</h6>
                 <p>मन्त्री</p>
             </div>
@@ -70,7 +54,18 @@
                     src="{{asset('assets/frontend/image/submetro.jpg')}}"
                     alt="माननीय चन्द्र बहादुर शाही">
             </div>
-            <div class="textbox-01 mx-auto">
+            <div class="textbox-01 mx-auto my-auto">
+                <h6>माननीय चन्द्र बहादुर शाही</h6>
+                <p>मन्त्री</p>
+            </div>
+        </div>
+        <div class="card-employee rounded shadow d-flex">
+            <div class="avatar avatar-lg">
+                <img
+                    src="{{asset('assets/frontend/image/submetro.jpg')}}"
+                    alt="माननीय चन्द्र बहादुर शाही">
+            </div>
+            <div class="textbox-01 mx-auto my-auto">
                 <h6>माननीय चन्द्र बहादुर शाही</h6>
                 <p>मन्त्री</p>
             </div>
