73e8c335058e0e5de81f2ec31cd9cd5af7dea1ff
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-10-19 16:46:13 +0545

    notice board ui change

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -7,9 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Digital e-Palika</title>
     <link rel="stylesheet" href="{{asset('assets/frontend/css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/frontend/css/navbar.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/frontend/css/header.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/frontend/css/footer.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
