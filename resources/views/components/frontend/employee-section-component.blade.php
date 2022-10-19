280938e9f8570294500b37ad40d3479d6e1aacec
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-10-19 17:27:50 +0545

    notice board employee change

--- resources/views/components/frontend/employee-section-component.blade.php
+++ resources/views/components/frontend/employee-section-component.blade.php
@@ -2,39 +2,56 @@
     <div id="carouselExampleSlidesOnly" class="carousel slide height" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                @foreach($employees->take(3) as $employee)
-                    <div class="{{$loop->first ? 'd-flex justify-content-center' : 'd-flex justify-content-between'}} pb-3">
+                    <div class="d-flex justify-content-center pb-3">
                         <div class="card-employee rounded d-flex">
                             <div class="avatar avatar-lg">
                                 <img
-                                    src="{{$employee->photo_url}}"
-                                    alt="{{$employee->name}}">
+                                    src="{{$employees->first()->photo_url ?? ''}}"
+                                    alt="{{$employees->first()->name ?? ''}}">
                             </div>
                             <div class="textbox-01 px-2">
-                                <h6>{{$employee->name}}</h6>
-                                <p>{{$employee->designation}}</p>
+                                <h6>{{$employees->first()->name ?? ''}}</h6>
+                                <p>{{$employees->first()->designation ?? ''}}</p>
                             </div>
                         </div>
                     </div>
-                @endforeach
+                    <div class="row pb-3">
+                        @foreach($employees->skip(1)->take(2) as $otherEmployee)
+                            <div class="col-md-6">
+                                <div class="card-employee rounded d-flex">
+                                    <div class="avatar avatar-lg">
+                                        <img
+                                            src="{{$otherEmployee->photo_url}}"
+                                            alt="{{$otherEmployee->name}}">
+                                    </div>
+                                    <div class="textbox-01 px-2">
+                                        <h6>{{$otherEmployee->name}}</h6>
+                                        <p>{{$otherEmployee->designation}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
             </div>
             @foreach($employees->skip(3)->chunk(4) as $empChunk)
                 <div class="carousel-item">
-                    @foreach($empChunk as $employee)
-                        <div class="d-flex justify-content-between pb-3">
-                            <div class="card-employee rounded d-flex">
-                                <div class="avatar avatar-lg">
-                                    <img
-                                        src="{{$employee->photo_url}}"
-                                        alt="{{$employee->name}}">
-                                </div>
-                                <div class="textbox-01 px-2">
-                                    <h6>{{$employee->name}}</h6>
-                                    <p>{{$employee->designation}}</p>
+                    <div class="row">
+                        @foreach($empChunk as $employee)
+                            <div class="col-md-6 mb-3">
+                                <div class="card-employee rounded d-flex">
+                                    <div class="avatar avatar-lg">
+                                        <img
+                                            src="{{$employee->photo_url}}"
+                                            alt="{{$employee->name}}">
+                                    </div>
+                                    <div class="textbox-01 px-2">
+                                        <h6>{{$employee->name}}</h6>
+                                        <p>{{$employee->designation}}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
             @endforeach
         </div>
