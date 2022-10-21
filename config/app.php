24de3d99546b65ca202ed8b9b3e0cb141776e547
Ninja <bijaygamuwatharu@gmail.com>
2022-10-21 13:14:53 +0545

    digital-board

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-    'website_type' => env('WEBSITE_TYPE', 'website'),
+    'website_type' => env('WEBSITE_TYPE', 'digital_board'),
 
     /*
     |--------------------------------------------------------------------------
