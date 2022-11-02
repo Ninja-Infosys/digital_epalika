3ad2cc2d9e7ab8088f0dde2dba14011d43555279
manoj <manojoli667@gmail.com>
2022-11-02 14:35:50 +0545

    updates

--- Modules/Grant/Database/Seeders/GrantPermissionTableSeeder.php
+++ Modules/Grant/Database/Seeders/GrantPermissionTableSeeder.php
@@ -24,6 +24,10 @@ class GrantPermissionTableSeeder extends Seeder
             'grantType_create',
             'grantType_edit',
             'grantType_delete',
+            'grantActivity_access',
+            'grantActivity_create',
+            'grantActivity_edit',
+            'grantActivity_delete',
         ];
 
         $this->storePermission($permissions);
--- Modules/Grant/Routes/admin.php
+++ Modules/Grant/Routes/admin.php
@@ -6,6 +6,7 @@ use Modules\Grant\Http\Controllers\Admin\DashboardController;
 use Modules\Grant\Http\Controllers\Admin\GrantTypeController;
 use Modules\Grant\Http\Controllers\Admin\InfrastructureController;
 use Modules\Grant\Http\Controllers\Admin\ThematicAreaController;
+use Modules\Grant\Http\Controllers\GrantActivityController;
 
 Route::get('dashboard', DashboardController::class)->name('dashboard');
 
@@ -13,6 +14,7 @@ Route::prefix('setting')->group(function (){
     Route::resource('infrastructure', InfrastructureController::class);
     Route::resource('thematicArea', ThematicAreaController::class);
     Route::resource('grantType', GrantTypeController::class);
+    Route::resource('grantActivity', GrantActivityController::class);
 });
 
 Route::view('grantDetail','grant::admin.grant_detail.create')->name('grantDetail.create');
