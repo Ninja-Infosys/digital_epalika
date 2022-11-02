998412c5a0dea86fdbd4ecd871e56a0beaad4230
manoj <manojoli667@gmail.com>
2022-11-02 10:55:34 +0545

    grant module started

--- app/View/Components/PrintButton.php
+++ app/View/Components/PrintButton.php
@@ -2,13 +2,16 @@
 
 namespace App\View\Components;
 
+use App\Models\Settings\OfficeSetting;
 use Illuminate\View\Component;
 
 class PrintButton extends Component
 {
+    public object $setting;
+
     public function __construct(public $title)
     {
-
+        $this->setting = OfficeSetting::with('province','district','localBody')->first();
     }
 
     public function render()
