e6ca3ef59d009d9cd17ec6444f422665bd4e2f79
Ninja <bijaygamuwatharu@gmail.com>
2022-11-02 13:32:10 +0545

    roaster department

--- Modules/Roaster/Resources/views/admin/setting/department/create.blade.php
+++ Modules/Roaster/Resources/views/admin/setting/department/create.blade.php
@@ -1,74 +1,62 @@
-@extends('backend.layouts.master')
+@extends('admin.layouts.master')
 @section('content')
-    <div class="">
-        <div class="page-title d-flex justify-content-between">
-            <h5>बिभागहरु</h5>
-            <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{route('dashboard')}}">ड्यासबोर्ड</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">बिभागहरुको विवरण</li>
-                </ol>
-            </nav>
-        </div>
-        <div class="card mb-3">
-            <div class="card-header d-flex justify-content-between">
-                <h6>बिभागहरुको विवरण</h6>
-            </div>
-            <form action="{{route('admin.setting.department.store')}}" method="post">
-                <div class="card-body">
-                    @csrf
-                    <div class="col-md-12 col-sm-12 form-group">
-                        <label for="title">बिभाग * </label>
-                        <input id="title" type="text" name="title" placeholder="बिभाग"
-                               class="form-control @error('title') is-invalid @enderror" value="{{old('title')}}">
-                        @error('title')
-                        <div class="text-danger">{{$message}}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item">
+                            <a href="#">
+                                <i class="fa fa-home"></i> गृहपृष्ठ
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#">तालिम व्यवस्थापन</a>
+                        </li>
+                        <li class="breadcrumb-item active">नयाँ बिभाग थप्नुहोस्</li>
+                    </ol>
                 </div>
-            </form>
+                <h4 class="page-title">बिभाग थप्नुहोस्</h4>
+            </div>
         </div>
+    </div>
 
-        <div class="card">
-
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <thead>
-                        <tr>
-                            <th>क्र.सं</th>
-                            <th>बिभाग</th>
-                            <th></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @forelse($departments as $departments)
-                            <tr>
-                                <td>{{$loop->iteration}}</td>
-                                <td>{{$departments->title}}</td>
-                                <td class="d-flex justify-center">
-                                    <a href="{{route('admin.settings.department.edit', $departments)}}" type="button"
-                                       class="btn btn-primary">
-                                        <i class="fa fa-edit"></i>
-                                    </a>
-                                    <form action="{{route('admin.settings.department.destroy', $departments)}}"
-                                          method="post">
-                                        @csrf
-                                        @method('delete')
-                                        <button type="submit" class="show_confirm btn btn-danger">
-                                            <i class="fa fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="3">Data not found !!!</td>
-                            </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <h4 class="header-title">विभाग थप्नुहोस्</h4>
+                        <a href="{{route('admin.roaster.setting.department.index')}}"
+                           class="btn btn-sm btn-outline-primary">
+                            <i class="fa fa-list"></i> विभाग सूची
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('admin.roaster.setting.department.store')}}" method="post"
+                          enctype="multipart/form-data">
+                        @csrf
+                        <fieldset class="border p-2 mb-2">
+                            <legend class="font-16 text-info">
+                                <strong> विवरण </strong>
+                            </legend>
+                            <div class="row">
+                                <div class="col-md-6 mb-2">
+                                    <label for="title" class="form-label">विभाग *</label>
+                                    <input id="title" type="text" name="title" placeholder="बिभाग"
+                                           class="form-control @error('title') is-invalid @enderror"
+                                           value="{{old('title')}}">
+                                    @error('title')
+                                    <div class="text-danger">{{$message}}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                        </fieldset>
+                        <button type="submit" class="btn btn-primary">
+                            Save
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
--- Modules/Roaster/Resources/views/admin/setting/subject/create.blade.php
+++ Modules/Roaster/Resources/views/admin/setting/subject/create.blade.php
@@ -1,5 +1,4 @@
 @extends('admin.layouts.master')
-
 @section('content')
     <div class="row">
         <div class="col-12">
@@ -21,7 +20,6 @@
             </div>
         </div>
     </div>
-
     <div class="row">
         <div class="col-md-12">
             <div class="card">
