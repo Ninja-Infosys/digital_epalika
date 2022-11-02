3cc5737139c5f6acdbedc7304f49f56e1fc22ec1
manoj <manojoli667@gmail.com>
2022-11-02 12:58:07 +0545

    updates

--- Modules/Grant/Routes/admin.php
+++ Modules/Grant/Routes/admin.php
@@ -9,3 +9,5 @@ Route::get('dashboard', DashboardController::class)->name('dashboard');
 Route::prefix('setting')->group(function (){
     Route::resource('infrastructure', InfrastructureController::class);
 });
+
+Route::view('grantDetail','grant::admin.grant_detail.create')->name('grantDetail.create');
