910faa1d364eea22064d8e8d6c9dbed44249c70b
sush1l <sushilkc188@gmail.com>
2022-11-04 16:36:05 +0545

    emap form print made

--- Modules/EMap/Resources/views/admin/map/report/first_phase.blade.php
+++ Modules/EMap/Resources/views/admin/map/report/first_phase.blade.php
@@ -1,4 +1,4 @@
-<p class="mb-3">
+<span class="mb-3">
     यस {{config('applicationDetail.office_type')}}को वडा नं.<span
         class="underline-dotted">{{$mapApply->landDetail->ward_no??''}}</span>टोल<span
         class="underline-dotted">{{$mapApply->landDetail->tole??''}}</span>मा अवस्थित साविक<span
@@ -9,109 +9,109 @@
     निर्माण गर्ने क्रममा
     प्लिन्थ लेभलसम्मको निर्माण कार्य सम्पन्न भएको हुँदा मिति<span class="underline-dotted custom-width"></span> मा
     स्थलगत निरीक्षण गरी देहाय बमोजिमको प्रतिवेदन पेश गरेको छु |
-</p>
-<p>१. सडक अधिकार क्षेत्र सम्बन्धि मापदण्ड (सडक सेटब्याक) पालना
+</span><br>
+<span>१. सडक अधिकार क्षेत्र सम्बन्धि मापदण्ड (सडक सेटब्याक) पालना
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio1"
            value="option1">
-    <label class="form-check-label" for="inlineRadio1">भएको</label>
+    <label class="form-check-label" for="inlineRadio1">भएको</label>&emsp;
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio2"
            value="option2">
-    <label class="form-check-label" for="inlineRadio2">नभएको</label> (छैन भने विवरण खुलाउने)<br>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-</p>
-<p class="mt-2">२. साइट प्लानमा देखाइए बमोजिम सेटब्याक पालना
+    <label class="form-check-label" for="inlineRadio2">नभएको</label>&emsp; (छैन भने विवरण खुलाउने)<br>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+</span><br>
+<span class="mt-2">२. साइट प्लानमा देखाइए बमोजिम सेटब्याक पालना
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio1"
            value="option1">
-    <label class="form-check-label" for="inlineRadio1">भएको</label>
+    <label class="form-check-label" for="inlineRadio1">भएको</label>&emsp;
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio2"
            value="option2">
-    <label class="form-check-label" for="inlineRadio2">नभएको</label> (छैन भने विवरण खुलाउने)<br>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-</p>
-<p class="mt-2">३. ग्राउण्ड कभरेजमा फरक
+    <label class="form-check-label" for="inlineRadio2">नभएको</label>&emsp; (छैन भने विवरण खुलाउने)<br>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+</span><br>
+<span class="mt-2">३. ग्राउण्ड कभरेजमा फरक
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio1"
            value="option1">
-    <label class="form-check-label" for="inlineRadio1">परेको</label>
+    <label class="form-check-label" for="inlineRadio1">परेको</label>&emsp;
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio2"
            value="option2">
-    <label class="form-check-label" for="inlineRadio2">नपरेको</label> (फरक परे विवरण खुलाउने)<br>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-</p>
-<p class="mt-2">४. भवनको लम्बाई र चौडाई फिल्ड र नक्सामा फेरबदल
+    <label class="form-check-label" for="inlineRadio2">नपरेको</label>&emsp; (फरक परे विवरण खुलाउने)<br>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+</span><br>
+<span class="mt-2">४. भवनको लम्बाई र चौडाई फिल्ड र नक्सामा फेरबदल
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio1"
            value="option1">
-    <label class="form-check-label" for="inlineRadio1">भएको</label>
+    <label class="form-check-label" for="inlineRadio1">भएको</label>&emsp;
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio2"
            value="option2">
-    <label class="form-check-label" for="inlineRadio2">नभएको</label> (भए सो को विवरण खुलाउने)<br>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-</p>
-<p class="mt-2">५. नेपाल राष्ट्रिय भवन निर्माण संहिता २०६० अनुसार निर्माण
+    <label class="form-check-label" for="inlineRadio2">नभएको</label> &emsp;(भए सो को विवरण खुलाउने)<br>
+   <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+</span><br>
+<span class="mt-2">५. नेपाल राष्ट्रिय भवन निर्माण संहिता २०६० अनुसार निर्माण
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio1"
            value="option1">
-    <label class="form-check-label" for="inlineRadio1">भएको</label>
+    <label class="form-check-label" for="inlineRadio1">भएको</label>&emsp;
     <input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions" id="inlineRadio2"
            value="option2">
-    <label class="form-check-label" for="inlineRadio2">नभएको</label> आंशिक पालना भएको (तपसिलको विवरणमा चिन्ह लगाउने,
+    <label class="form-check-label" for="inlineRadio2">नभएको</label>&emsp; आंशिक पालना भएको (तपसिलको विवरणमा चिन्ह लगाउने,
     नक्सा अनुसार फरक परेमा विवरण खुलाउने)<br>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-</p>
-<table class="table table-bordered mt-3">
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+</span><br>
+<table class="table table-sm table-bordered mt-2">
     <thead>
     <tr>
         <th scope="col">पिलरवाला घर</th>
-        <th scope="col">भएको</th>
-        <th scope="col">नभएको</th>
+        <th class="text-center" scope="col">भएको</th>
+        <th class="text-center" scope="col">नभएको</th>
         <th scope="col">गारोवाला घर</th>
     </tr>
     </thead>
     <tbody>
     <tr>
         <td>नक्सा अनुसार पिलरको संख्या र नाप</td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
         <td>नक्सा अनुसार गारोको चौडाई</td>
     </tr>
     <tr>
         <td>नक्सा अनुसार पिलरको डण्डीको संख्या र नाप</td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
         <td>सबै कुनामा ठाडो डण्डी</td>
     </tr>
     <tr>
         <td>बिम र पिलरको जोर्नी पर्याप्त मात्रामा बाँधिएको</td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
         <td rowspan="2">भ्याल र भेन्टिलेशनहरु<br>
             गारोको २ फिट टाढा
@@ -119,19 +119,19 @@
     </tr>
     <tr>
         <td>जगबिम</td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
-        <td><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
+        <td class="text-center"><input class="form-check-input form-check-inline" type="checkbox" name="inlineRadioOptions"
                    id="inlineRadio1" value="option1"></td>
 
     </tr>
     </tbody>
 </table>
-<p class="mt-2">६. अन्य कुनै कुरा भएको खुलाउने :<br>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-    <span class="underline-dotted custom-width"></span>
-</p>
+<span class="mt-2">६. अन्य कुनै कुरा भएको खुलाउने :<br>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+    <span class="underline-dotted"></span>
+</span><br>
