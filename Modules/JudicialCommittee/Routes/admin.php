21eb7fa37f4973eb3f74451793e16dfd37c12ec0
Ninja <bijaygamuwatharu@gmail.com>
2022-11-06 15:45:48 +0545

    judicial committee froms

--- Modules/JudicialCommittee/Routes/admin.php
+++ Modules/JudicialCommittee/Routes/admin.php
@@ -8,3 +8,6 @@ Route::get('dashboard', DashboardController::class)->name('dashboard');
 
 //static routes
 Route::view('application-form','judicialcommittee::admin.static.application_form')->name('applicationForm');
+Route::view('nissa-form','judicialcommittee::admin.static.nissa_form')->name('nissaForm');
+Route::view('defendant_continued_time','judicialcommittee::admin.static.defendant_continued_time')->name('defendantContinuedTime');
+Route::view('stay_date_form','judicialcommittee::admin.static.stay_date_form')->name('stayDateForm');
