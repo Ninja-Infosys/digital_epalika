0dc15c7aae2c3776fa29bf49d147453c5bc2e36a
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-11-06 13:23:37 +0545

    task management ui

--- Modules/TaskManagement/Resources/views/admin/layouts/sidebar.blade.php
+++ Modules/TaskManagement/Resources/views/admin/layouts/sidebar.blade.php
@@ -4,31 +4,17 @@
         <span> ड्यासबोर्ड</span>
     </a>
 </li>
-<li class="{{request()->is('admin/grant/report/*') ? 'active' : ''}}">
-    <a href="#sidebarGrantReport"
-       {{request()->is('admin/circular/report/*') ? 'aria-expanded=true  ' : ''}}
-       data-bs-toggle="collapse">
-        <i class="fa fa-clipboard-list"></i>
-        <span>रिपोर्ट</span>
-        <span class="menu-arrow">
-                        <i class="fa fa-angle-right"></i>
-                    </span>
+<li class="{{request()->is('admin/grant/dashboard') ? 'active' : ''}}">
+    <a href="{{route('admin.taskManagement.dashboard')}}">
+        <i class="fa fa-tasks"></i>
+        <span> दैनिक कार्य</span>
     </a>
-    <div class="collapse {{request()->is('admin/grant/report/*') ? 'show' : ''}}"
-         id="sidebarGrantReport">
-        <ul class="nav-second-level">
-            <li class="{{request()->is('admin/grant/report/registration') ? 'active' : ''}}">
-                <a href="{{route('admin.circular.registration.report')}}">
-                    <span> अनुदानको रिपोर्ट   </span>
-                </a>
-            </li>
-            <li class="{{request()->is('admin/grant/report/dispatch') ? 'active' : ''}}">
-                <a href="{{route('admin.circular.dispatch.report')}}">
-                    <span> आवेदन दर्ता रिपोर्ट</span>
-                </a>
-            </li>
-        </ul>
-    </div>
+</li>
+<li class="{{request()->is('admin/grant/dashboard') ? 'active' : ''}}">
+    <a href="{{route('admin.taskManagement.dashboard')}}">
+        <i class="fa fa-clipboard-list"></i>
+        <span> रिपोर्ट</span>
+    </a>
 </li>
 
 <li class="{{request()->is('admin/grant/setting/*') ? 'active' : ''}}">
@@ -44,41 +30,11 @@
     <div class="collapse {{request()->is('admin/grant/setting/*') ? 'show' : ''}}"
          id="sidebarGrantSetting">
         <ul class="nav-second-level">
-            @can('infrastructure_access')
                 <li class="{{request()->is('admin/grant/setting/infrastructure') ? 'active' : ''}}">
                     <a href="{{route('admin.grant.infrastructure.index')}}">
-                        <span>  पूर्वाधार शीर्षकहरु  </span>
+                        <span> शाखाहरु अनुसार कार्यहरू </span>
                     </a>
                 </li>
-            @endcan
-            @can('thematicArea_access')
-                <li class="{{request()->is('admin/grant/setting/thematicArea') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.thematicArea.index')}}">
-                        <span>  विषयगत क्षेत्र  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('grantType_access')
-                <li class="{{request()->is('admin/grant/setting/grantType') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.grantType.index')}}">
-                        <span> अनुदान प्रकार  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('grantProgram_access')
-                <li class="{{request()->is('admin/grant/setting/grantProgram') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.grantProgram.index')}}">
-                        <span> अनुदान कार्यक्रम  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('grantActivity_access')
-                <li class="{{request()->is('admin/grant/setting/grantActivity') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.grantActivity.index')}}">
-                        <span> अनुदान क्रियाकलाप  </span>
-                    </a>
-                </li>
-            @endcan
         </ul>
     </div>
 </li>
