91118c8fa5b166e4d94fafbfabb488adf120cb79
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-11-08 17:11:53 +0545

    task management

--- Modules/TaskManagement/Resources/views/admin/report/index.blade.php
+++ Modules/TaskManagement/Resources/views/admin/report/index.blade.php
@@ -1,4 +1,21 @@
 @extends('admin.layouts.master')
 @section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item">
+                            <a href="{{route('admin.taskManagement.dashboard')}}">
+                                <i class="fa fa-home"></i> गृहपृष्ठ
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active">रिपोर्ट</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">रिपोर्ट</h4>
+            </div>
+        </div>
+    </div>
     @livewire('taskmanagement::task-report-livewire')
 @endsection
--- Modules/TaskManagement/Resources/views/livewire/task-report-livewire.blade.php
+++ Modules/TaskManagement/Resources/views/livewire/task-report-livewire.blade.php
@@ -3,99 +3,6 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    {{--                <form wire:submit.prevent="submitFormData">--}}
-                    {{--                    <div class="row">--}}
-                    {{--                        <div class="col-md-3 mb-2">--}}
-                    {{--                        <label>मिति</label>--}}
-                    {{--                        <div class="input-group">--}}
-                    {{--                            <input type="text"--}}
-                    {{--                                   wire:model="form.from_date" class="form-control" id="from_date" placeholder="मिति देखि">--}}
-                    {{--                            @error('form.from_date')--}}
-                    {{--                            <div class="invalid-feedback">{{$message}}</div>--}}
-                    {{--                            @enderror--}}
-                    {{--                            <input type="text"--}}
-                    {{--                                   wire:model="form.to_date" class="form-control" id="to_date" placeholder="मिति सम्म">--}}
-                    {{--                            @error('form.to_date')--}}
-                    {{--                            <div class="invalid-feedback">{{$message}}</div>--}}
-                    {{--                            @enderror--}}
-                    {{--                        </div>--}}
-                    {{--                        </div>--}}
-                    {{--                        <div class="col-md-3 mb-2">--}}
-                    {{--                            <label for="branch_id" class="form-label">शाखा</label>--}}
-                    {{--                            <select--}}
-                    {{--                                wire:model="form.branch_id"--}}
-                    {{--                                class="form-select @error('branch_id') is-invalid @enderror"--}}
-                    {{--                                id="branch_id">--}}
-                    {{--                                <option value="">--- छान्नुहोस् ---</option>--}}
-                    {{--                                @foreach($branches as $branch)--}}
-                    {{--                                    @if(count($branch->branches)>0)--}}
-                    {{--                                        <optgroup label="{{$branch->branch_name}}">--}}
-                    {{--                                            @foreach($branch->branches as $sub_branch)--}}
-                    {{--                                                <option--}}
-                    {{--                                                    value="{{$sub_branch->id}}">--}}
-                    {{--                                                    {{$sub_branch->branch_name}}--}}
-                    {{--                                                </option>--}}
-                    {{--                                            @endforeach--}}
-                    {{--                                        </optgroup>--}}
-                    {{--                                    @else--}}
-                    {{--                                        <option--}}
-                    {{--                                            value="{{$branch->id}}">--}}
-                    {{--                                            {{$branch->branch_name}}--}}
-                    {{--                                        </option>--}}
-                    {{--                                    @endif--}}
-                    {{--                                @endforeach--}}
-                    {{--                            </select>--}}
-                    {{--                            @error('form.branch_id')--}}
-                    {{--                            <div class="invalid-feedback">{{$message}}</div>--}}
-                    {{--                            @enderror--}}
-                    {{--                        </div>--}}
-                    {{--                        <div class="col-md-3 mb-2">--}}
-                    {{--                            <label for="branch_id" class="form-label">शाखाहरु अनुसार कार्यहरू</label>--}}
-                    {{--                            <select--}}
-                    {{--                                wire:model="form.task_category_id"--}}
-                    {{--                                class="form-select @error('task_category_id') is-invalid @enderror"--}}
-                    {{--                                id="branch_id">--}}
-                    {{--                                <option value="">--- छान्नुहोस् ---</option>--}}
-                    {{--                                @foreach($taskCategories as $taskCategory)--}}
-                    {{--                                    <option value="{{$taskCategory->id}}">--}}
-                    {{--                                        {{$taskCategory->title}}--}}
-                    {{--                                    </option>--}}
-                    {{--                                @endforeach--}}
-                    {{--                            </select>--}}
-                    {{--                            @error('form.task_category_id')--}}
-                    {{--                            <div class="invalid-feedback">{{$message}}</div>--}}
-                    {{--                            @enderror--}}
-                    {{--                        </div>--}}
-                    {{--                        <div class="col-md-3 mb-2">--}}
-                    {{--                            <label for="branch_id" class="form-label">कार्य विभाजन *</label>--}}
-                    {{--                            <select--}}
-                    {{--                                wire:model="form.task_division_id"--}}
-                    {{--                                class="form-select @error('branch_id') is-invalid @enderror"--}}
-                    {{--                                id="branch_id">--}}
-                    {{--                                <option value="">--- छान्नुहोस् ---</option>--}}
-                    {{--                                @foreach($taskDivisions as $taskDivision)--}}
-                    {{--                                    <option value="{{$taskDivision->id}}">--}}
-                    {{--                                        {{$taskDivision->title}}--}}
-                    {{--                                    </option>--}}
-                    {{--                                @endforeach--}}
-                    {{--                            </select>--}}
-                    {{--                            @error('form.task_division_id')--}}
-                    {{--                            <div class="invalid-feedback">{{$message}}</div>--}}
-                    {{--                            @enderror--}}
-                    {{--                        </div>--}}
-                    {{--                    </div>--}}
-                    {{--                    <button type="submit" class="btn btn-primary">--}}
-                    {{--                        <i class="fa fa-search"></i>--}}
-                    {{--                    </button>--}}
-                    {{--                </form>--}}
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
                     <form class="p-1 border mt-1">
                         <div class="row">
                             <div class="col-md-3 mb-1">
@@ -153,34 +60,53 @@
                             </div>
                         </div>
                     </form>
-
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="col-sm-3">
+                        <button class="btn btn-sm btn-info"
+                                onclick="printJS({
+                                printable: 'printData',
+                                css: '{{asset('assets/backend/css/print.css')}}',
+                                type: 'html'
+                                })">
+                            <i class="fa fa-print"></i> Print
+                        </button>
+                    </div>
                     <div class="table-responsive mt-2">
-                        <table class="table table-sm table-bordered">
-                            <thead>
-                            <tr>
-                                <th>क्र.स</th>
-                                <th>मिति</th>
-                                <th>मुख्य कार्य</th>
-                                <th>कार्य</th>
-                                <th>कैफियत</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @forelse($dailyTasks as $task)
-                            <tr>
-                                <td>{{$loop->iteration}}</td>
-                                <td>{{$task->date}}</td>
-                                <td>{{$task->taskDivision->taskCategory->title??''}}</td>
-                                <td>{{$task->taskDivision->title??''}}</td>
-                                <td>{{$task->remarks}}</td>
-                            </tr>
-                            @empty
-                            <tr>
-                                <td colspan="5" class="text-center">तालिकामा कुनै डाटा उपलब्ध छैन !!!</td>
-                            </tr>
-                            @endforelse
-                            </tbody>
-                        </table>
+                        <div id="printData">
+                            <table class="table table-sm table-bordered">
+                                <thead>
+                                <tr>
+                                    <th>क्र.स</th>
+                                    <th>मिति</th>
+                                    <th>मुख्य कार्य</th>
+                                    <th>कार्य</th>
+                                    <th>कैफियत</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @forelse($dailyTasks as $task)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>{{$task->date}}</td>
+                                        <td>{{$task->taskDivision->taskCategory->title??''}}</td>
+                                        <td>{{$task->taskDivision->title??''}}</td>
+                                        <td>{{$task->remarks}}</td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5" class="text-center">तालिकामा कुनै डाटा उपलब्ध छैन !!!</td>
+                                    </tr>
+                                @endforelse
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
