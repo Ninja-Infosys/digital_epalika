89868738f53c8ada11c1ca1caca3d0c6dec2cc1d
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2022-11-09 11:57:55 +0545

    small changes

--- resources/views/admin/setting/layouts/sidebar.blade.php
+++ resources/views/admin/setting/layouts/sidebar.blade.php
@@ -4,6 +4,68 @@
         <span> गृहपृष्ठ </span>
     </a>
 </li>
+
+@can('fiscalYear_access')
+    <li class="{{request()->is('admin/setting/fiscalYear*') ? 'active' : ''}}">
+        <a href="{{route('admin.fiscalYear.index')}}">
+            <i class="fa fa-calendar"></i>
+            <span> आर्थिक बर्ष </span>
+        </a>
+    </li>
+@endcan
+@can('ethnicity_access')
+    <li class="{{request()->is('admin/setting/fiscalYear/*') ? 'active' : ''}}">
+        <a href="{{route('admin.ethnicity.index')}}">
+            <i class="fa fa-user"></i>
+            <span> जातियता </span>
+        </a>
+    </li>
+@endcan
+
+@can('branch_access')
+    <li class="{{request()->is('admin/setting/branch*') ? 'active' : ''}}">
+        <a href="{{route('admin.branch.index')}}">
+            <i class="fa fa-code-branch"></i>
+            <span> शाखाहरु थप्नुहोस् </span>
+        </a>
+    </li>
+@endcan
+<li class="{{request()->is('admin/setting/officeSetting*') ? 'active' : ''}}">
+    <a href="{{route('admin.officeSetting.index')}}">
+        <i class="fa fa-cogs"></i>
+        <span> कार्यालय सेटिङ </span>
+    </a>
+</li>
+<li class="{{request()->is('admin/setting/designation*') || request()->is('admin/setting/department*') ? 'active' : ''}}">
+    <a href="#designationDepartment"
+       {{request()->is('admin/setting/designation*') || request()->is('admin/setting/department*') ? 'aria-expanded=true' : ''}}
+       data-bs-toggle="collapse">
+        <i class="fa fa-list"></i>
+        <span>पद/विभाग</span>
+        <span class="menu-arrow">
+                            <i class="fas fa-angle-right"></i>
+                        </span>
+    </a>
+    <div class="collapse {{request()->is('admin/setting/designation*') || request()->is('admin/setting/department*') ? 'show' : ''}}"
+         id="designationDepartment">
+        <ul class="nav-second-level">
+            @can('designation_access')
+                <li class="{{request()->is('admin/setting/designation*') ? 'active' : ''}}">
+                    <a href="{{route('admin.designation.index')}}">
+                        <span> पद थप्नुहोस् </span>
+                    </a>
+                </li>
+            @endcan
+            @can('department_access')
+                <li class="{{request()->is('admin/setting/department*') ? 'active' : ''}}">
+                    <a href="{{route('admin.department.index')}}">
+                        <span> विभाग थप्नुहोस् </span>
+                    </a>
+                </li>
+            @endcan
+        </ul>
+    </div>
+</li>
 <li class="{{request()->is('admin/setting/userManagement/*') ? 'active' : ''}}">
     <a href="#userManagement"
        {{request()->is('admin/setting/userManagement/*') ? 'aria-expanded=true  ' : ''}}
@@ -30,24 +92,6 @@
         </ul>
     </div>
 </li>
-
-@can('fiscalYear_access')
-    <li class="{{request()->is('admin/setting/fiscalYear*') ? 'active' : ''}}">
-        <a href="{{route('admin.fiscalYear.index')}}">
-            <i class="fa fa-calendar"></i>
-            <span> आर्थिक बर्ष </span>
-        </a>
-    </li>
-@endcan
-@can('ethnicity_access')
-    <li class="{{request()->is('admin/setting/fiscalYear/*') ? 'active' : ''}}">
-        <a href="{{route('admin.ethnicity.index')}}">
-            <i class="fa fa-user"></i>
-            <span> जातियता </span>
-        </a>
-    </li>
-@endcan
-
 <li class="{{request()->is('admin/setting/units/*') ? 'active' : ''}}">
     <a href="#measurementUnits"
        {{request()->is('admin/setting/units/*') ? 'aria-expanded=true  ' : ''}}
@@ -85,51 +129,3 @@
         </ul>
     </div>
 </li>
-
-@can('branch_access')
-    <li class="{{request()->is('admin/setting/branch*') ? 'active' : ''}}">
-        <a href="{{route('admin.branch.index')}}">
-            <i class="fa fa-code-branch"></i>
-            <span> शाखा </span>
-        </a>
-    </li>
-@endcan
-
-<li class="{{request()->is('admin/setting/designation*') || request()->is('admin/setting/department*') ? 'active' : ''}}">
-    <a href="#designationDepartment"
-       {{request()->is('admin/setting/designation*') || request()->is('admin/setting/department*') ? 'aria-expanded=true' : ''}}
-       data-bs-toggle="collapse">
-        <i class="fa fa-list"></i>
-        <span>पद/विभाग</span>
-        <span class="menu-arrow">
-                            <i class="fas fa-angle-right"></i>
-                        </span>
-    </a>
-    <div class="collapse {{request()->is('admin/setting/designation*') || request()->is('admin/setting/department*') ? 'show' : ''}}"
-         id="designationDepartment">
-        <ul class="nav-second-level">
-            @can('designation_access')
-                <li class="{{request()->is('admin/setting/designation*') ? 'active' : ''}}">
-                    <a href="{{route('admin.designation.index')}}">
-                        <span> पद थप्नुहोस् </span>
-                    </a>
-                </li>
-            @endcan
-            @can('department_access')
-                <li class="{{request()->is('admin/setting/department*') ? 'active' : ''}}">
-                    <a href="{{route('admin.department.index')}}">
-                        <span> विभाग थप्नुहोस् </span>
-                    </a>
-                </li>
-            @endcan
-        </ul>
-    </div>
-</li>
-
-<li class="{{request()->is('admin/setting/officeSetting*') ? 'active' : ''}}">
-    <a href="{{route('admin.officeSetting.index')}}">
-        <i class="fa fa-cogs"></i>
-        <span> कार्यालय सेटिङ </span>
-    </a>
-</li>
-
