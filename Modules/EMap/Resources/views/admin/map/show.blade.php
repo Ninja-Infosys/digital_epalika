6112681dc11763c29e7546f6000e635a9e52ff17
Ninja <bijaygamuwatharu@gmail.com>
2022-11-11 15:22:39 +0545

    emap alert

--- Modules/EMap/Resources/views/admin/map/show.blade.php
+++ Modules/EMap/Resources/views/admin/map/show.blade.php
@@ -75,11 +75,10 @@
                         @foreach(\Modules\EMap\Enums\NoticeTypeEnum::cases() as $noticeType)
                             <section id="{{\Illuminate\Support\Str::limit($noticeType->value,10,'mmm')}}">
                                 <h4 class="mt-2"> {{$noticeType->label()}}</h4>
-                                <div class="card-body mt-2 border_yellow">
-                                    <div class="">
-                                    <i class="fa fa-exclamation-triangle map_template_exclamation fs-3" data-bs-toggle="tooltip" data-bs-placement="right" title="शिर्षक कार्य"></i>
-                                    </div>
-                                    <div class="d-flex justify-content-end">
+                                <div class=" border_yellow p-1">
+                                    <i class="fa fa-exclamation-triangle map_template_exclamation "></i>
+
+                                    <div class="d-flex justify-content-end ">
                                         @if($mapApply->applyMapNotices->pluck('file_type')->unique()->contains($noticeType))
                                         <a href="{{route('emap.admin.map.map-apply.notice.upload.get-template-data',[$mapApply,$noticeType->value])}}" class="mx-2">
                                             <i class="fa fa-pen"></i>
