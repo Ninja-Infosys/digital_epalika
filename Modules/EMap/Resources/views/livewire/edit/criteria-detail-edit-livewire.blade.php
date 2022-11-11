eb56b676f15cccb57fe7f25cb18faa60afe31421
Tekchan Khatri <tekam7755@gmail.com>
2022-11-11 17:21:43 +0545

    Emap Module

--- Modules/EMap/Resources/views/livewire/edit/criteria-detail-edit-livewire.blade.php
+++ Modules/EMap/Resources/views/livewire/edit/criteria-detail-edit-livewire.blade.php
@@ -9,107 +9,110 @@
                 <div class="row">
                     <div class="col-md-12">
                         <b>मापदण्ड सम्बन्धि विवरण :</b>
-                        <table
-                            class="table table-hover table-responsive table-bordered">
-                            <thead>
-                            <tr>
-                                <th>क्र.सं</th>
-                                <th>विवरण</th>
-                                <th>मापदण्ड अनुसार</th>
-                                <th>नक्सा अनुसार</th>
-                                <th>अनुपालन</th>
-                                <th>कैफियत</th>
-                                <th>#</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($criteriaDetails as $key=>$criteriaDetail)
+                        <div class="table-responsive">
+                            <table
+                                class="table table-hover table-responsive table-bordered">
+                                <thead>
                                 <tr>
-                                    <td>{{$loop->iteration}}</td>
-                                    <td>
-                                        <label
-                                            for="name">
-                                            {{\Modules\EMap\Enums\DetailsRegardingCriteriaEnum::tryFrom($criteriaDetail['detail'])->label()}}
-                                        </label>
-                                        <input type="hidden"
-                                               id="criteriaDetails.{{$key}}.detail"
-                                               wire:model="criteriaDetails.{{$key}}.detail"
-                                            {{$dataToEdit !== $key ?'disabled':''}}
-                                        >
-                                        @error("criteriaDetails.$key.detail")
-                                        <p class="text-danger">{{$message}}</p>
-                                        @enderror
-                                    </td>
-                                    <td>
-                                        <input type="text"
-                                               wire:model="criteriaDetails.{{$key}}.according_to_criteria"
-                                            {{$dataToEdit !== $key ?'disabled':''}}
-                                        >
-                                        @error("criteriaDetails.$key.according_to_criteria")
-                                        <p class="text-danger">{{$message}}</p>
-                                        @enderror
-                                    </td>
-                                    <td>
-                                        <input type="text"
-                                               wire:model="criteriaDetails.{{$key}}.according_to_map"
-                                            {{$dataToEdit !== $key ?'disabled':''}}
-                                        >
-                                        @error("criteriaDetails.$key.according_to_map")
-                                        <p class="text-danger">{{$message}}</p>
-                                        @enderror
-                                    </td>
-                                    <td>
-                                        <input type="text"
-                                               wire:model="criteriaDetails.{{$key}}.compliance"
-                                            {{$dataToEdit !== $key ?'disabled':''}}
-                                        >
-                                        @error("criteriaDetails.$key.compliance")
-                                        <p class="text-danger">{{$message}}</p>
-                                        @enderror
-                                    </td>
+                                    <th>क्र.सं</th>
+                                    <th>विवरण</th>
+                                    <th>मापदण्ड अनुसार</th>
+                                    <th>नक्सा अनुसार</th>
+                                    <th>अनुपालन</th>
+                                    <th>कैफियत</th>
+                                    <th>#</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($criteriaDetails as $key=>$criteriaDetail)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>
+                                            <label
+                                                for="name">
+                                                {{\Modules\EMap\Enums\DetailsRegardingCriteriaEnum::tryFrom($criteriaDetail['detail'])->label()}}
+                                            </label>
+                                            <input type="hidden"
+                                                   id="criteriaDetails.{{$key}}.detail"
+                                                   wire:model="criteriaDetails.{{$key}}.detail"
+                                                {{$dataToEdit !== $key ?'disabled':''}}
+                                            >
+                                            @error("criteriaDetails.$key.detail")
+                                            <p class="text-danger">{{$message}}</p>
+                                            @enderror
+                                        </td>
+                                        <td>
+                                            <input type="text"
+                                                   wire:model="criteriaDetails.{{$key}}.according_to_criteria"
+                                                {{$dataToEdit !== $key ?'disabled':''}}
+                                            >
+                                            @error("criteriaDetails.$key.according_to_criteria")
+                                            <p class="text-danger">{{$message}}</p>
+                                            @enderror
+                                        </td>
+                                        <td>
+                                            <input type="text"
+                                                   wire:model="criteriaDetails.{{$key}}.according_to_map"
+                                                {{$dataToEdit !== $key ?'disabled':''}}
+                                            >
+                                            @error("criteriaDetails.$key.according_to_map")
+                                            <p class="text-danger">{{$message}}</p>
+                                            @enderror
+                                        </td>
+                                        <td>
+                                            <input type="text"
+                                                   wire:model="criteriaDetails.{{$key}}.compliance"
+                                                {{$dataToEdit !== $key ?'disabled':''}}
+                                            >
+                                            @error("criteriaDetails.$key.compliance")
+                                            <p class="text-danger">{{$message}}</p>
+                                            @enderror
+                                        </td>
 
-                                    <td>
-                                        <input type="text"
-                                               wire:model="criteriaDetails.{{$key}}.remarks"
-                                            {{$dataToEdit !== $key ?'disabled':''}}
-                                        >
-                                        @error("criteriaDetails.$key.remarks")
-                                        <p class="text-danger">{{$message}}</p>
-                                        @enderror
-                                    </td>
-                                    <td>
-                                        @if($dataToEdit === null)
-                                            <div class="d-flex">
-                                                <button type="button" class="edit btn btn-primary"
-                                                        wire:click.prevent="setDataForEdit({{$key}})"><i
-                                                        class="fa fa-pen"></i>
-                                                </button>
-                                            </div>
-                                        @else
-
-                                            @if($dataToEdit===$key)
+                                        <td>
+                                            <input type="text"
+                                                   wire:model="criteriaDetails.{{$key}}.remarks"
+                                                {{$dataToEdit !== $key ?'disabled':''}}
+                                            >
+                                            @error("criteriaDetails.$key.remarks")
+                                            <p class="text-danger">{{$message}}</p>
+                                            @enderror
+                                        </td>
+                                        <td>
+                                            @if($dataToEdit === null)
                                                 <div class="d-flex">
-                                                    <button type="button" class="edit btn btn-primary mx-1"
-                                                            wire:click.prevent="saveFormData"><i
-                                                            class="fa fa-save"></i></button>
-                                                    <button type="button" class="edit btn btn-danger mx-1"
-                                                            wire:click.prevent="setDataForEdit()"><i
-                                                            class="fa fa-trash"></i>
+                                                    <button type="button" class="edit btn btn-primary"
+                                                            wire:click.prevent="setDataForEdit({{$key}})"><i
+                                                            class="fa fa-pen"></i>
                                                     </button>
                                                 </div>
+                                            @else
+
+                                                @if($dataToEdit===$key)
+                                                    <div class="d-flex">
+                                                        <button type="button" class="edit btn btn-primary mx-1"
+                                                                wire:click.prevent="saveFormData"><i
+                                                                class="fa fa-save"></i></button>
+                                                        <button type="button" class="edit btn btn-danger mx-1"
+                                                                wire:click.prevent="setDataForEdit()"><i
+                                                                class="fa fa-trash"></i>
+                                                        </button>
+                                                    </div>
+                                                @endif
+
                                             @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                @error("criteriaDetails")
+                                <p class="text-danger">{{$message}}</p>
+                                @enderror
 
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
-                            @error("criteriaDetails")
-                            <p class="text-danger">{{$message}}</p>
-                            @enderror
+                                </tbody>
 
-                            </tbody>
+                            </table>
+                        </div>
 
-                        </table>
                     </div>
                 </div>
             </div>
