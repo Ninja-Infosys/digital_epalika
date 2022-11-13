70c615332a0ecee306669229472d10fde46cc46a
Ninja <bijaygamuwatharu@gmail.com>
2022-11-13 16:00:57 +0545

    emap notice clear top and bottom scripts

--- Modules/EMap/Resources/views/admin/notice/revised_superstructure_permit_order.blade.php
+++ Modules/EMap/Resources/views/admin/notice/revised_superstructure_permit_order.blade.php
@@ -1,4 +1,49 @@
-
+@extends('admin.layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item">
+                            <a href="{{route('admin.dashboard')}}">
+                                <i class="fa fa-home"></i> गृहपृष्ठ
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="{{route('admin.circular.registration.index')}}">ई-नक्सा </a>
+                        </li>
+                        <li class="breadcrumb-item active">नक्सा विवरण</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">ई-नक्सा</h4>
+            </div>
+        </div>
+    </div>
+            <div>
+                @error('file')
+                <div class="alert alert-danger">
+                    {{$message}}
+                </div>
+                @enderror
+            </div>
+    <div class="row mb-2">
+        <div class="col-sm-4">
+            <h4> {{\Modules\EMap\Enums\NoticeTypeEnum::REVISED_SUPERSTRUCTURE_PERMIT_ORDER->label()}}</h4>
+        </div>
+        <div class="col-sm-8">
+            <div class="text-sm-end">
+                <div class="btn-group mb-3">
+                    <x-application-component
+                        :application-type="\Modules\EMap\Enums\NoticeTypeEnum::REVISED_SUPERSTRUCTURE_PERMIT_ORDER"
+                        url="{{route('emap.admin.map.map-apply.notice.upload.order',$mapApply)}}"/>
+                </div>
+                <div class="btn-group mb-3">
+                    <x-print-button title="{{$mapApply->client->name}}संशोधित सुपरस्ट्रक्चर ईजाजत सम्बन्धमा"/>
+                </div>
+            </div>
+        </div><!-- end col-->
+    </div>
             <div class="row justify-content-center">
                 <div class="col-lg-12">
                     <div class="card mb_30">
@@ -38,3 +83,24 @@
                     </div>
                 </div>
             </div>
+    @push('style')
+        <style>
+            .font-black p {
+                color: black;
+            }
+
+            .underline-dotted {
+                border-bottom: dotted 2px !important;
+                padding: 0 20px;
+            }
+
+            .custom-width {
+                padding: 0 50px !important;
+            }
+        </style>
+    @endpush
+    @push('scripts')
+        <script src="{{asset('assets/backend/js/printAjaxScript.js')}}"></script>
+    @endpush
+
+@endsection
