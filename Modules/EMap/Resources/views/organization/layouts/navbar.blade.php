e1137a33964f945a519eefe01f8e35cfbe5fba31
Ninja <bijaygamuwatharu@gmail.com>
2022-11-14 15:26:15 +0545

    organization: dashboard notification bell

--- Modules/EMap/Resources/views/organization/layouts/navbar.blade.php
+++ Modules/EMap/Resources/views/organization/layouts/navbar.blade.php
@@ -10,9 +10,86 @@
                 </div>
                 <div class="header_right d-flex justify-content-between align-items-center">
                     <div class="header_notification_warp d-flex align-items-center">
-                        <li>
-                            <a href="#"> <img src="{{asset('assets/backend/emap/admin/img/icon/bell.svg')}}" alt="">
+                        <li class="dropdown notification-list topbar-dropdown">
+                            <a class=""
+                               href="#"
+                               role="button"
+                               id="dropdownMenuLink"
+                               data-bs-toggle="dropdown"
+                               aria-expanded="false"
+                                >
+                                <i class="fa fa-bell ring-bell noti-icon fs-2 px-2"></i>
+                                <span class="badge bg-danger rounded-circle noti-icon-badge">
+                                        1
+                                    </span>
+
                             </a>
+                                <div class="dropdown-menu p-3" aria-labelledby="dropdownMenuLink">
+                                    <div class="dropdown-item d-flex justify-content-around">
+                                        <p class="m-0 text-dark px-5">नोटिफिकेसन</p>
+                                            <a href="#"  >
+                                                <p class="text-danger">सबै खाली गर्नुहोस्</p>
+                                            </a>
+                                    </div>
+                                    <hr class="p-0 m-0">
+                                    <ul data-bs-spy="scroll"  class="scrollspy-example p-0 m-0">
+                                        <li >
+                                                <a href="#" id="scrollspyHeading2">
+                                                    <p class="text-dark">
+                                                        <i class="fa fa-bell"></i>
+                                                        नोटिफिकेसन शिर्षक यहा नोटिफिकेसन शिर्षक यहा <br/>
+                                                        <small class="text-muted">२०७९-०७-२७</small>
+                                                    </p>
+                                                </a>
+                                        </li>
+                                        <li >
+                                                <a href="#" id="scrollspyHeading2">
+                                                    <p class="text-dark">
+                                                        <i class="fa fa-bell"></i>
+                                                        नोटिफिकेसन शिर्षक यहा <br/>
+                                                        <small class="text-muted">२०७९-०७-२७</small>
+                                                    </p>
+                                                </a>
+                                        </li>
+                                        <li >
+                                                <a href="#" id="scrollspyHeading2">
+                                                    <p class="text-dark">
+                                                        <i class="fa fa-bell"></i>
+                                                        नोटिफिकेसन शिर्षक यहा नोटिफिकेसन शिर्षक यहा<br/>
+                                                        <small class="text-muted">२०७९-०७-२७</small>
+                                                    </p>
+                                                </a>
+                                        </li>
+                                        <li >
+                                                <a href="#" id="scrollspyHeading2">
+                                                    <p class="text-dark">
+                                                        <i class="fa fa-bell"></i>
+                                                        नोटिफिकेसन शिर्षक यहा <br/>
+                                                        <small class="text-muted">२०७९-०७-२७</small>
+                                                    </p>
+                                                </a>
+                                        </li>
+                                        <li >
+                                                <a href="#" id="scrollspyHeading2">
+                                                    <p class="text-dark">
+                                                        <i class="fa fa-bell"></i>
+                                                        नोटिफिकेसन शिर्षक यहा <br/>
+                                                        <small class="text-muted">२०७९-०७-२७</small>
+                                                    </p>
+                                                </a>
+                                        </li>
+                                        <li >
+                                                <a href="#" id="scrollspyHeading2">
+                                                    <p class="text-dark">
+                                                        <i class="fa fa-bell"></i>
+                                                        नोटिफिकेसन शिर्षक यहा नोटिफिकेसन शिर्षक यहा<br/>
+                                                        <small class="text-muted">२०७९-०७-२७</small>
+                                                    </p>
+                                                </a>
+                                        </li>
+                                        <p class="text-center p-1">कुनै डाटा उपलब्ध छैन <i class="fa fa-exclamation ring-bell noti-icon text-danger px-2"></i></p>
+                                    </ul>
+                                </div>
                         </li>
                         <li>
                             <a href="#"> <img src="{{asset('assets/backend/emap/admin/img/icon/msg.svg')}}" alt=""> </a>
