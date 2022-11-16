68cfe61c491ea57decae7cbaf0e56aa01d18d7fc
sush1l <sushilkc188@gmail.com>
2022-11-16 17:09:41 +0545

    Recommendation forms are made

---
+++ Modules/Recommendation/Resources/views/admin/relation/birth_amendment.blade.php
@@ -0,0 +1,74 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb_30">
+                <div class="card-header p-3">
+                    <div class="main-title d-flex justify-content-between">
+                        <h3> </h3>
+                        <div class="d-flex justify-content-end">
+                            <button id="printButton" class="btn btn-sm btn-success" printElementId='printData'
+                                    requestRoute="{{route('print.application-print')}}">
+                                <i class="fa fa-print"></i> Print
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="card mb_30">
+                        <div class="card-body p-3">
+                            <div class="font-black" id="printData">
+                                <div class="image mt-3">
+                                    <img height="80" width="100" src="{{asset('images/np.png')}}">
+                                    <div class="text-center" style="margin-top: -80px;">
+                                        <h5><b>....................पालिका</b><br>
+                                            <b>वडा नं. ....................को कार्यालय</b><br>
+                                            <b>........(कार्यालय रहेको स्थान) ............(जिल्ला)</b>.<br>
+                                            <b>........................ प्रदेश, नेपाल</b>
+                                        </h5>
+                                    </div>
+                                </div>
+
+
+                                <p class="text-center my-3"><b>जन्म मिति संशोधन सिफारिस । (७) </b></p>
+                                <p><b>जो जससँग सम्बन्धित छ ।</b></p>
+                                <p class="my-3">...............गाउँ/नगरपालिका ............... वडा नं. ..............को ................. मा बस्ने श्री ................. ले
+                                    जन्म मिति संशोधन गरिदिन सिफारिस गरिदिन दिनु भएको निवेदन अनुसार श्री ................. को नाति श्री.................को छोरा/छोरि श्री ............... को
+                                    जन्म मिति ................. कारणले संशोधन गर्नुपर्ने भएकोले जन्म दर्ता/शौक्षिक प्रमाणपत्र/नेपाली नागरिकता प्रमाण बमोजिम हुने गरी एउटै कायम गर्न देहाय बमोजिम हुने गरी नीजको
+                                    जन्म मिति संशोधन गरिदिनुहुन् स्थानीय सरकार संचालन ऐन, २०७४ को दफा १२(२)ङ(७) बमोजिम सिफारिस गरिन्छ ।</p>
+                                <p>देहाय<br>
+                                <table class="table table-bordered">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">फरक भएको जन्म मिति </th>
+                                        <th scope="col">कायम गर्नुपर्ने जन्म मिति</th>
+                                        <th scope="col">कायम गर्ने आधार</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    </tbody>
+                                </table></p>
+                                <div class="d-flex justify-content-end mt-2">
+                                    <p class="text-center">हस्ताक्षर ........................<br>(वडा अध्क्षय) </p>
+                                </div>
+
+                                <p class="mt-5">नोट : जन्म मिति संशोधन सिफारिसका लागि आवश्यकतानुसार सर्जमिन मुचुल्का, प्रहरी प्रतिवेदन समेत राख्नुपर्नेछ ।</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @push('scripts')
+        <script src="{{asset('assets/backend/js/printAjaxScript.js')}}"></script>
+    @endpush
+
+@endsection
--- Modules/Recommendation/Routes/admin.php
+++ Modules/Recommendation/Routes/admin.php
@@ -1,5 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Modules\Recommendation\Http\Controllers\Admin\DashboardController;
 
 Route::get('dashboard', DashboardController::class)->name('dashboard');
+Route::view('relation_identify','recommendation::admin.relation.relation_identify')->name('relation_identify');
+Route::view('citizenship_recommendation','recommendation::admin.relation.citizenship_recommendation')->name('citizenship_recommendation');
+Route::view('restored_tax','recommendation::admin.relation.restored_tax')->name('restored_tax');
+Route::view('residental_idenrtification','recommendation::admin.relation.residental_idenrtification')->name('residental_idenrtification');
+Route::view('weapon','recommendation::admin.relation.weapon')->name('weapon');
+Route::view('home_land_tax','recommendation::admin.relation.home_land_tax')->name('home_land_tax');
+Route::view('birth_date','recommendation::admin.relation.birth_date')->name('birth_date');
+Route::view('birth_amendment','recommendation::admin.relation.birth_amendment')->name('birth_amendment');
+Route::view('business_closed','recommendation::admin.relation.business_closed')->name('business_closed');
+Route::view('business_operation','recommendation::admin.relation.business_operation')->name('business_operation');
+
+
+
+
