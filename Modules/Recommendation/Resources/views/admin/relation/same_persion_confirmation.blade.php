c5273ec073ae2a461c59ee47d1a162375d4b6968
Ninja <bijaygamuwatharu@gmail.com>
2022-11-22 16:29:08 +0545

    relation form recommendation

---
+++ Modules/Recommendation/Resources/views/admin/relation/same_persion_confirmation.blade.php
@@ -0,0 +1,83 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb_30">
+                <div class="card-header p-3">
+                    <div class="main-title d-flex justify-content-between">
+                        <h3></h3>
+                        <div class="d-flex justify-content-end">
+                            <button id="printButton" class="btn btn-sm btn-success" printElementId='printData'
+                                    requestRoute="{{route('print.application-print')}}">
+                                <i class="fa fa-print"></i> Print
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="card mb_30">
+                        <div class="card-body p-3">
+                            <div class="font-black" id="printData">
+                                <div class="image mt-3">
+                                    <img height="80" width="100" src="{{asset('images/np.png')}}">
+                                    <div class="text-center" style="margin-top: -80px;">
+                                        <h5>
+                                            <b>....................पालिका</b><br>
+                                            <b>वडा नं. ....................को कार्यालय</b><br>
+                                            <b>........(कार्यालय रहेको स्थान) ............(जिल्ला)</b>.<br>
+                                            <b>........................ प्रदेश, नेपाल</b>
+                                        </h5>
+                                    </div>
+                                </div>
+                                <p class="text-center pt-4"><b>विषय : कुनै व्यत्तिको नाम थर जन्ममिति तथा वतन फरक फरक भएको भए
+                                        सो व्यक्ति एउतै हो भन्ने सिफारिस। (१५) </b></p>
+                                <p >जो जससंग सम्बन्धित छ ।</p>
+
+                                <P>
+                                    ............................ गाउँ/नगरपालिका वडा नं. ............को ........................................
+                                    मा निवासी श्री ........................................ को नाती/नातिनी श्री .......................................
+                                    को छोरा/छोरी श्री.......................... को तपसिलमा उल्लेखित विवरण अनुसारको कागजातमा नाम, थर/जन्ममिति/वतन फरक फरक हुन गएको हुनाले सो
+                                     फरक हुन् गएको नाम, थर/जन्ममिति/हुन् गएको व्यक्ति एकै भएको सिफारिस पाउं भनि यस कार्यलयमा निवेदन दिनुभएकोमा मिति...............................
+                                    मा गरिएको स्थानीय/प्रहरी सर्जमिन मुचुल्का समेतको आधारमा सो व्यहोरा मनासिव भएको देखिएकोले सो फरक फरक नाम, थर/जन्म मिति/वतन भएको व्यक्ति एकै भएको
+                                    व्यहोरा स्थानीय सरकार संचालन ऐन. २०७४ को दफा १२(२) ङ (१४) बमोजिम सिफारिस साथ अनुरोध गरिन्छ ।
+                                </P>
+                                <h4 class="text-center py-2"><b>फरक नाम, थर/जन्ममिति र कागजातको विवरण</b></h4>
+
+                                <table class="table table-bordered my-2">
+                                    <thead>
+                                    <tr>
+                                        <th >फरक भएको कागजात</th>
+                                        <th >फरक भएको नाम, थर/जन्म मिति/वतन</th>
+                                        <th >हुनु पर्ने नाम, थर/जन्ममिति/वतन</th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                <div class="d-flex justify-content-end mt-2">
+                                    <p class="text-center"> हतक्षर्........................
+                                        <br>
+                                        (वडा अध्यक्ष) </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @push('scripts')
+        <script src="{{asset('assets/backend/js/printAjaxScript.js')}}"></script>
+    @endpush
+
+@endsection
+
+
