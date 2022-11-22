113a395639f4a85826f56558ecafc7fa9b915486
Yekdev Rokaya <yekdevrc@gmail.com>
2022-11-22 16:28:26 +0545

    updates

---
+++ app/Http/Requests/BudgetSource/StoreBudgetSourceRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\BudgetSource;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBudgetSourceRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'source_name' => ['required']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'source_name.required' => 'स्रोत आवश्यक छ'
+        ];
+    }
+}
---
+++ app/Http/Requests/PlanLevel/StoreBudgetHeadRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\PlanLevel;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreBudgetHeadRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'budget_head_id' => ['nullable', Rule::exists('budget_heads', 'id')->withoutTrashed()],
+            'title' => ['required']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'शीर्षक आवश्यक छ'
+        ];
+    }
+}
