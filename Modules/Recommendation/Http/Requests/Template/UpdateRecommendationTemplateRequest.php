48746ebbc56786a2c54314a1ba6dc0143026f083
sush1l <sushilkc188@gmail.com>
2022-11-27 15:43:20 +0545

    recommendation template

--- Modules/Recommendation/Http/Requests/Template/UpdateRecommendationTemplateRequest.php
+++ Modules/Recommendation/Http/Requests/Template/UpdateRecommendationTemplateRequest.php
@@ -3,6 +3,8 @@
 namespace Modules\Recommendation\Http\Requests\Template;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use Modules\Recommendation\Enums\ApplicationTypeEnum;
 
 class UpdateRecommendationTemplateRequest extends FormRequest
 {
@@ -14,7 +16,10 @@ class UpdateRecommendationTemplateRequest extends FormRequest
     public function rules():array
     {
         return [
-            //
+            'title' => ['required'],
+            'for' => ['required', new Enum(ApplicationTypeEnum::class)],
+            'data' => ['required'],
+            'requires_header' => ['nullable', 'boolean'],
         ];
     }
 }
