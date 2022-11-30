3660675f409a5d97113aecf22d45d38e5c67f43c
manoj <manojoli667@gmail.com>
2022-11-30 17:04:46 +0545

    address optimize

--- Modules/Plan/Http/Livewire/InstallmentDetailLivewire.php
+++ Modules/Plan/Http/Livewire/InstallmentDetailLivewire.php
@@ -2,30 +2,90 @@
 
 namespace Modules\Plan\Http\Livewire;
 
+use Illuminate\Validation\Rules\Enum;
 use Livewire\Component;
+use Modules\Plan\Entities\Project;
+use Modules\Plan\Entities\ProjectInstallmentDetail;
+use Modules\Plan\Enums\InstallmentTypeEnum;
 
 class InstallmentDetailLivewire extends Component
 {
-    public $installmentDetails=[];
+    public bool $createModalOpened = false;
+    public object $project;
 
-    public bool $createModalOpened=false;
-
-    public array $form=[
-        'installment_type'=>null,
-        'date'=>null,
-        'amount'=>null,
-        'construction_material_quantity'=>null,
-        'remarks'=>null
+    public array $form = [
+        'installment_type' => null,
+        'date' => null,
+        'amount' => null,
+        'construction_material_quantity' => null,
+        'remarks' => null
     ];
+
+    protected $listeners = ['dateChanged'];
+
+    public function mount($project_id)
+    {
+        $this->getProjectData($project_id);
+    }
+
+    public function dateChanged($nepaliDate, $englishDate)
+    {
+        $this->form['date'] = $nepaliDate;
+    }
 
     public function openCreateModal()
     {
-        $this->createModalOpened=true;
+        $this->createModalOpened = true;
     }
 
     public function closeModal()
     {
-        $this->createModalOpened=false;
+        $this->reset('form');
+        $this->createModalOpened = false;
+    }
+
+    public function getProjectData($project_id)
+    {
+        $this->project=Project::with('projectInstallmentDetails')->find($project_id);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'form.installment_type' => ['required', new Enum(InstallmentTypeEnum::class)],
+            'form.date' => ['nullable'],
+            'form.amount' => ['required', 'numeric'],
+            'form.construction_material_quantity' => ['nullable'],
+            'form.remarks' => ['nullable']
+        ];
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submitFormData()
+    {
+        $this->project->projectInstallmentDetails()->create($this->validate()['form']);
+        $this->closeModal();
+        $this->getProjectData($this->project->id);
+
+        $this->dispatchBrowserEvent('toast_message', [
+            'type' => 'success',
+            'title' => 'किस्ता विवरण सफलतापूर्वक थपियो',
+        ]);
+    }
+
+    public function deleteInstallmentDetail(ProjectInstallmentDetail $projectInstallmentDetail)
+    {
+        $projectInstallmentDetail->delete();
+        $this->getProjectData($this->project->id);
+
+        $this->dispatchBrowserEvent('toast_message', [
+            'type' => 'success',
+            'title' => 'किस्ता विवरण सफलतापूर्वक मेटाइयो',
+        ]);
     }
 
     public function render()
--- Modules/Plan/Resources/views/livewire/installment-detail-livewire.blade.php
+++ Modules/Plan/Resources/views/livewire/installment-detail-livewire.blade.php
@@ -1,6 +1,7 @@
 <div class="card">
     <div class="card-header d-flex justify-content-between">
-        <h4 class="header-title">५. उपभोक्ता समिति समुदायमा अधारित संस्था गैरसरकारी संस्थाले प्राप्त गर्ने किस्ता विवरण:</h4>
+        <h4 class="header-title">५. उपभोक्ता समिति समुदायमा अधारित संस्था गैरसरकारी संस्थाले प्राप्त गर्ने किस्ता
+            विवरण:</h4>
         <button type="button" wire:click="openCreateModal" class="btn btn-xs btn-outline-primary">
             <i class="fa fa-plus-circle"> नयाँ थप्नुहोस्</i>
         </button>
@@ -19,10 +20,15 @@
                 </tr>
                 </thead>
                 <tbody>
-                @forelse($installmentDetails as $key=>$installmentDetail)
+                @forelse($project->projectInstallmentDetails as $key=>$installmentDetail)
                     <tr>
+                        <td>{{$installmentDetail->installment_type->label()}}</td>
+                        <td>{{$installmentDetail->date}}</td>
+                        <td>{{$installmentDetail->amount}}</td>
+                        <td>{{$installmentDetail->construction_material_quantity}}</td>
+                        <td>{{$installmentDetail->remarks}}</td>
                         <td>
-                            <button type="button" wire:click="removeProjectGrantDetails({{$key}})"
+                            <button type="button" wire:click="deleteInstallmentDetail({{$installmentDetail->id}})"
                                     class="btn btn-xs btn-outline-danger">
                                 <i class="fa fa-trash"></i>
                             </button>
@@ -38,20 +44,128 @@
                 </tbody>
             </table>
         </div>
-        @if($createModalOpened)
-            <div class="modal fade show" id="bs-example-modal-lg" tabindex="-1" aria-labelledby="myLargeModalLabel" aria-modal="true" role="dialog" style="display: block;backdrop-filter: brightness(50%);">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title" id="myLargeModalLabel">Large modal</h4>
-                            <button type="button" class="btn-close" wire:click="closeModal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
+        <div class="modal fade show {{!$createModalOpened ? 'd-none' : ''}}" id="bs-example-modal-lg" tabindex="-1"
+             aria-labelledby="myLargeModalLabel" aria-modal="true" role="dialog"
+             style="display: block;backdrop-filter: brightness(50%);">
+            <div class="modal-dialog modal-lg modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header bg-primary">
+                        <h4 class="modal-title text-light" id="myLargeModalLabel">
+                            किस्ताको विवरण उल्लेख गर्नुहोस्।
+                        </h4>
+                        <button type="button" class="btn-close" wire:click="closeModal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form wire:submit.prevent="submitFormData">
+                            <div class="row">
+                                <div class="col-md-6 mb-2">
+                                    <label for="installment_type" class="form-label">किस्ताको क्रम *</label>
+                                    <select
+                                        wire:model="form.installment_type"
+                                        id="installment_type"
+                                        class="form-select">
+                                        <option value="">--- छान्नुहोस् ---</option>
+                                        @foreach(\Modules\Plan\Enums\InstallmentTypeEnum::cases() as $installmentType)
+                                            <option
+                                                value="{{$installmentType->value}}">
+                                                {{$installmentType->label()}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    @error('form.installment_type')
+                                    <div class="invalid-feedback">{{$message}}</div>
+                                    @enderror
+                                </div>
+                                <div class="col-md-6 mb-2">
+                                    <label for="date" class="form-label"> मिति </label>
+                                    <input
+                                        type="text"
+                                        wire:model="form.date"
+                                        class="form-control @error('form.date') is-invalid @enderror"
+                                        id="date"
+                                        placeholder="मिति"
+                                    />
+                                    @error('form.date')
+                                    <div class="invalid-feedback">{{$message}}</div>
+                                    @enderror
+                                </div>
+                                <div class="col-md-6 mb-2">
+                                    <label for="amount" class="form-label"> किस्ताको रकम * </label>
+                                    <input
+                                        type="number"
+                                        wire:model="form.amount"
+                                        class="form-control @error('form.amount') is-invalid @enderror"
+                                        id="amount"
+                                        placeholder="किस्ताको रकम"
+                                    />
+                                    @error('form.amount')
+                                    <div class="invalid-feedback">{{$message}}</div>
+                                    @enderror
+                                </div>
+                                <div class="col-md-6 mb-2">
+                                    <label for="construction_material_quantity" class="form-label"> निर्माण समाग्री
+                                        परिमाण </label>
+                                    <input
+                                        type="text"
+                                        wire:model="form.construction_material_quantity"
+                                        class="form-control @error('form.construction_material_quantity') is-invalid @enderror"
+                                        id="construction_material_quantity"
+                                        placeholder="निर्माण समाग्री परिमाण"
+                                    />
+                                    @error('form.construction_material_quantity')
+                                    <div class="invalid-feedback">{{$message}}</div>
+                                    @enderror
+                                </div>
+                                <div class="col-md-12 mb-2">
+                                    <label for="remarks" class="form-label">कैफियत</label>
+                                    <textarea
+                                        wire:model="form.remarks"
+                                        id="remarks"
+                                        class="form-control @error('form.remarks') is-invalid @enderror"
+                                        placeholder="कैफियत"
+                                        cols="30" rows="2"></textarea>
+                                    @error('form.remarks')
+                                    <div class="invalid-feedback">{{$message}}</div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="text-end">
+                                <button type="button" wire:click="closeModal" class="btn btn-danger">
+                                    Close
+                                </button>
+                                <button type="submit" class="btn btn-primary">
+                                    Save
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        @endif
+        </div>
     </div>
 </div>
+
+@once
+    @push('scripts')
+        <script src="{{asset('assets/backend/js/nepali.datepicker.v3.7.min.js')}}"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $("#date").nepaliDatePicker({
+                    ndpYear: true,
+                    ndpMonth: true,
+                    container: '#bs-example-modal-lg',
+                    onChange: function () {
+                        let inputFieldDate = $("#date").val();
+                        let parsedDate = NepaliFunctions.ParseDate(inputFieldDate);
+                        let englishDate = NepaliFunctions.BS2AD(parsedDate.parsedDate)
+                        let formattedDate = NepaliFunctions.ConvertDateFormat(englishDate, "YYYY-MM-DD")
+                        //$("#en_from_date").val(formattedDate);
+
+                        Livewire.emit('dateChanged', inputFieldDate, formattedDate);
+                    }
+                });
+            });
+        </script>
+    @endpush
+@endonce
