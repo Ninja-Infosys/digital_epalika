bc822cd6ffec7cd637193874c9f15c3e5be9ae81
Nabin Chaudhary <cnabin222@gmail.com>
2022-12-02 17:08:34 +0545

    emergency number crud

--- resources/views/admin/setting/layouts/sidebar.blade.php
+++ resources/views/admin/setting/layouts/sidebar.blade.php
@@ -13,11 +13,21 @@
         </a>
     </li>
 @endcan
+
 @can('ethnicity_access')
     <li class="{{request()->is('admin/setting/fiscalYear/*') ? 'active' : ''}}">
         <a href="{{route('admin.ethnicity.index')}}">
             <i class="fa fa-user"></i>
             <span> जातियता </span>
+        </a>
+    </li>
+@endcan
+
+@can('emergencyNumber_access')
+    <li class="{{request()->is('admin/setting/fiscalYear/*') ? 'active' : ''}}">
+        <a href="{{route('admin.emergencyNumber.index')}}">
+            <i class="fa fa-user"></i>
+            <span> Emergency Number </span>
         </a>
     </li>
 @endcan
