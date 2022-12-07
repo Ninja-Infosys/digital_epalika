1af40e0cbb1d272c94ccad51dc02be90c91b41c5
Yekdev Rokaya <yekdevrc@gmail.com>
2022-12-07 14:09:12 +0545

    updates

--- Modules/EMap/Database/Migrations/2022_09_29_063649_create_designer_details_table.php
+++ Modules/EMap/Database/Migrations/2022_09_29_063649_create_designer_details_table.php
@@ -10,17 +10,17 @@ return new class () extends Migration {
         Schema::create('designer_details', function (Blueprint $table) {
             $table->id();
             $table->foreignId('map_apply_id')->constrained()->cascadeOnDelete();
-            $table->string('name');
-            $table->string('father_name')->nullable();
-            $table->string('grandfather_name')->nullable();
-            $table->string('phone')->nullable();
-            $table->text('address')->nullable();
-            $table->string('local_body')->nullable();
-            $table->integer('ward_no')->nullable();
-            $table->string('post')->nullable();
-            $table->string('nec_council_no')->nullable();
-            $table->string('local_body_registration_no')->nullable();
-            $table->string('consulting_firm_name')->nullable();
+            $table->string('name')->comment('नाम');
+            $table->string('father_name')->nullable()->comment('बुवाको नाम ');
+            $table->string('grandfather_name')->nullable()->comment('बाजेको नाम ');
+            $table->string('phone')->nullable()->comment('फोन');
+            $table->text('address')->nullable()->comment('ठेगाना ');
+            $table->string('local_body')->nullable()->comment('स्थानीय निकाय ');
+            $table->integer('ward_no')->nullable()->comment('वार्ड ');
+            $table->string('post')->nullable()->comment('पोस्ट');
+            $table->string('nec_council_no')->nullable()->comment('nec काउन्सिल नं');
+            $table->string('local_body_registration_no')->nullable()->comment('स्थानीय निकाय दर्ता नं');
+            $table->string('consulting_firm_name')->nullable()->comment('परामर्श फर्म नाम');
             $table->timestamps();
             $table->softDeletes();
         });
--- Modules/EMap/Database/Migrations/2022_10_14_051439_create_map_registrations_table.php
+++ Modules/EMap/Database/Migrations/2022_10_14_051439_create_map_registrations_table.php
@@ -10,13 +10,13 @@ return new class () extends Migration {
         Schema::create('map_registrations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('map_apply_id')->constrained()->cascadeOnDelete();
-            $table->string('form_receipt')->nullable();
-            $table->string('application_registration_fee')->nullable();
-            $table->string('other')->nullable();
-            $table->string('nepali_date')->nullable();
-            $table->string('english_date')->nullable();
-            $table->string('receipt_no')->nullable();
-            $table->string('recipient')->nullable();
+            $table->string('form_receipt')->nullable()->comment('फारम रसिद');
+            $table->string('application_registration_fee')->nullable()->comment('आवेदन दर्ता फारम');
+            $table->string('other')->nullable()->comment('अन्य ');
+            $table->string('nepali_date')->nullable()->comment('नेपाली मिति ');
+            $table->string('english_date')->nullable()->comment('अंग्रजी मिति ');
+            $table->string('receipt_no')->nullable()->comment('रसिद नम्बर');
+            $table->string('recipient')->nullable()->comment('प्राप्तकर्ता');
             $table->timestamps();
             $table->softDeletes();
         });
