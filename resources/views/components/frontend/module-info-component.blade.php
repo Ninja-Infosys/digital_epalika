d4ae70b63fa916eb8fc1fbfcf3bc7ca8885aa9be
sush1l <sushilkc188@gmail.com>
2022-12-07 12:12:04 +0545

    ui made

--- resources/views/components/frontend/module-info-component.blade.php
+++ resources/views/components/frontend/module-info-component.blade.php
@@ -2,9 +2,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="{{route('grievanceHandling.grievance')}}">
-                <div class="p-2 text-center">
-                    <img src="{{asset('assets/backend/images/modules/grievancehandling.png')}}" width="50" height="50">
-                    <h4 class="p-1">गुनासो</h4>
+                <div class="py-4 text-center">
+                    <img src="{{asset('assets/frontend/image/gunaso.png')}}" width="50" height="50">
+                    <h4 class="p-2">गुनासो</h4>
                 </div>
             </a>
         </div>
@@ -12,9 +12,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="{{route('e-map')}}">
-                <div class="p-2 text-center">
+                <div class="py-4 text-center">
                     <img src="{{asset('assets/backend/images/modules/emap.png')}}" width="50" height="50">
-                    <h4 class="p-1">घर-नक्सा</h4>
+                    <h4 class="p-2">घर-नक्सा</h4>
                 </div>
             </a>
         </div>
@@ -22,9 +22,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="{{route('helpdesk.helpdesk')}}">
-                <div class="p-2 text-center">
-                    <img src="{{asset('assets/backend/images/modules/helpdesk.png')}}" width="50" height="50">
-                    <h4 class="p-1">हेल्प डेस्क</h4>
+                <div class="py-4 text-center">
+                    <img src="{{asset('assets/frontend/image/help-desk.png')}}" width="50" height="50">
+                    <h4 class="p-2">हेल्प डेस्क</h4>
                 </div>
             </a>
         </div>
@@ -32,9 +32,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="#">
-                <div class="p-2 text-center">
-                    <img src="{{asset('assets/backend/images/modules/sifarish-parnali.png')}}" width="50" height="50">
-                    <h4 class="p-1">सिफारिस</h4>
+                <div class="py-4 text-center">
+                    <img src="{{asset('assets/frontend/image/sifarish.png')}}" width="50" height="50">
+                    <h4 class="p-2">सिफारिस</h4>
                 </div>
             </a>
         </div>
@@ -42,9 +42,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="{{route('businessRegistration.business')}}">
-                <div class="p-2 text-center">
+                <div class="py-4 text-center">
                     <img src="{{asset('assets/backend/images/modules/businessregistration.png')}}" width="50" height="50">
-                    <h4>व्यवसाय दर्ता</h4>
+                    <h4 class="p-2">व्यवसाय दर्ता</h4>
                 </div>
             </a>
         </div>
@@ -52,9 +52,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="{{route('grant.index')}}">
-                <div class="p-2 text-center">
-                    <img src="{{asset('assets/backend/images/modules/anudan.png')}}" width="50" height="50">
-                    <h4>अनुदान</h4>
+                <div class="py-4 text-center">
+                    <img src="{{asset('assets/frontend/image/anudan.png')}}" width="50" height="50">
+                    <h4 class="p-2">अनुदान</h4>
                 </div>
             </a>
         </div>
@@ -62,9 +62,9 @@
     <div class="col-md-3">
         <div class="info-card disable_menu">
             <a href="#">
-                <div class="p-2 text-center">
-                    <img src="{{asset('assets/backend/images/modules/rajashow.png')}}" width="50" height="50">
-                    <h4>राजस्व</h4>
+                <div class="py-4 text-center">
+                    <img src="{{asset('assets/frontend/image/rajswa.png')}}" width="50" height="50">
+                    <h4 class="p-2">राजस्व</h4>
                 </div>
             </a>
         </div>
@@ -72,9 +72,9 @@
     <div class="col-md-3">
         <div class="info-card">
             <a href="{{route('roaster.index')}}">
-                <div class="p-2 text-center">
-                    <img src="{{asset('assets/backend/images/modules/roaster.png')}}" width="50" height="50">
-                    <h4>तालिम</h4>
+                <div class="py-4 text-center">
+                    <img src="{{asset('assets/frontend/image/talim.png')}}" width="50" height="50">
+                    <h4 class="p-2">तालिम</h4>
                 </div>
             </a>
         </div>
