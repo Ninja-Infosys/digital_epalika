c2567e77ad88d4fda5b32dcfc8b99b2008877697
Tekchan Khatri <tekam7755@gmail.com>
2022-12-11 15:45:33 +0545

    business Registration Report

--- Modules/BusinessRegistration/Resources/views/admin/businessRegistrationReport/index.blade.php
+++ Modules/BusinessRegistration/Resources/views/admin/businessRegistrationReport/index.blade.php
@@ -48,6 +48,32 @@
                                             :get-today-date="false"
                                         />
 
+                                </div>
+                                <div class="col-md-6 mb-2">
+                                    <x-date-input-component
+                                        nameNe="to_date" labelNe="सम्म"
+                                        nameEn="en_to_date" labelEn="To Date"
+                                        :get-today-date="false"
+                                    />
+                                </div>
+                            </div>
+                        </fieldset>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>आर्थिक बर्ष </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="fiscal_year">आर्थिक बर्ष</label>
+                                            <select name="fiscal_year[]" multiple data-toggle="select2"
+                                                    id="fiscal_year" class="form-control">
+                                                <option value="" disabled>--- छान्नुहोस् ---</option>
+                                                @foreach($fiscalYears as $fiscalYear)
+                                                    <option value="{{$fiscalYear->id}}">{{$fiscalYear->title}}</option>
+                                                @endforeach
+                                            </select>
                                     </div>
                                     <div class="col-md-6 mb-2">
                                         <x-date-input-component
@@ -75,6 +101,29 @@
                                                     @endforeach
                                                 </select>
 
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-4">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            व्यवसाय प्रकृति
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="business_nature">व्यवसाय प्रकृति अनुसार</label>
+                                            <select name="business_nature[]" multiple data-toggle="select2"
+                                                    id="business_nature"
+                                                    class="form-control">
+                                                <option value="" disabled> --व्यवसाय प्रकृति अनुसार--</option>
+                                                @foreach(\Modules\BusinessRegistration\Enums\BusinessNature::cases() as $businessNature)
+                                                    <option
+                                                        value="{{$businessNature->value}}">{{$businessNature->label()}}</option>
+                                                @endforeach
+                                            </select>
                                             </div>
                                         </div>
                                     </fieldset>
@@ -99,6 +148,29 @@
                                                     @endforeach
                                                 </select>
 
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-4">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            दर्ता र नविकरण
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="registration_renewal">दर्ता र नविकरण अनुसार</label>
+                                            <select name="registration_renewal[]" id="registration_renewal" multiple
+                                                    data-toggle="select2"
+                                                    class="form-control">
+                                                <option value="" disabled> --दर्ता र नविकरण अनुसार--</option>
+                                                @foreach(\Modules\BusinessRegistration\Enums\BusinessTypeEnum::cases() as $businessTypeEnum)
+                                                    <option
+                                                        value="{{$businessTypeEnum->value}}">{{$businessTypeEnum->label()}}</option>
+                                                @endforeach
+                                            </select>
                                             </div>
                                         </div>
                                     </fieldset>
@@ -123,6 +195,51 @@
                                                     @endforeach
                                                 </select>
 
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-4">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            उदेश्य
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="business_purpose">उदेश्य अनुसार</label>
+                                            <select name="business_purpose[]" multiple data-toggle="select2"
+                                                    id="business_purpose" class="form-control">
+                                                <option value="" disabled> --उदेश्य अनुसार--</option>
+                                                @foreach($businessPurposes as $businessPurpose)
+                                                    <option
+                                                        value="{{$businessPurpose->id}}">{{$businessPurpose->title}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-4">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            कारोबार वस्तु
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="object_transaction">कारोबार वस्तु अनुसार</label>
+                                            <select name="object_transaction[]"
+                                                    multiple data-toggle="select2"
+                                                    id="object_transaction" class="form-control">
+                                                <option value="" disabled> --कारोबार वस्तु--</option>
+                                                @foreach($objectTransactions as $objectTransaction)
+                                                    <option
+                                                        value="{{$objectTransaction->id}}">{{$objectTransaction->title}}</option>
+                                                @endforeach
+                                            </select>
                                             </div>
                                         </div>
                                     </fieldset>
@@ -169,6 +286,31 @@
                                                     @endforeach
                                                 </select>
 
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-4">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            पुँजीगत लगानी र राजस्वो
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="investment_revenue">पुँजीगत लगानी र राजस्वो</label>
+                                            <select name="investment_revenue[]" id="investment_revenue" multiple
+                                                    data-toggle="select2"
+                                                    class="form-control">
+                                                <option value="" disabled> --पुँजीगत लगानी र राजस्वो--</option>
+                                                @foreach($investmentRevenues as $investmentRevenue)
+                                                    <option
+                                                        value="{{$investmentRevenue->id}}">{{$investmentRevenue->title}}
+                                                        ({{$investmentRevenue->registration_amount}})
+                                                    </option>
+                                                @endforeach
+                                            </select>
                                             </div>
                                         </div>
                                     </fieldset>
@@ -214,29 +356,29 @@
                                                        id="investment.from" class="form-control" placeholder="लगानी अनुसार">
                                             </div>
 
-                                            <div class="col-md-6 mb-2">
-                                                <label for="investment.to">सम्म</label>
-                                                <input type="number" name="investment[to]"
-                                                       value="{{old('investment.to',$investmentData['to'])}}"
-                                                       id="investment.to" class="form-control" placeholder="लगानी अनुसार">
-                                            </div>
+                                        <div class="col-md-6 mb-2">
+                                            <label for="investment.to">सम्म</label>
+                                            <input type="number" name="investment[to]"
+                                                   value="{{old('investment.to',$investmentData['to'])}}"
+                                                   id="investment.to" class="form-control" placeholder="लगानी अनुसार">
                                         </div>
-                                    </fieldset>
-                                </div>
-                                <div class="col-md-6">
-                                    <fieldset class="border p-2 mb-2">
-                                        <legend class="font-16 text-info">
-                                            <strong>
-                                                रोजगार संख्या
-                                            </strong>
-                                        </legend>
-                                        <div class="row">
-                                            <div class="col-md-6 mb-2">
-                                                <label for="employment.from">देखि</label>
-                                                <input type="number" name="employment[from]"
-                                                       value="{{old('employment.from',$employmentData['from'])}}"
-                                                       id="employment.from" class="form-control" placeholder="लगानी अनुसार">
-                                            </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-6">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            रोजगार संख्या
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-6 mb-2">
+                                            <label for="employment.from">देखि</label>
+                                            <input type="number" name="employment[from]"
+                                                   value="{{old('employment.from',$employmentData['from'])}}"
+                                                   id="employment.from" class="form-control" placeholder="लगानी अनुसार">
+                                        </div>
 
                                             <div class="col-md-6 mb-2">
                                                 <label for="employment.to">सम्म</label>
@@ -289,6 +431,33 @@
                                                         <option value="{{$year}}">{{$year}}</option>
                                                     @endforeach
                                                 </select>
+                                        <div class="col-md-6 mb-2">
+                                            <label for="introBoard.to">सम्म</label>
+                                            <input type="number" name="introBoard[to]"
+                                                   value="{{old('introBoard.to',$introBoardData['to'])}}"
+                                                   id="introBoard.to" class="form-control" placeholder="लगानी अनुसार">
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="col-md-6">
+                                <fieldset class="border p-2 mb-2">
+                                    <legend class="font-16 text-info">
+                                        <strong>
+                                            व्यवसाय स्थापना साल
+                                        </strong>
+                                    </legend>
+                                    <div class="row">
+                                        <div class="col-md-12 mb-2">
+                                            <label for="business_year">साल</label>
+                                            <select name="business_year[]" id="business_year" multiple
+                                                    data-toggle="select2"
+                                                    class="form-control">
+                                                <option value="" disabled> --साल--</option>
+                                                @foreach($businessYears as $year)
+                                                    <option value="{{$year}}">{{$year}}</option>
+                                                @endforeach
+                                            </select>
 
                                             </div>
                                         </div>
@@ -297,6 +466,19 @@
                             </div>
 
 
+                        <fieldset class="border p-2 mb-2">
+                            <legend class="font-16 text-info">
+                                <strong>
+                                    Columns
+                                </strong>
+                            </legend>
+                            <div class="row">
+                                @foreach($columnData as $columns)
+                                    <div class="col-md-6 mb-2">
+                                        <label for="column.{{$columns['table_name']}}">{{$columns['name']}}</label>
+                                        <select name="columns[{{$columns['table_name']}}][]" id="column.{{$columns['table_name']}}" multiple data-toggle="select2"
+                                                class="form-control">
+                                            <option value="" disabled>--select Column--</option>
                             <fieldset class="border p-2 mb-2">
                                 <legend class="font-16 text-info">
                                     <strong>
