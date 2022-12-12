53b9d1a3b323ebff513b20fef05040d162551ed9
manoj <manojoli667@gmail.com>
2022-12-12 17:20:09 +0545

    yojana report

--- Modules/Plan/Http/Controllers/Admin/DashboardController.php
+++ Modules/Plan/Http/Controllers/Admin/DashboardController.php
@@ -5,11 +5,17 @@ namespace Modules\Plan\Http\Controllers\Admin;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Modules\Plan\Entities\Project;
+use Modules\Plan\Enums\ProjectStatusEnum;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('plan::admin.dashboard');
+        $not_started_project_count=Project::where('project_status',ProjectStatusEnum::NOT_STARTED)->count();
+        $in_progress_project_count=Project::where('project_status',ProjectStatusEnum::IN_PROGRESS)->count();
+        $completed_project_count=Project::where('project_status',ProjectStatusEnum::COMPLETED)->count();
+
+        return view('plan::admin.dashboard',compact('not_started_project_count','in_progress_project_count','completed_project_count'));
     }
 }
--- Modules/Plan/Resources/views/admin/dashboard.blade.php
+++ Modules/Plan/Resources/views/admin/dashboard.blade.php
@@ -6,19 +6,16 @@
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item">
-                            <a href="{{route('admin.judicialCommittee.dashboard')}}">
+                            <a href="{{route('admin.plan.dashboard')}}">
                                 <i class="fa fa-home"></i> गृहपृष्ठ
                             </a>
-                        </li>
-                        <li class="breadcrumb-item active">
-                            न्यायिक समिति
                         </li>
                     </ol>
                 </div>
                 <h4 class="page-title">गृहपृष्ठ </h4>
             </div>
             <div class="row">
-                <div class="col-md-6 col-xl-3">
+                <div class="col-md-6 col-xl-4">
                     <div class="widget-rounded-circle card">
                         <div class="card-body">
                             <div class="row">
@@ -30,17 +27,17 @@
                                 <div class="col-6">
                                     <div class="text-end">
                                         <h3 class="mt-1"><span data-plugin="counterup">
-                                        2
+                                        {{$not_started_project_count}}
                                     </span>
                                         </h3>
-                                        <p class="text-muted mb-1">जम्मा उजुरी</p>
+                                        <p class="text-muted mb-1">शुरु नभएका योजनाहरु </p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
                         </div>
                     </div> <!-- end widget-rounded-circle-->
                 </div> <!-- end col-->
-                <div class="col-md-6 col-xl-3">
+                <div class="col-md-6 col-xl-4">
                     <div class="widget-rounded-circle card">
                         <div class="card-body">
                             <div class="row">
@@ -52,17 +49,17 @@
                                 <div class="col-6">
                                     <div class="text-end">
                                         <h3 class="mt-1"><span data-plugin="counterup">
-                                         3
+                                         {{$in_progress_project_count}}
                                     </span>
                                         </h3>
-                                        <p class="text-muted mb-1">जम्मा दर्ता उजुरी</p>
+                                        <p class="text-muted mb-1">चालु योजनाहरु</p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
                         </div>
                     </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
-                <div class="col-md-6 col-xl-3">
+                </div>
+                <div class="col-md-6 col-xl-4">
                     <div class="widget-rounded-circle card">
                         <div class="card-body">
                             <div class="row">
@@ -74,35 +71,16 @@
                                 <div class="col-6">
                                     <div class="text-end">
                                         <h3 class="mt-1"><span data-plugin="counterup">
-                                         3
+                                         {{$completed_project_count}}
                                     </span>
                                         </h3>
-                                        <p class="text-muted mb-1">छिनोफानो भैसकेको</p>
+                                        <p class="text-muted mb-1">सम्पन्न योजनाहरू</p>
                                     </div>
                                 </div>
                             </div> <!-- end row-->
                         </div>
                     </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
-                <div class="col-md-6 col-xl-3">
-                    <div class="widget-rounded-circle card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="avatar-lg rounded-circle bg-blue border-blue border">
-                                        <i class="fa fa-scroll font-22 avatar-title text-white"></i>
-                                    </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="text-end">
-                                        <h3 class="mt-1"><span data-plugin="counterup">4</span></h3>
-                                        <p class="text-muted mb-1">नहेरीयको</p>
-                                    </div>
-                                </div>
-                            </div> <!-- end row-->
-                        </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
+                </div>
             </div>
         </div>
     </div>
