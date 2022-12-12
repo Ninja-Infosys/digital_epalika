6ce23a67f10c0cccf69b6a3f2ef65d71963c4973
shishir24rai <100944968+shishir24rai@users.noreply.github.com>
2022-12-12 13:27:19 +0545

    Clock and header dynamic

---
+++ resources/views/components/convert-to-unicode.blade.php
@@ -0,0 +1,15 @@
+<span id="unicode{{$id}}"></span>
+@once
+    @push('scripts')
+        <script src="{{asset('assets/backend/js/nepali.datepicker.v3.7.min.js')}}"></script>
+    @endpush
+@endonce
+@push('scripts')
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            $("#unicode{{$id}}").html(NepaliFunctions.ConvertToUnicode({{$number}}))
+        });
+    </script>
+@endpush
+
