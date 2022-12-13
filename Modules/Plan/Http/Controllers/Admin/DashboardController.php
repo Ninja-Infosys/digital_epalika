5d0dd3219018b09ff593990770ce94372ec37899
manoj <manojoli667@gmail.com>
2022-12-13 15:11:41 +0545

    yojana ward wise chart

--- Modules/Plan/Http/Controllers/Admin/DashboardController.php
+++ Modules/Plan/Http/Controllers/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Plan\Http\Controllers\Admin;
 
+use App\Models\Settings\OfficeSetting;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,10 +13,46 @@ class DashboardController extends Controller
 {
     public function __invoke()
     {
-        $not_started_project_count=Project::where('project_status',ProjectStatusEnum::NOT_STARTED)->count();
-        $in_progress_project_count=Project::where('project_status',ProjectStatusEnum::IN_PROGRESS)->count();
-        $completed_project_count=Project::where('project_status',ProjectStatusEnum::COMPLETED)->count();
+        $not_started_project_count = Project::where('project_status', ProjectStatusEnum::NOT_STARTED)->count();
+        $in_progress_project_count = Project::where('project_status', ProjectStatusEnum::IN_PROGRESS)->count();
+        $completed_project_count = Project::where('project_status', ProjectStatusEnum::COMPLETED)->count();
+        $wardWiseProjects=$this->getWardWiseProjects();
 
-        return view('plan::admin.dashboard',compact('not_started_project_count','in_progress_project_count','completed_project_count'));
+        return view('plan::admin.dashboard', compact(
+            'not_started_project_count',
+            'in_progress_project_count',
+            'completed_project_count',
+            'wardWiseProjects'
+        ));
+    }
+
+    private function getWardWiseProjects()
+    {
+        $wardsData=collect();
+
+        $projects=Project::where('fiscal_year_id',$this->getOfficeSetting()->fiscal_year_id)->get();
+
+        foreach ($this->getOfficeSetting()->localBody->ward_no as $ward){
+            $wardsData->push([
+                'ward_no'=>"वार्ड नं. $ward",
+                'projects_count'=>$projects->where('ward_no',$ward)->count()
+            ]);
+        }
+
+        return [
+            'labels' => $wardsData->pluck('ward_no')->toArray(),
+            'dataSets' => [
+                [
+                    'data' => $wardsData->pluck('projects_count')->toArray(),
+                    'label' => 'जम्मा',
+                    'fill' => 'false',
+                ],
+            ],
+        ];
+    }
+
+    private function getOfficeSetting()
+    {
+        return OfficeSetting::with('fiscalYear','localBody')->first();
     }
 }
