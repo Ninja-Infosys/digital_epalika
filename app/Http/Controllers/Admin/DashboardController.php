b8478d749274d51a423275a5cf2e8c5ae10b5bae
shishir24rai <100944968+shishir24rai@users.noreply.github.com>
2022-12-13 13:06:08 +0545

    Dashboard dynamic

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,35 +10,28 @@ use Modules\BusinessRegistration\Entities\BusinessDetail;
 use Modules\Circular\Entities\Dispatch;
 use Modules\Circular\Entities\Registration;
 use Modules\DigitalBoard\Entities\Notice;
+use Modules\EMap\Entities\MapApply;
 use Modules\EMap\Entities\Organization;
 use Modules\ExecutiveMeeting\Entities\MeetingEvent;
 use Modules\GrievanceHandling\Entities\GrievanceDetail;
 use Modules\GrievanceHandling\Entities\GrievanceType;
 use Modules\GrievanceHandling\Entities\GrievanceUser;
 use Modules\Plan\Entities\PlanArea;
+use Modules\Plan\Entities\Project;
+use Modules\Roaster\Entities\Training;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
         $user_count = User::count();
-        $grievance_user_count = GrievanceUser::count();
-        $notice_count = Notice::where('type', 'Notice')->count();
-        $news_count = Notice::where('type', 'News')->count();
-        $registration_count = Registration::count();
-        $dispatch_count = Dispatch::count();
-        $unseen_grievance = GrievanceDetail::whereNull('grievance_detail_id')->whereStatus('Unseen');
-        $unseen_grievances = $unseen_grievance->limit(5)->get();
-        $unseen_grievance_count = $unseen_grievance->count();
-        $replied_grievance_count = GrievanceDetail::whereNull('grievance_detail_id')->whereStatus('Replied')->count();
-        $investigated_grievance_count = GrievanceDetail::whereNull('grievance_detail_id')->whereStatus('Investigated')->count();
-        $closed_grievance_count = GrievanceDetail::whereNull('grievance_detail_id')->whereStatus('Closed')->count();
-        $ward_meetings_count = MeetingEvent::where('event_for', 'ward')->count();
-        $municipal_meetings_count = MeetingEvent::where('event_for', 'municipal')->count();
-        $grievanceTypes = GrievanceType::withCount('grievanceDetails')->latest()->get();
+
         $businessDetail_count = BusinessDetail::whereNotNull('registration_no')->count();
         $activityLogs = ActivityLog::with('user')->whereDate('created_at', today()->toDateString())->paginate(5);
-
+        $training_count = Training::whereDate('closed_date', '<=', today()->toDateString())->count();
+        $project_count = Project::count();
+        $map_count = MapApply::count();
+        $grievance_count = GrievanceDetail::approved()->count();
         $planAreas = $this->setPlanData();
 
 
@@ -46,22 +39,11 @@ class DashboardController extends Controller
             'businessDetail_count',
             'planAreas',
             'activityLogs',
-            'grievance_user_count',
-            'notice_count',
-            'news_count',
-            'registration_count',
-            'dispatch_count',
-            'unseen_grievance_count',
-            'replied_grievance_count',
-            'investigated_grievance_count',
-            'closed_grievance_count',
-            'municipal_meetings_count',
-            'ward_meetings_count',
-            'grievanceTypes',
-            'unseen_grievances',
+            'training_count',
+            'project_count',
+            'map_count',
+            'grievance_count'
         ]));
-
-
     }
 
     private function setPlanData(): array
