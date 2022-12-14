700afca259122bc0f1c72a3fff62ebc43faa8c03
Yekdev Rokaya <yekdevrc@gmail.com>
2022-12-14 13:26:09 +0545

    updates

---
+++ Modules/JudicialCommittee/Http/Requests/LawSuiteNature/updateLawSuiteNatureRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\JudicialCommittee\Http\Requests\LawSuiteNature;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class updateLawSuiteNatureRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'title_en' => ['nullable', 'string', 'max:255'],
+            'code' => ['required', 'numeric'],
+        ];
+    }
+
+
+    public function messages()
+    {
+        return [
+            'title.required' => ['शीर्षक आवश्यक छ'],
+            'code.required' => ['कोड आवश्यक छ']
+        ];
+    }
+}
