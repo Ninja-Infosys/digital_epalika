05bad4f5e3a5e5e8cbf815892bc167b388088b1d
sush1l <sushilkc188@gmail.com>
2022-12-15 17:17:48 +0545

    gunaso

--- Modules/GrievanceHandling/Resources/views/admin/user/index.blade.php
+++ Modules/GrievanceHandling/Resources/views/admin/user/index.blade.php
@@ -6,12 +6,12 @@
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item">
-                            <a href="{{route('admin.dashboard')}}">
+                            <a href="{{ route('admin.dashboard') }}">
                                 <i class="fa fa-home"></i> गृहपृष्ठ
                             </a>
                         </li>
                         <li class="breadcrumb-item">
-                            <a href="{{route('admin.grievanceHandling.grievanceDetail.index')}}">प्रयोगकर्ताको
+                            <a href="{{ route('admin.grievanceHandling.grievanceDetail.index') }}">प्रयोगकर्ताको
                                 बिबरण </a>
                         </li>
                         <li class="breadcrumb-item active">प्रयोगकर्ताको बिबरण</li>
@@ -34,28 +34,36 @@
                     <div class="table-responsive">
                         <table class="table table-sm mb-0 table-striped table-hover">
                             <thead>
-                            <tr>
-                                <th>क्र.स</th>
-                                <th>नाम</th>
-                                <th>इमेल</th>
-                                <th>फोन नं</th>
-                                <th>गुनासोहरुको संख्या</th>
-                            </tr>
+                                <tr>
+                                    <th>क्र.स</th>
+                                    <th>नाम</th>
+                                    <th>इमेल</th>
+                                    <th>फोन नं</th>
+                                    <th>गुनासोहरुको संख्या</th>
+                                    <th>#</th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @forelse($grievanceUsers as $grievanceUser)
-                                <tr>
-                                    <th scope="row">{{$loop->iteration}}</th>
-                                    <td>{{$grievanceUser->name}}</td>
-                                    <td>{{$grievanceUser->email}}</td>
-                                    <td>{{$grievanceUser->phone}}</td>
-                                    <td>{{$grievanceUser->grievance_details_count}}</td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td class="text-center" colspan="7">तालिकामा कुनै डाटा उपलब्ध छैन !!!</td>
-                                </tr>
-                            @endforelse
+                                @forelse($grievanceUsers as $grievanceUser)
+                                    <tr>
+                                        <th scope="row">{{ $loop->iteration }}</th>
+                                        <td>{{ $grievanceUser->name }}</td>
+                                        <td>{{ $grievanceUser->email }}</td>
+                                        <td>{{ $grievanceUser->phone }}</td>
+                                        <td>{{ $grievanceUser->grievance_details_count }}</td>
+                                        <td>
+                                            <a href="{{route('admin.grievanceHandling.grievanceUser.show',$grievanceUser)}}"
+                                           title="थप हेर्नुहोस्"
+                                           class="btn btn-xs btn-outline-primary">
+                                            <i class="fa fa-eye"></i>
+                                        </a>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td class="text-center" colspan="7">तालिकामा कुनै डाटा उपलब्ध छैन !!!</td>
+                                    </tr>
+                                @endforelse
                             </tbody>
                         </table>
                     </div>
@@ -64,5 +72,3 @@
         </div>
     </div>
 @endsection
-
-
