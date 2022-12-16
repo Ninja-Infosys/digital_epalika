167f6e35c0afe6eae79f6121a932dfe9402931e8
manoj <manojoli667@gmail.com>
2022-12-16 16:51:03 +0545

    list registration dashboard & report

--- Modules/TaskManagement/Resources/views/admin/dashboard.blade.php
+++ Modules/TaskManagement/Resources/views/admin/dashboard.blade.php
@@ -1,108 +1,89 @@
 @extends('admin.layouts.master')
 @section('content')
     <div class="row mt-2">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item">
-                            <a href="{{route('admin.taskManagement.dashboard')}}">
-                                <i class="fa fa-home"></i> गृहपृष्ठ
-                            </a>
-                        </li>
-                        <li class="breadcrumb-item active">
-                            कार्य व्यवस्थापन
-                        </li>
-                    </ol>
-                </div>
-                <h4 class="page-title">गृहपृष्ठ </h4>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-xl-2">
-                    <div class="widget-rounded-circle card-primary">
-                        <div class="card-body" style="padding: 10px 20px;">
-                            <div class="row">
-                                <div class="col">
-                                    <div class="avatar-lg rounded-circle bg-light border">
-                                        <h3 class="mt-1 text-center"><span data-plugin="counterup">
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card-primary">
+                <div class="card-body" style="padding: 10px 20px;">
+                    <div class="row">
+                        <div class="col">
+                            <div class="avatar-lg rounded-circle bg-light border">
+                                <h3 class="mt-1 text-center"><span data-plugin="counterup">
                                             {{$dailyTaskCount}}
                                        </span>
-                                           </h3>
-                                    </div>
-                                    <p class="text my-1">आज थपिएको कार्य</p>
-                                </div>
-                            </div> <!-- end row-->
+                                </h3>
+                            </div>
+                            <p class="text my-1">आज थपिएको कार्य</p>
                         </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
+                    </div> <!-- end row-->
+                </div>
+            </div> <!-- end widget-rounded-circle-->
+        </div>
 
-                <div class="col-md-6 col-xl-2">
-                    <div class="widget-rounded-circle card-primary"  style="background-color: #0047AB">
-                        <div class="card-body" style="padding: 10px 20px;">
-                            <div class="row">
-                                <div class="col">
-                                    <div class="avatar-lg rounded-circle bg-light border">
-                                        <h3 class="mt-1 text-center"><span data-plugin="counterup">
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card-primary" style="background-color: #0047AB">
+                <div class="card-body" style="padding: 10px 20px;">
+                    <div class="row">
+                        <div class="col">
+                            <div class="avatar-lg rounded-circle bg-light border">
+                                <h3 class="mt-1 text-center"><span data-plugin="counterup">
                                             {{$totalTaskCount}}
                                        </span>
-                                           </h3>
-                                    </div>
-                                    <p class="text my-1">जम्मा प्रयोगकर्ता</p>
-                                </div>
-                            </div> <!-- end row-->
+                                </h3>
+                            </div>
+                            <p class="text my-1">जम्मा प्रयोगकर्ता</p>
                         </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
+                    </div> <!-- end row-->
+                </div>
+            </div> <!-- end widget-rounded-circle-->
+        </div>
 
-                <div class="col-md-6 col-xl-2">
-                    <div class="widget-rounded-circle card-primary">
-                        <div class="card-body" style="padding: 10px 20px;">
-                            <div class="row">
-                                <div class="col">
-                                    <div class="avatar-lg rounded-circle bg-light border">
-                                        <h3 class="mt-1 text-center"><span data-plugin="counterup">
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card-primary">
+                <div class="card-body" style="padding: 10px 20px;">
+                    <div class="row">
+                        <div class="col">
+                            <div class="avatar-lg rounded-circle bg-light border">
+                                <h3 class="mt-1 text-center"><span data-plugin="counterup">
                                             {{$totalTaskCategory}}
                                        </span>
-                                           </h3>
-                                    </div>
-                                    <p class="text my-1">उपलब्ध सेवाहरु</p>
-                                </div>
-                            </div> <!-- end row-->
+                                </h3>
+                            </div>
+                            <p class="text my-1">उपलब्ध सेवाहरु</p>
                         </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
+                    </div> <!-- end row-->
+                </div>
+            </div> <!-- end widget-rounded-circle-->
+        </div>
 
-                <div class="col-md-6 col-xl-2">
-                    <div class="widget-rounded-circle card-primary"  style="background-color: #0047AB">
-                        <div class="card-body" style="padding: 10px 20px;">
-                            <div class="row">
-                                <div class="col">
-                                    <div class="avatar-lg rounded-circle bg-light border">
-                                        <h3 class="mt-1 text-center"><span data-plugin="counterup">
+        <div class="col-md-6 col-xl-2">
+            <div class="widget-rounded-circle card-primary" style="background-color: #0047AB">
+                <div class="card-body" style="padding: 10px 20px;">
+                    <div class="row">
+                        <div class="col">
+                            <div class="avatar-lg rounded-circle bg-light border">
+                                <h3 class="mt-1 text-center"><span data-plugin="counterup">
                                             {{$totalTaskDivision}}
                                        </span>
-                                           </h3>
-                                    </div>
-                                    <p class="text my-1">कार्य विभाजन</p>
-                                </div>
-                            </div> <!-- end row-->
+                                </h3>
+                            </div>
+                            <p class="text my-1">कार्य विभाजन</p>
                         </div>
-                    </div> <!-- end widget-rounded-circle-->
-                </div> <!-- end col-->
-            </div>
+                    </div> <!-- end row-->
+                </div>
+            </div> <!-- end widget-rounded-circle-->
         </div>
     </div>
-    <div class="row">
-
+    <div class="row mt-2">
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-body">
-                    <x-charts.pie-chart-component id="task-report-chart"
-                                                  chartName="आर्थिक वर्ष अनुसार जम्मा कार्यहरु"
-                                                  :labels="$taskData['labels']"
-                                                  :dataSets="$taskData['dataSets']"/>
-                </div> <!-- end card-body-->
-            </div> <!-- end card-->
-        </div> <!-- end col -->
+                    <x-charts.pie-chart-component
+                        id="task-report-chart"
+                        chartName="आर्थिक वर्ष अनुसार जम्मा कार्यहरु"
+                        :labels="$taskData['labels']"
+                        :dataSets="$taskData['dataSets']"/>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
