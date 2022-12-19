48d0451c8357bdaadf381a29217da8192643faf9
Ninja <bijaygamuwatharu@gmail.com>
2022-12-19 13:38:55 +0545

    enterprise Type

--- Modules/Grant/Routes/admin.php
+++ Modules/Grant/Routes/admin.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Grant\Http\Controllers\Admin\DashboardController;
+use Modules\Grant\Http\Controllers\Admin\Setting\EnterpriseTypeController;
 use Modules\Grant\Http\Controllers\Admin\Setting\GrantTypeController;
 
 Route::get('dashboard', DashboardController::class)->name('dashboard');
 
 Route::prefix('setting')->as('setting.')->group(function () {
     Route::resource('grantType', GrantTypeController::class);
+    Route::resource('enterpriseType', EnterpriseTypeController::class);
 });
 
