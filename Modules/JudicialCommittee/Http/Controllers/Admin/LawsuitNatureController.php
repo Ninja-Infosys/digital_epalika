54abf0502dee2806f84326131ba97579293efffc
manoj <manojoli667@gmail.com>
2022-12-19 11:32:06 +0545

    judicial committee

--- Modules/JudicialCommittee/Http/Controllers/Admin/LawsuitNatureController.php
+++ Modules/JudicialCommittee/Http/Controllers/Admin/LawsuitNatureController.php
@@ -13,35 +13,41 @@ class LawsuitNatureController extends Controller
 {
     public function index()
     {
+        $this->checkAuthorization('lawsuitNature_access');
+
         $lawSuitNatures=LawsuitNature::latest()->get();
+
         return view('judicialcommittee::admin.setting.lawsuit_nature.index', compact('lawSuitNatures'));
     }
 
     public function create()
     {
+        $this->checkAuthorization('lawsuitNature_create');
+
         return view('judicialcommittee::admin.setting.lawsuit_nature.create');
     }
 
     public function store(StoreLawSuiteNatureRequest $request)
     {
+        $this->checkAuthorization('lawsuitNature_create');
+
         LawsuitNature::create($request->validated());
 
         toast('मुद्दा प्रकृति सफलतापूर्वक थपियो', 'success');
         return back();
     }
 
-    public function show($id)
-    {
-        return view('judicialcommittee::show');
-    }
-
     public function edit(LawsuitNature $lawsuitNature)
     {
+        $this->checkAuthorization('lawsuitNature_edit');
+
         return view('judicialcommittee::admin.setting.lawsuit_nature.edit', compact('lawsuitNature'));
     }
 
     public function update(updateLawSuiteNatureRequest $request, LawsuitNature $lawsuitNature)
     {
+        $this->checkAuthorization('lawsuitNature_edit');
+
         $lawsuitNature->update($request->validated());
 
         toast('मुद्दा प्रकृति सफलतापूर्वक अपडेट गरियो','success');
@@ -50,6 +56,8 @@ class LawsuitNatureController extends Controller
 
     public function destroy(LawsuitNature $lawsuitNature)
     {
+        $this->checkAuthorization('lawsuitNature_delete');
+
         $lawsuitNature->delete();
 
         toast('मुद्दा प्रकृति सफलतापूर्वक हटाइयो','success');
--- Modules/JudicialCommittee/Resources/views/admin/layouts/sidebar.blade.php
+++ Modules/JudicialCommittee/Resources/views/admin/layouts/sidebar.blade.php
@@ -5,36 +5,36 @@
     </a>
 </li>
 @can('chiefJudicialMember_access')
-<li>
-    <a href="{{route('admin.judicialCommittee.chiefJudicialMember.index')}}">
-        <i class="fa fa-user"></i>
-        <span>प्रमुख न्यायिक सदस्य</span>
-    </a>
-</li>
+    <li>
+        <a href="{{route('admin.judicialCommittee.chiefJudicialMember.index')}}">
+            <i class="fa fa-user"></i>
+            <span>प्रमुख न्यायिक सदस्य</span>
+        </a>
+    </li>
 @endcan
 @can('administrationMember_access')
-<li>
-    <a href="{{route('admin.judicialCommittee.administrationMember.index')}}">
-        <i class="fa fa-user"></i>
-        <span>प्रशासन सदस्यहरू</span>
-    </a>
-</li>
+    <li>
+        <a href="{{route('admin.judicialCommittee.administrationMember.index')}}">
+            <i class="fa fa-user"></i>
+            <span>प्रशासन सदस्यहरू</span>
+        </a>
+    </li>
 @endcan
 @can('judicialMember_access')
-<li>
-    <a href="{{route('admin.judicialCommittee.judicialMember.index')}}">
-        <i class="fa fa-user"></i>
-        <span>न्यायिक समिति विवरण </span>
-    </a>
-</li>
+    <li>
+        <a href="{{route('admin.judicialCommittee.judicialMember.index')}}">
+            <i class="fa fa-user"></i>
+            <span>न्यायिक समिति विवरण </span>
+        </a>
+    </li>
 @endcan
 @can('complaintApplication_access')
-<li>
-    <a href="{{route('admin.judicialCommittee.complaintApplication.index')}}">
-        <i class="fa fa-edit"></i>
-        <span>निबेदन फारम</span>
-    </a>
-</li>
+    <li>
+        <a href="{{route('admin.judicialCommittee.complaintApplication.index')}}">
+            <i class="fa fa-edit"></i>
+            <span>निबेदन फारम</span>
+        </a>
+    </li>
 @endcan
 <li>
     <a href="{{route('admin.judicialCommittee.nissaForm')}}">
@@ -70,46 +70,13 @@
             <i class="fas fa-angle-right"></i>
         </span>
     </a>
-    <div class="collapse {{request()->is('admin/grant/setting/*') ? 'show' : ''}}"
+    <div class="collapse {{request()->is('admin/judicialCommittee/setting/*') ? 'show' : ''}}"
          id="sidebarGrantSetting">
         <ul class="nav-second-level">
-            <li class="{{request()->is('admin/grant/setting/lawsuitNature') ? 'active' : ''}}">
-                <a href="{{route('admin.judicialCommittee.lawsuitNature.index')}}">
-                    <span>  मुद्दा प्रकृति </span>
-                </a>
-            </li>
-            @can('infrastructure_access')
-                <li class="{{request()->is('admin/grant/setting/infrastructure') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.infrastructure.index')}}">
-                        <span>  पूर्वाधार शीर्षकहरु  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('thematicArea_access')
-                <li class="{{request()->is('admin/grant/setting/thematicArea') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.thematicArea.index')}}">
-                        <span>  विषयगत क्षेत्र  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('grantType_access')
-                <li class="{{request()->is('admin/grant/setting/grantType') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.grantType.index')}}">
-                        <span> अनुदान प्रकार  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('grantProgram_access')
-                <li class="{{request()->is('admin/grant/setting/grantProgram') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.grantProgram.index')}}">
-                        <span> अनुदान कार्यक्रम  </span>
-                    </a>
-                </li>
-            @endcan
-            @can('grantActivity_access')
-                <li class="{{request()->is('admin/grant/setting/grantActivity') ? 'active' : ''}}">
-                    <a href="{{route('admin.grant.grantActivity.index')}}">
-                        <span> अनुदान क्रियाकलाप  </span>
+            @can('lawsuitNature_access')
+                <li class="{{request()->is('admin/judicialCommittee/setting/lawsuitNature') ? 'active' : ''}}">
+                    <a href="{{route('admin.judicialCommittee.lawsuitNature.index')}}">
+                        <span>  मुद्दा प्रकृति </span>
                     </a>
                 </li>
             @endcan
