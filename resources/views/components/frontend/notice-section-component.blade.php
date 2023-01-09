c4b5c33929302dde4cadd2d71f42f2c534356235
sush1l <sushilkc188@gmail.com>
2023-01-09 14:00:37 +0545

    mobile view

--- resources/views/components/frontend/notice-section-component.blade.php
+++ resources/views/components/frontend/notice-section-component.blade.php
@@ -47,14 +47,14 @@
         @endforeach
 
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
-    </button>
+    </button> --}}
 </div>
 
 @push('styles')
@@ -77,6 +77,7 @@
             overflow: hidden;
             position: relative;
             box-sizing: border-box;
+        }
         }
     </style>
 @endpush
