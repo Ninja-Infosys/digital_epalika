84559a84d57c083509369d0696f3855f6ff12e8a
Yekdev Rokaya <yekdevrc@gmail.com>
2023-01-09 16:08:23 +0545

    Organization Registration

--- Modules/OrganizationRegistration/Entities/Institution.php
+++ Modules/OrganizationRegistration/Entities/Institution.php
@@ -9,15 +9,46 @@ use App\Traits\EventObserveTrait;
 
 class Institution extends Model
 {
-    use HasFactory,SoftDeletes,EventObserveTrait;
+    use HasFactory, SoftDeletes, EventObserveTrait;
 
-   protected $dates = [
-       'created_at',
-       'updated_at',
-       'deleted_at'
-   ];
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
-   protected $fillable = [
-       'name'
-   ];
+    protected $fillable = [
+        'user_id',
+        'registration_no',
+        'registration_date',
+        'registration_date_en',
+        'name',
+        'province_id',
+        'district_id',
+        'local_body_id',
+        'ward_no',
+        'institution_address',
+        'contact_no',
+        'email',
+        'dao_registration_no',
+        'dao_registration_date',
+        'dao_registration_date_en',
+        'swc_registration_no',
+        'swc_registration_date',
+        'swc_registration_date_en',
+        'pan_vat',
+        'objective',
+        'area',
+        'minute',
+        'application',
+        'Legislation',
+        'Ward_recommendation',
+        'stamp',
+        'proposed_person',
+        'supervisor_person',
+        'approval_person',
+        'proposed_person_designation',
+        'supervisor_person_designation',
+        'approval_person_designation',
+    ];
 }
