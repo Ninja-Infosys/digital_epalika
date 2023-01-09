9b8e374d18e7051f9de1773a1aee9df745162ff2
Tekchan Khatri <tekam7755@gmail.com>
2023-01-09 10:41:39 +0545

    Identity module

--- Modules/Identity/Resources/views/admin/seniorCitizen/edit.blade.php
+++ Modules/Identity/Resources/views/admin/seniorCitizen/edit.blade.php
@@ -23,10 +23,15 @@
                 <div class="card-header">
                     <div class="d-flex justify-content-between">
                         <h4 class="header-title">नयाँ जेष्ठ नागरिक सम्पादन गर्नुहोस्</h4>
-                        <a href="{{route('identity.admin.seniorCitizenDetail.index')}}"
-                           class="btn btn-sm btn-outline-primary">
-                            <i class="fa fa-list"></i> जेष्ठ नागरिक सुची
-                        </a>
+                        <div>
+                            <a href="{{route('identity.admin.seniorCitizenDetail.index')}}"
+                               class="btn btn-sm btn-outline-primary">
+                                <i class="fa fa-list"></i> जेष्ठ नागरिक सुची
+                            </a>
+                            <a href="https://localhost:8003/mfs100" target="_blank" class="btn btn-sm btn-outline-primary">
+                                Run MFS 100
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <div class="card-body">
