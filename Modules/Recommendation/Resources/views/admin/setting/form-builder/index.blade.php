e652d54d708e763c965a62df56d2a76540ad7a65
Tekchan Khatri <tekam7755@gmail.com>
2023-01-18 15:01:55 +0545

    form builder

--- Modules/Recommendation/Resources/views/admin/setting/form-builder/index.blade.php
+++ Modules/Recommendation/Resources/views/admin/setting/form-builder/index.blade.php
@@ -80,7 +80,7 @@
                                                 method="post">
                                                 @csrf
                                                 @method('delete')
-                                                @if($formBuilder->status===0)
+                                                @if($formBuilder->status)
                                                 <button class="btn btn-xs btn-outline-danger show_confirm">
                                                     <i class="fa fa-trash"></i>
                                                 </button>
@@ -151,7 +151,7 @@
                                                 method="post">
                                                 @csrf
                                                 @method('delete')
-                                                @if($recommendationTemplate->status===0)
+                                                @if($recommendationTemplate->status)
                                                     @can('recommendationTemplate_delete')
                                                     <button class="btn btn-xs btn-outline-danger show_confirm">
                                                         <i class="fa fa-trash"></i>
