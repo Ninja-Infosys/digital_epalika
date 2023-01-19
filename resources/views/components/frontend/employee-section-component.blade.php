2d03dbd076865531b39243f031160bfdef518afa
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2023-01-19 13:53:24 +0545

    front changes

--- resources/views/components/frontend/employee-section-component.blade.php
+++ resources/views/components/frontend/employee-section-component.blade.php
@@ -4,14 +4,14 @@
             <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
                 <div class="emp-section">
                     @foreach ($empChunk as $employee)
-                        <div class="emp-card d-flex align-items-center bg-info p-1 rounded">
+                        <div class="emp-card d-flex align-items-center p-1 rounded border">
                             <div class="flex-shrink-0">
                                 <img src="{{ $employee->photo_url }}" class="rounded" alt="{{ $employee->name }}" height="120">
                             </div>
                             <div class="flex-grow-1 ms-3">
                                 <h5>{{ $employee->name }}</h5>
-                                <h6>{{ $employee->designation }}</h6>
-                                <p><i class="fa-solid fa-phone"></i> {{ $employee->phone }}</p>
+                                <h6 class="text-muted">{{ $employee->designation }}</h6>
+                                <h6 class="text-muted"><i class="fa-solid fa-phone"></i> {{ $employee->phone }}</h6>
                             </div>
                         </div>
                     @endforeach
