6547427307c5cb2d36f97375eccc4206caaf1c22
manoj <manojoli667@gmail.com>
2023-01-24 12:11:18 +0545

    plan setting added

---
+++ Modules/Plan/Http/Controllers/Admin/Setting/ExpenseHeadController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Modules\Plan\Http\Controllers\Admin\Setting;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ExpenseHeadController extends Controller
+{
+    public function index()
+    {
+        $this->checkAuthorization('expenseHead_access');
+
+        return view('plan::index');
+    }
+
+    public function create()
+    {
+        return view('plan::create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        return view('plan::show');
+    }
+
+    public function edit($id)
+    {
+        return view('plan::edit');
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
