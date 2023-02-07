153f4dd9f750fb98f1ed110d2ca1f992bd2ffa00
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2023-02-07 13:13:22 +0545

    business certificate ui design

--- Modules/BusinessRegistration/Resources/views/admin/businessRegistration/print.blade.php
+++ Modules/BusinessRegistration/Resources/views/admin/businessRegistration/print.blade.php
@@ -22,7 +22,7 @@
         </div>
     </div>
     <div class="d-flex justify-content-center">
-        <div class="col-md-7">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center justify-content-between">
