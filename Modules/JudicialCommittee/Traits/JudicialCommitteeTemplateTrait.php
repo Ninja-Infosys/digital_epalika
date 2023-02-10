1847a66bd466350fa0f44d1126b87cd3e8a133c9
manoj <manojoli667@gmail.com>
2023-02-10 17:04:30 +0545

    judicial committee

--- Modules/JudicialCommittee/Traits/JudicialCommitteeTemplateTrait.php
+++ Modules/JudicialCommittee/Traits/JudicialCommitteeTemplateTrait.php
@@ -243,7 +243,7 @@ trait JudicialCommitteeTemplateTrait
 
         return [
             '[@complainant.brief_name]' => ($complainants?->first()->name ?? '') . $complainants->count() > 1 ? " सहित" . ($complainants->count() - 1) . " जना" : '',
-            '[@complainant.name]' => implode(',', $complainants->pluck('name')),
+            '[@complainant.name]' => implode(',', $complainants->pluck('name')->toArray()),
             '[@complainant.age]' => ($complainants?->first()->age ?? ''),
             '[@complainant.father_name]' => ($complainants?->first()->father_name ?? ''),
             '[@complainant.grandfather_name]' => ($complainants?->first()->grandfather_name ?? ''),
@@ -262,7 +262,7 @@ trait JudicialCommitteeTemplateTrait
 
         return [
             '[@defendant.brief_name]' => ($defendants?->first()->name ?? '') . $defendants->count() > 1 ? " सहित" . ($defendants->count() - 1) . " जना" : '',
-            '[@defendant.name]' => implode(',', $defendants->pluck('name')),
+            '[@defendant.name]' => implode(',', $defendants->pluck('name')->toArray()),
             '[@defendant.age]' => ($defendants?->first()->age ?? ''),
             '[@defendant.father_name]' => ($defendants?->first()->father_name ?? ''),
             '[@defendant.grandfather_name]' => ($defendants?->first()->grandfather_name ?? ''),
