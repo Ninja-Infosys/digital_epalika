44ce93dba775b2a9571f4a53017b071e1ff4004e
ShiShiR RaI <rphantom05@gmail.com>
2023-02-14 12:13:06 +0545

    Chart

--- public/assets/backend/js/chart/chart.init.js
+++ public/assets/backend/js/chart/chart.init.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     const url = $('#charts').data('chart-url');
+    const loading = $(".loading");
     $.ajax({
         method: 'GET',
         url,
@@ -10,20 +11,20 @@ $(document).ready(() => {
                     createChart(targetElement, response[key]);
                 }
             })
-            const loading = $(".loading");
             loading.addClass('d-none');
         },
-        error: (error) => {
-            console.error(`Error fetching chart data: ${error}`);
+        error: () => {
+            loading.html('<p class="text-center">डाटा छैन !!!</p>');
         },
     })
+
     function createChart(element, data) {
         const chartType = $(element).attr('chart-type');
 
         if (chartType === 'pie' || chartType === 'donut') {
             if (Array.isArray(data)) {
-                const pieData = data.map(function(val) {
-                    return { name: val.name, y: val.data };
+                const pieData = data.map(function (val) {
+                    return {name: val.name, y: val.data};
                 });
                 setPieChart(element, chartType, pieData);
             } else {
@@ -31,8 +32,8 @@ $(document).ready(() => {
             }
         } else {
             if (typeof data === 'object' && Array.isArray(data.dataSets) && Array.isArray(data.labels)) {
-                const chartDatasets = data.dataSets.map(function(dataset) {
-                    return { name: dataset.label, data: dataset.data };
+                const chartDatasets = data.dataSets.map(function (dataset) {
+                    return {name: dataset.label, data: dataset.data};
                 });
                 setBarChart(element, chartType, data.labels, chartDatasets);
             } else {
@@ -40,13 +41,14 @@ $(document).ready(() => {
             }
         }
     }
+
     function setBarChart(element, charType, labels, datasets) {
         const title = $(element).attr('chart-title');
         Highcharts.chart(element, {
-            chart: { type: charType },
-            title: { text: title, align: 'left', style: {fontFamily: 'Mukta'}},
-            xAxis: { categories: labels, crosshair: true },
-            yAxis: { min: 0, title: { text: null } },
+            chart: {type: charType},
+            title: {text: title, align: 'left', style: {fontFamily: 'Mukta'}},
+            xAxis: {categories: labels, crosshair: true},
+            yAxis: {min: 0, title: {text: null}},
             tooltip: {
                 headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td><td style="padding:0"><b>{point.y}</b></td></tr>',
@@ -54,31 +56,33 @@ $(document).ready(() => {
                 shared: true,
                 useHTML: true
             },
-            plotOptions: { column: { pointPadding: 0.2, borderWidth: 0 } },
+            plotOptions: {column: {pointPadding: 0.2, borderWidth: 0}},
             series: datasets
         });
     }
+
     function setPieChart(element, chartType, data) {
         const title = $(element).attr('chart-title');
         const isDonut = chartType === 'donut' ? '60%' : '0%';
         Highcharts.chart(element, {
-            chart: { type: 'pie' },
-            title: { text: title, align: 'left',style: {fontFamily: 'Mukta'} },
-            tooltip: { pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>' },
-            accessibility: { point: { valueSuffix: '%' } },
+            chart: {type: 'pie'},
+            title: {text: title, align: 'left', style: {fontFamily: 'Mukta'}},
+            tooltip: {pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'},
+            accessibility: {point: {valueSuffix: '%'}},
             plotOptions: {
                 pie: {
                     allowPointSelect: true,
                     cursor: 'pointer',
                     innerSize: isDonut,
-                    dataLabels: { enabled: false },
+                    dataLabels: {enabled: false},
                     showInLegend: true
                 }
             },
-            series: [{ name: 'डाटा', colorByPoint: true, data: data }]
+            series: [{name: 'डाटा', colorByPoint: true, data: data}]
         });
     }
 })
+
 function toastMessage(type, title) {
     swal.fire({
         title: title,
