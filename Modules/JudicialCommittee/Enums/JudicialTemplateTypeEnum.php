5b40290ace7ad11c656c707c48fe16bb89a10879
manoj <manojoli667@gmail.com>
2023-02-15 14:59:05 +0545

    judicial committee template

--- Modules/JudicialCommittee/Enums/JudicialTemplateTypeEnum.php
+++ Modules/JudicialCommittee/Enums/JudicialTemplateTypeEnum.php
@@ -11,6 +11,7 @@ enum JudicialTemplateTypeEnum: string
     case DATE_SHEET = "date_sheet";
     case DEFENDANT_ISSUED_DEADLINE = "defendant_issued_deadline";
     case DATE_COMPENSATION = "date_compensation";
+    case DECISION = "decision";
 
     public function label(): string
     {
@@ -24,9 +25,10 @@ enum JudicialTemplateTypeEnum: string
             self::DEFENDANT_ISSUED_DEADLINE => 'प्रतिवादी म्याद जारी',
             self::DATE_COMPENSATION => 'तारिख भरपाई',
             self::JUDICIAL_RECEIPT_BILL => 'निस्सा सनाखत',
-            self::DEFENDANT_APPLICATION_IDENTIFICATION=>'प्रतिवादी निस्सा सनाखत',
+            self::DEFENDANT_APPLICATION_IDENTIFICATION => 'प्रतिवादी निस्सा सनाखत',
             self::COMPLAINANT_APPLICATION => 'वादी दर्ता नालेस',
             self::DEFENDANT_APPLICATION => 'प्रतिवादी दर्ता नालेस',
+            self::DECISION => 'निर्णय'
         };
     }
 }
