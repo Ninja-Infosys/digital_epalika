30156183b637e55b0b2f492a04f2cc44e292e386
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2023-02-15 13:09:15 +0545

    ckeditor

--- Modules/EMap/Resources/views/admin/template/create.blade.php
+++ Modules/EMap/Resources/views/admin/template/create.blade.php
@@ -122,7 +122,8 @@
         </div>
     </div>
     @push('scripts')
-        <script src="{{asset('assets/backend/js/ckeditor.js')}}"></script>
+        <script src="{{asset('assets/backend/ckeditor/ckeditor.js')}}"></script>
+        <script src="{{asset('assets/backend/ckeditor/editor.js')}}"></script>
         <script>
             $(document).ready(function () {
                 $.ajaxSetup({
