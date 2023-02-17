6f8c09d55feb440ebe17d25e1d33679a96d55e7b
manoj <manojoli667@gmail.com>
2023-02-17 17:15:46 +0545

    status added to complaint application

--- Modules/JudicialCommittee/Http/Controllers/Admin/DashboardController.php
+++ Modules/JudicialCommittee/Http/Controllers/Admin/DashboardController.php
@@ -15,15 +15,13 @@ class DashboardController extends Controller
 {
     use NepaliDateConverter;
 
-    protected OfficeSetting $officeSetting;
     protected Collection $currentYearApplications;
 
     public function __construct()
     {
         parent::__construct();
 
-        $this->officeSetting = OfficeSetting::first();
-        $this->currentYearApplications = ComplaintApplication::with('judicialReceiptBill')->where('fiscal_year_id', $this->officeSetting->fiscal_year_id)->get();
+        $this->currentYearApplications = ComplaintApplication::with('judicialReceiptBill')->where('fiscal_year_id', officeSetting()->fiscal_year_id)->get();
     }
 
     public function __invoke()
@@ -34,13 +32,13 @@ class DashboardController extends Controller
         $registeredApplicationsCount = ComplaintApplication::whereHas('judicialReceiptBill')->count();
         $currentYearApplicationsCount = $this->currentYearApplications->count();
         $currentMonthApplicationsCount = $this->currentYearApplications->where('month', $today_nepali_date['m'])->count();
-        if (request()->ajax()){
-           return [
-            'monthlyApplications' => $this->getMonthlyApplications(),
-            'lawsuitNatureWiseApplications' => $this->getLawsuitNatureWiseApplications(),
-            'fiscalYearWiseApplications' => $this->getFiscalYearWiseApplications(),
-            'lawsuitNatureWiseApplicationsData' => $this->getLawsuitNatureWiseApplicationsData()
-           ];
+        if (request()->ajax()) {
+            return [
+                'monthlyApplications' => $this->getMonthlyApplications(),
+                'lawsuitNatureWiseApplications' => $this->getLawsuitNatureWiseApplications(),
+                'fiscalYearWiseApplications' => $this->getFiscalYearWiseApplications(),
+                'lawsuitNatureWiseApplicationsData' => $this->getLawsuitNatureWiseApplicationsData()
+            ];
         }
         return view('judicialcommittee::admin.dashboard', compact(
             'totalApplicationsCount',
@@ -56,8 +54,8 @@ class DashboardController extends Controller
         $unregisteredApplications = [];
 
         foreach ($this->month_name as $key => $month) {
-            $registeredApplications[] = $this->currentYearApplications->where('month', ($key + 1))->where('judicialReceiptBill', '!=', null)->count();
-            $unregisteredApplications[] = $this->currentYearApplications->where('month', ($key + 1))->where('judicialReceiptBill', null)->count();
+            $registeredApplications[] = (int)$this->currentYearApplications->where('month', ($key + 1))->where('judicialReceiptBill', '!=', null)->count();
+            $unregisteredApplications[] = (int)$this->currentYearApplications->where('month', ($key + 1))->where('judicialReceiptBill', null)->count();
         }
 
         return [
@@ -80,11 +78,11 @@ class DashboardController extends Controller
     private function getLawsuitNatureWiseApplications()
     {
         return LawsuitNature::withCount(['complaintApplications' => function ($query) {
-            $query->where('fiscal_year_id', $this->officeSetting->fiscal_year_id);
-        }])->get()->map(function ($lawsuitNature){
+            $query->where('fiscal_year_id', officeSetting()->fiscal_year_id);
+        }])->get()->map(function ($lawsuitNature) {
             return [
-              'name' => $lawsuitNature->title,
-                'data' => $lawsuitNature->complaint_applications_count
+                'name' => $lawsuitNature->title,
+                'data' => (int)$lawsuitNature->complaint_applications_count
             ];
         });
     }
@@ -99,8 +97,8 @@ class DashboardController extends Controller
 
             return [
                 'title' => $fiscalYear->title,
-                'registered_applications_count' => $registeredApplicationsCount,
-                'unregistered_applications_count' => $unregisteredApplicationsCount
+                'registered_applications_count' => (int)$registeredApplicationsCount,
+                'unregistered_applications_count' => (int)$unregisteredApplicationsCount
             ];
         });
 
@@ -124,7 +122,7 @@ class DashboardController extends Controller
     private function getLawsuitNatureWiseApplicationsData()
     {
         $lawsuitNatures = LawsuitNature::with(['complaintApplications' => function ($query) {
-            $query->with('judicialReceiptBill')->where('fiscal_year_id', $this->officeSetting->fiscal_year_id);
+            $query->with('judicialReceiptBill')->where('fiscal_year_id', officeSetting()->fiscal_year_id);
         }])->get()->map(function ($lawsuitNature) {
             $registeredApplicationsCount = 0;
             $unregisteredApplicationsCount = 0;
@@ -133,9 +131,9 @@ class DashboardController extends Controller
 
             return [
                 'title' => $lawsuitNature->title,
-                'registered_applications_count' => $registeredApplicationsCount,
-                'unregistered_applications_count' => $unregisteredApplicationsCount,
-                'total_applications_count' => $registeredApplicationsCount + $unregisteredApplicationsCount
+                'registered_applications_count' => (int)$registeredApplicationsCount,
+                'unregistered_applications_count' => (int)$unregisteredApplicationsCount,
+                'total_applications_count' => (int)$registeredApplicationsCount + $unregisteredApplicationsCount
             ];
         });
 
