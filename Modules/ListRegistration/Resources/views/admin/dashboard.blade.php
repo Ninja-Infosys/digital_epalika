37d919325415f63991020bee719a0bcc773c8a2b
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2023-02-17 12:01:53 +0545

    charts

--- Modules/ListRegistration/Resources/views/admin/dashboard.blade.php
+++ Modules/ListRegistration/Resources/views/admin/dashboard.blade.php
@@ -1,60 +1,55 @@
 @extends('admin.layouts.master')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-4">
-            <div class="widget-rounded-circle card-primary">
+    <div class="row mt-2">
+        <div class="col-12">
+            <div class="card widget-inline">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center">
-                                    <span data-plugin="counterup">
-                                            {{$totalRegistrations}}
-                                        </span>
-                                </h3>
+                        <div class="col-sm-6 col-xl-3 border-end">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-check avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mt-1 mb-0"><span data-plugin="counterup"> {{$totalRegistrations}}</span></h3>
+                                <p class="text-muted font-15 mb-0">जम्मा दर्ताहरु</p>
                             </div>
-                            <p class="text my-1">जम्मा दर्ताहरु</p>
                         </div>
-                    </div> <!-- end row-->
-                </div>
-            </div> <!-- end widget-rounded-circle-->
-        </div>
-        <div class="col-md-4">
-            <div class="widget-rounded-circle card-secondary">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center"><span data-plugin="counterup">
-                                           {{$yearlyRegistrations}}
-                                       </span>
-                                </h3>
+
+                        <div class="col-sm-6 col-xl-3 border-end">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-check avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mt-1 mb-0"><span data-plugin="counterup">{{$yearlyRegistrations}}</span></h3>
+                                <p class="text-muted font-15 mb-0 text-truncate">वार्षिक दर्ताहरु</p>
                             </div>
-                            <p class="text my-1">वार्षिक दर्ताहरु</p>
                         </div>
-                    </div> <!-- end row-->
-                </div>
-            </div> <!-- end widget-rounded-circle-->
-        </div>
-        <div class="col-md-4">
-            <div class="widget-rounded-circle card-primary">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center">
-                                    <span data-plugin="counterup">
-                                            {{$monthlyRegistrations}}
-                                        </span>
-                                </h3>
+
+                        <div class="col-sm-6 col-xl-3 border-end">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-check avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mb-0 mt-1"><span data-plugin="counterup"> {{$monthlyRegistrations}}</span></h3>
+                                <p class="text-muted font-15 mb-0">मासिक दर्ताहरु</p>
                             </div>
-                            <p class="text my-1">मासिक दर्ताहरु</p>
                         </div>
-                    </div> <!-- end row-->
+
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-map avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mb-0 mt-1"><span data-plugin="counterup">6</span></h3>
+                                <p class="text-muted font-15 mb-0">मासिक दर्ता नक्सा</p>
+                            </div>
+                        </div>
+
+                    </div> <!-- end row -->
                 </div>
-            </div> <!-- end widget-rounded-circle-->
-        </div>
+            </div> <!-- end card-->
+        </div> <!-- end col-->
     </div>
     <div class="row" id="charts" data-chart-url="{{route('admin.listRegistrations.dashboard')}}">
         <div class="col-lg-6">
