32647604e0ade4846334f6731fb80079c6cc0fa5
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2023-02-17 14:17:09 +0545

    charts changes

--- Modules/JudicialCommittee/Resources/views/admin/dashboard.blade.php
+++ Modules/JudicialCommittee/Resources/views/admin/dashboard.blade.php
@@ -1,128 +1,110 @@
 @extends('admin.layouts.master')
 @section('content')
-    <div class="row">
-        <div class="col-md-3">
-            <div class="widget-rounded-circle card-primary">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center">
-                                    <span data-plugin="counterup">
-                                        {{$totalApplicationsCount}}
-                                    </span>
-                                </h3>
-                            </div>
-                            <p class="text my-1">जम्मा निवेदनहरु</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="widget-rounded-circle card-secondary">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center">
-                                    <span data-plugin="counterup">
-                                        {{$registeredApplicationsCount}}
-                                   </span>
-                                </h3>
-                            </div>
-                            <p class="text my-1">दर्ता भएका निवेदनहरु</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="widget-rounded-circle card-primary">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center">
-                                    <span data-plugin="counterup">
-                                        {{$currentYearApplicationsCount}}
-                                    </span>
-                                </h3>
-                            </div>
-                            <p class="text my-1">चालू आर्थिक वर्षका निवेदनहरु</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="widget-rounded-circle card-secondary">
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col">
-                            <div class="avatar-lg rounded-circle bg-light border">
-                                <h3 class="mt-1 text-center">
-                                    <span data-plugin="counterup">
-                                        {{$currentMonthApplicationsCount}}
-                                   </span>
-                                </h3>
-                            </div>
-                            <p class="text my-1">हालको महिनाका निवेदनहरु</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="row mt-2">
+        <div class="col-12">
+            <div class="card widget-inline">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6 col-xl-3 border-end">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-list avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mt-1 mb-0"><span data-plugin="counterup"> {{$totalApplicationsCount}}</span></h3>
+                                <p class="text-muted font-15 mb-0">जम्मा निवेदनहरु</p>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-xl-3 border-end">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-list avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mt-1 mb-0"><span data-plugin="counterup">{{$registeredApplicationsCount}}</span></h3>
+                                <p class="text-muted font-15 mb-0 text-truncate">दर्ता भएका निवेदनहरु</p>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-xl-3 border-end">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-list avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mb-0 mt-1"><span data-plugin="counterup">{{$currentYearApplicationsCount}}</span></h3>
+                                <p class="text-muted font-15 mb-0">चालू आर्थिक वर्षका निवेदनहरु</p>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-6 col-xl-3">
+                            <div class="d-flex flex-column align-items-center">
+                                <div class="avatar-sm bg-blue rounded-circle">
+                                    <i class="fas fa-clipboard-list avatar-title font-18 text-white"></i>
+                                </div>
+                                <h3 class="mb-0 mt-1"><span data-plugin="counterup">{{$currentMonthApplicationsCount}}</span></h3>
+                                <p class="text-muted font-15 mb-0">चालू महिनाका निवेदनहरु</p>
+                            </div>
+                        </div>
+
+                    </div> <!-- end row -->
+                </div>
+            </div> <!-- end card-->
+        </div> <!-- end col-->
+    </div>
+    <div class="row" id="charts" data-chart-url="{{route('admin.judicialCommittee.dashboard')}}">
         <div class="col-lg-6">
             <div class="card">
                 <div class="card-body">
-                    <x-charts.bar-chart-component
-                        chart-type="line"
-                        id="monthly-applications-chart" chartTitle="चालु आर्थिक({{ $officeSetting->fiscalYear->title ?? '' }}) वर्षका मासिक निवेदनहरु"
-                        :labels="$monthlyApplications['labels']"
-                        :dataSets="$monthlyApplications['dataSets']"
-                    />
+                    <div id="monthlyApplications" chart-type="column" chart-title="चालु आ.वका मासिक निवेदनहरु"></div>
+                    <div class="loading">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="col-lg-6">
             <div class="card">
                 <div class="card-body">
-                    <x-charts.pie-chart-component
-                        id="lawsuit-nature-wise-chart"
-                        chartName="चालु आर्थिक({{ $officeSetting->fiscalYear->title ?? '' }}) वर्षका मुद्दा प्रकृति अनुसारका निवेदनहरु"
-                        :labels="$lawsuitNatureWiseApplications['labels']"
-                        :dataSets="$lawsuitNatureWiseApplications['dataSets']"
-                    />
+                    <div id="lawsuitNatureWiseApplications" chart-type="pie" chart-title="चालु आ.वका मुद्दा प्रकृति अनुसारका निवेदनहरु"></div>
+                    <div class="loading">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div id="fiscalYearWiseApplications" chart-type="column" chart-title="आर्थिक वर्ष अनुसारका निवेदनहरु"></div>
+                    <div class="loading">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <div id="lawsuitNatureWiseApplicationsData" chart-type="column" chart-title="चालु आ.वका मुद्दा प्रकृति (निवेदन स्थिति) अनुसारका निवेदनहरु"></div>
+                    <div class="loading">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border" role="status"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row mt-2">
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-body">
-                    <x-charts.bar-chart-component
-                        id="fiscal-year-applications-chart" chartTitle="आर्थिक वर्ष अनुसारका निवेदनहरु"
-                        :labels="$fiscalYearWiseApplications['labels']"
-                        :dataSets="$fiscalYearWiseApplications['dataSets']"
-                    />
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-body">
-                    <x-charts.bar-chart-component
-                        id="lawsuit-nature-wise-applications-data"
-                        chartTitle="चालु आर्थिक({{ $officeSetting->fiscalYear->title ?? '' }}) वर्षका मुद्दा प्रकृति (निवेदन स्थिति) अनुसारका निवेदनहरु"
-                        :labels="$lawsuitNatureWiseApplicationsData['labels']"
-                        :dataSets="$lawsuitNatureWiseApplicationsData['dataSets']"
-                    />
-                </div>
-            </div>
-        </div>
-    </div>
+    @push('scripts')
+        <script src="{{asset('assets/backend/js/chart/chart.js')}}"></script>
+        <script src="{{asset('assets/backend/js/chart/chart-export.js')}}"></script>
+        <script src="{{asset('assets/backend/js/chart/export-data.js')}}"></script>
+        <script src="{{asset('assets/backend/js/chart/accessibility.js')}}"></script>
+        <script src="{{asset('assets/backend/js/chart/chart.init.js')}}"></script>
+    @endpush
 @endsection
