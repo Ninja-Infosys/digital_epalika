fc45e6b541addc99f17b798fef7cbeaf7d23d912
manoj <manojoli667@gmail.com>
2023-02-27 12:23:32 +0545

    list registration & circular report changes

--- Modules/JudicialCommittee/Http/Controllers/Admin/JudicialReceiptBillController.php
+++ Modules/JudicialCommittee/Http/Controllers/Admin/JudicialReceiptBillController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Str;
 use Modules\JudicialCommittee\Entities\ComplaintApplication;
 use Modules\JudicialCommittee\Entities\JudicialCommitteeTemplate;
 use Modules\JudicialCommittee\Entities\JudicialReceiptBill;
+use Modules\JudicialCommittee\Enums\ComplaintApplicationStatusEnum;
 use Modules\JudicialCommittee\Enums\JudicialTemplateTypeEnum;
 use Modules\JudicialCommittee\Events\ComplaintLogEvent;
 use Modules\JudicialCommittee\Http\Requests\JudicialReceiptBillRequest;
@@ -48,9 +49,10 @@ class JudicialReceiptBillController extends Controller
 
         if ($judicialReceiptBill->wasRecentlyCreated) {
             $complaintApplication->update([
-                'registration_no' => $officeSetting->fiscalYear->title . '-' . ($complaintApplication->lawsuitNature->code ?? '') . '-' . Str::padLeft($complaintApplication->id, 4, 0)
+                'registration_no' => $officeSetting->fiscalYear->title . '-' . ($complaintApplication->lawsuitNature->code ?? '') . '-' . Str::padLeft($complaintApplication->id, 4, 0),
+                'application_status' => ComplaintApplicationStatusEnum::PENDING
             ]);
-            event(new ComplaintLogEvent($complaintApplication->id,JudicialReceiptBill::class,$judicialReceiptBill->id,'दर्ता शुल्क तिरेको',"$judicialReceiptBill->bill_date बिल मितिमा निवेदन दर्ता को लागि दर्ता शुल्क तिरेको"));
+            event(new ComplaintLogEvent($complaintApplication->id, JudicialReceiptBill::class, $judicialReceiptBill->id, 'दर्ता शुल्क तिरेको', "$judicialReceiptBill->bill_date बिल मितिमा निवेदन दर्ता को लागि दर्ता शुल्क तिरेको"));
         }
 
         toast('दर्ता दस्तुर विवरण सफलतापूर्वक अद्यावधिक गरियो', 'success');
