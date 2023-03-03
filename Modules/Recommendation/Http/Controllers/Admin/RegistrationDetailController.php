af835503a34903888a87cfe04d6edc1360e1614d
Tekchan Khatri <tekam7755@gmail.com>
2023-03-03 16:23:37 +0545

    Report

--- Modules/Recommendation/Http/Controllers/Admin/RegistrationDetailController.php
+++ Modules/Recommendation/Http/Controllers/Admin/RegistrationDetailController.php
@@ -129,14 +129,20 @@ class RegistrationDetailController extends Controller
 
    private function getClientFile($request, RegistrationDetail $registrationDetail)
     {
-        foreach ($request->validated()['files']??[] as $file) {
-            $data = $file['file']->store('recommendation_file/' . Str::slug($request->input('date_ne')), 'public');
-            $registrationDetail->files()->create([
-                'file_name' => $file['file_name'],
-                'file' => $data,
-                'extension' => $file['file']->getClientOriginalExtension(),
-                'type' => 'ClientFile'
-            ]);
+        $files_count=collect($request->validated()['files'])->pluck('file')->filter(function ($file){
+            return !is_null($file);
+        })->count();
+
+        if($files_count>0){
+            foreach ($request->validated()['files']??[] as $file) {
+                $data = $file['file']->store('recommendation_file/' . Str::slug($request->input('date_ne')), 'public');
+                $registrationDetail->files()->create([
+                    'file_name' => $file['file_name'],
+                    'file' => $data,
+                    'extension' => $file['file']->getClientOriginalExtension(),
+                    'type' => 'ClientFile'
+                ]);
+            }
         }
     }
 
