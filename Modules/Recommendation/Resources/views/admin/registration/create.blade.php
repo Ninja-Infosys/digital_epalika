4182484ae78310b63993ee06706c55b887df84db
Prakash Dhami <6370420-OopsMissing@users.noreply.gitlab.com>
2023-03-03 16:00:27 +0545

    recomendation

--- Modules/Recommendation/Resources/views/admin/registration/create.blade.php
+++ Modules/Recommendation/Resources/views/admin/registration/create.blade.php
@@ -132,9 +132,9 @@
                                                 </div>
                                                 <div class="row border-bottom mb-2">
                                                     <div class="col-md-6 mb-2">
-                                                        <label for="title" class="form-label">शिर्षक *</label>
+                                                        <label for="title" class="form-label">शिर्षक</label>
                                                         <input type="text" name="files[][file_name]" class="form-control"
-                                                            id="title" placeholder="शिर्षक" required />
+                                                            id="title" placeholder="शिर्षक" />
                                                     </div>
                                                     <div class="col-md-6 mb-2">
                                                         <label for="documents" class="form-label">डकुमेन्ट </label>
