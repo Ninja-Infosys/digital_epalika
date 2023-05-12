8874a2d3e1d579c71cc171c8fe6b5f74a2ac41ab
Manoj Oli <manojoli667@gmail.com>
2023-05-12 12:34:46 +0545

    project document template

--- Modules/Plan/Resources/views/admin/project_document/create.blade.php
+++ Modules/Plan/Resources/views/admin/project_document/create.blade.php
@@ -94,7 +94,7 @@
                             url:$(this).val(),
                             success:function (resp){
                                 CKEDITOR.instances.data.setData(resp.data);
-                                console.log(resp.data)
+                                console.log(resp)
                             },
                             error:function (){
                                 alert('Something Went Wrong')
