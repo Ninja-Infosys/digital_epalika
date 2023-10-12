d9762d77cdfe783582f819c245014450b30db84f
Ninja PC 03 <tekam7755@gmail.com>
2023-10-12 10:47:13 +0545

    FORM

--- Modules/EMap/Http/Controllers/Admin/DocumentAttachController.php
+++ Modules/EMap/Http/Controllers/Admin/DocumentAttachController.php
@@ -60,7 +60,7 @@ class DocumentAttachController extends Controller
                     'form_id' => $form->id,
                     'status' => DocumentStatusEnum::APPROVED->value,
                     'uploaded_by_type' => User::class,
-                    'uploaded_by_id' => auth('auth_id')->user()->id,
+                    'uploaded_by_id' => auth()->user()->id,
                     'form_data_type' => FormDataType::class,
                     'form_data_id' => $formDataType->id,
                     'data' => $data['data'],
@@ -78,14 +78,14 @@ class DocumentAttachController extends Controller
                     'form_id' => $form->id,
                     'status' => DocumentStatusEnum::APPROVED->value,
                     'uploaded_by_type' => User::class,
-                    'uploaded_by_id' => auth('auth_id')->user()->id,
+                    'uploaded_by_id' => auth()->user()->id,
                     'bill' => $data['bill']->store('appliedDocument', 'public'),
                     'amount' => $data['amount'],
                 ]);
             });
             toast('फारम सफलतापूर्वक थपियो', 'success');
         }
-        return redirect(route('emap.admin.mapApply.admin-step.form-detail', [$mapApply, $form]));
+        return redirect(route('emap.admin.mapApply.admin-step.fill-detail', [$mapApply, $form]));
     }
 
 
@@ -95,11 +95,11 @@ class DocumentAttachController extends Controller
         return view('emap::organization.attach-document.documentDetail', compact('appliedDocument'));
     }
 
-
-    public function formStoreDetail(FormStore $formStore)
+    public function formStoreDetail( FormStore $formStore)
     {
         $formStore->load('formStoreStatuses');
-        return view('emap::organization.attach-document.formStoreDetail', compact('formStore'));
+        toast('स्थिति सफलतापूर्वक परिवर्तन गरियो', 'success');
+        return back('',compact('formStore'));
     }
 
     public function printTemplate(MapApply $mapApply, FormDataType $formDataType)
--- Modules/EMap/Routes/admin.php
+++ Modules/EMap/Routes/admin.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\EMap\Entities\New\MapPassGroup;
 use Modules\EMap\Http\Controllers\Admin\DashboardController;
+use Modules\EMap\Http\Controllers\Admin\DocumentAttachController;
 use Modules\EMap\Http\Controllers\Admin\EMapTemplateController;
 use Modules\EMap\Http\Controllers\Admin\MapController;
 use Modules\EMap\Http\Controllers\Admin\MapFeeController;
@@ -26,10 +27,13 @@ Route::put('appliedDocument/{appliedDocument}/updateAppliedDocumentStatus', [Adm
 Route::put('formStore/{formStore}/updateFormStoreStatus', [AdminStepController::class, 'updateFormStoreStatus'])->name('mapApply.admin-step.updateFormStoreStatus');
 Route::put('paymentStore/{paymentStore}/updatePaymentStoreStatus', [AdminStepController::class, 'updatePaymentStoreStatus'])->name('mapApply.admin-step.updatePaymentStoreStatus');
 
+
+
 Route::get('mapApply/{mapApply}/steps', [AdminStepController::class, 'formList'])->name('mapApply.admin-step.form-list');
 Route::get('mapApply/{mapApply}/form/{form}/fill', [AdminStepController::class, 'fillDetail'])->name('mapApply.admin-step.fill-detail');
 Route::get('mapApply/{mapApply}/form/{form}/detail', [AdminStepController::class, 'viewDetail'])->name('mapApply.admin-step.view-detail');
 Route::resource('mapApply/{mapApply}/mapRegistration', MapRegistrationController::class)->names('mapApply.mapRegistration');
+Route::resource('mapApply/{mapApply}/form/{form}/formDataType/{formDataType}/appliedDocument', DocumentAttachController::class);
 
 
 Route::controller(MapController::class)->prefix('map')->as('map.')->group(function () {
