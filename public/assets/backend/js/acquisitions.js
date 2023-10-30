c7f23d8f9b8d0ca6d27fbcc26f430e40abe0b06f
Shishir <raishishir774@gmail.com>
2023-10-30 12:05:52 +0545

    implemented chart.js

---
+++ public/assets/backend/js/acquisitions.js
@@ -0,0 +1,169 @@
+// bar chart
+const barChart1 = document.getElementById('barChart1').getContext('2d');
+
+const barChartData = {
+    labels: ['बैशाख', 'जेठ', 'असार', 'श्रावण', 'भदौ', 'आश्विन', 'कार्तिक', 'मंसिर', 'पुष', 'माघ', 'फाल्गुन', 'चैत्र'],
+    datasets: [
+        {
+            label: 'जम्मा',
+            data: [263, 127, 366, 174, 315, 319, 111, 309, 87, 278, 276, 265],
+            backgroundColor: 'rgba(6, 62, 147, 1)',
+            borderColor: 'rgba(6, 62, 147, 1)',
+            borderWidth: 1,
+        },
+        {
+            label: 'समाचार',
+            data: [43, 78, 328, 163, 274, 255, 104, 291, 86, 252, 168, 221],
+            backgroundColor: 'rgba(233, 1, 22, 1)',
+            borderColor: 'rgba(233, 1, 22, 1)',
+            borderWidth: 1,
+        },
+        {
+            label: 'सूचना',
+            data: [220, 49, 38, 11, 41, 64, 7, 18, 1, 26, 108, 44],
+            backgroundColor: 'rgba(0, 145, 62, 1)',
+            borderColor: 'rgba(0, 145, 62, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
+
+const barChartOptions = {
+    scales: {
+        y: {
+            beginAtZero: true,
+        },
+    },
+};
+
+new Chart(barChart1, {
+    type: 'bar',
+    data: barChartData,
+    options: barChartOptions,
+});
+
+// line chart
+const lineChart = document.getElementById('lineChart1').getContext('2d');
+
+const lineChartData = {
+    labels: ['बैशाख', 'जेठ', 'असार', 'श्रावण', 'भदौ', 'आश्विन', 'कार्तिक', 'मंसिर', 'पुष', 'माघ', 'फाल्गुन', 'चैत्र'],
+    datasets: [
+        {
+            label: 'जम्मा',
+            data: [-19, 45, 92, -74, 61, -21, 18, 28, -78, 83, -104, 64],
+            backgroundColor: 'rgba(6, 62, 147, 1)',
+            borderColor: 'rgba(6, 62, 147, 1)',
+            borderWidth: 1,
+        },
+        {
+            label: 'समाचार',
+            data: [-5, 16, 42, -30, 54, 0, -15, 37, -23, 58, -46, 11],
+            backgroundColor: 'rgba(233, 1, 22, 1)',
+            borderColor: 'rgba(233, 1, 22, 1)',
+            borderWidth: 1,
+        },
+        {
+            label: 'सूचना',
+            data: [-14, 29, 50, -44, 7, -21, 33, -9, -55, 25, -58, 53],
+            backgroundColor: 'rgba(0, 145, 62, 1)',
+            borderColor: 'rgba(0, 145, 62, 1)',
+            borderWidth: 1,
+        },
+    ],
+};
+
+const lineChartOptions = {
+    scales: {
+        y: {
+            beginAtZero: true,
+        },
+    },
+};
+
+new Chart(lineChart, {
+    type: 'line',
+    data: lineChartData,
+    options: lineChartOptions,
+});
+
+// doughnut chart
+const doughnut = document.getElementById('doughNut1').getContext('2d');
+
+const doughnutData = {
+    labels: ['नगदी रशिद', 'मालपोत रशिद'],
+    datasets: [
+        {
+            data: [1234, 987],
+            backgroundColor: ['rgba(6, 62, 147, 1)', 'rgba(233, 1, 22, 1)'],
+            borderColor: ['rgba(6, 62, 147, 1)', 'rgba(233, 1, 22, 1)'],
+            borderWidth: 1,
+        },
+    ],
+};
+
+const doughnutOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+};
+
+new Chart(doughnut, {
+    type: 'doughnut',
+    data: doughnutData,
+    options: doughnutOptions,
+});
+
+// pie chart
+const pieChart = document.getElementById('pieChart1').getContext('2d');
+
+const pieChartData = {
+    labels: ['नगद', 'बैंक'],
+    datasets: [
+        {
+            data: [5000, 2500],
+            backgroundColor: ['rgba(6, 62, 147, 1)', 'rgba(233, 1, 22, 1)'],
+            borderColor: ['rgba(6, 62, 147, 1)', 'rgba(233, 1, 22, 1)'],
+            borderWidth: 1,
+        },
+    ],
+};
+
+const pieChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+};
+
+new Chart(pieChart, {
+    type: 'pie',
+    data: pieChartData,
+    options: pieChartOptions,
+});
+
+// pie chart
+const polarAreChart = document.getElementById('polarAreaChart1').getContext('2d');
+
+const polarAreChartData = {
+    labels: [
+        'सिलाई कटाई',
+        'पशु रोग बारेमा',
+        'च्याउ खेती',
+    ],
+    datasets: [{
+        data: [11, 16, 7],
+        backgroundColor: [
+            'rgba(6, 62, 147, 1)',
+            'rgba(233, 1, 22, 1)',
+            'rgba(0, 145, 62, 1)',
+        ]
+    }]
+};
+
+const polarAreChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+};
+
+new Chart(polarAreChart, {
+    type: 'polarArea',
+    data: polarAreChartData,
+    options: polarAreChartOptions,
+});
