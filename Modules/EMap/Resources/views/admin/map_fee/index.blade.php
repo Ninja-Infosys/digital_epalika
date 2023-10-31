36268d95a6addbe4ef39bed2f880502681d536fe
nikeshstha <nikesh.shrestha@imark.com.np>
2023-10-31 17:41:56 +0545

    fix: 🐛 design fixes on emap

--- Modules/EMap/Resources/views/admin/map_fee/index.blade.php
+++ Modules/EMap/Resources/views/admin/map_fee/index.blade.php
@@ -3,11 +3,13 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
+            <h4 class="page-title mb-0">नक्शा दस्तुर </h4>
+                <div class="">
+                    <ol class="breadcrumb mb-2">
                         <li class="breadcrumb-item">
                             <a href="{{route('admin.dashboard')}}">
-                                <i class="fa fa-home"></i> गृहपृष्ठ
+                               <img class="icon me-1" src="http://127.0.0.1:8000/assets/backend/images/home.svg" alt="document-icon">
+                            गृहपृष्ठ
                             </a>
                         </li>
                         <li class="breadcrumb-item">
@@ -16,7 +18,6 @@
                         <li class="breadcrumb-item active">नक्शा दस्तुर</li>
                     </ol>
                 </div>
-                <h4 class="page-title">नक्शा दस्तुर </h4>
             </div>
         </div>
     </div>
@@ -24,7 +25,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
+                <div class="">
                     <div class="d-flex justify-content-between align-items-center">
                         <h4 class="header-title mb-0">नक्शा दस्तुर सूची</h4>
                         @can('mapFee_create')
@@ -35,9 +36,9 @@
                         @endcan
                     </div>
                 </div>
-                <div class="card-body">
+                <div class="mt-3">
                     <div class="table-responsive">
-                        <table class="table table-sm table-striped table-bordered">
+                        <table class="table table-sm table-striped ">
                             <thead>
                             <tr>
                                 <th>क्र.स</th>
