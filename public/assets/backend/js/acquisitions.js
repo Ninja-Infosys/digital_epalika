430c1d60e0d8903871687d2fac1f5c34979f3e59
nikeshstha <nikesh.shrestha@imark.com.np>
2023-10-31 12:12:30 +0545

    fix: 🐛 fixes design in dashboard

--- public/assets/backend/js/acquisitions.js
+++ public/assets/backend/js/acquisitions.js
@@ -1,6 +1,5 @@
 // bar chart
 const barChart1 = document.getElementById('barChart1').getContext('2d');
-
 const barChartData = {
     labels: ['बैशाख', 'जेठ', 'असार', 'श्रावण', 'भदौ', 'आश्विन', 'कार्तिक', 'मंसिर', 'पुष', 'माघ', 'फाल्गुन', 'चैत्र'],
     datasets: [
@@ -41,6 +40,47 @@ new Chart(barChart1, {
     data: barChartData,
     options: barChartOptions,
 });
+
+// horizontal bar chart 
+
+const barCharthorizontal = document.getElementById('barCharthorizontal').getContext('2d');
+const barChartDataHorizontal = {
+    labels: ['बैशाख', 'जेठ', 'असार', 'श्रावण', 'भदौ', 'आश्विन', 'कार्तिक', 'मंसिर', 'पुष', 'माघ', 'फाल्गुन', 'चैत्र'],
+    datasets: [
+        {
+            data: [263, 127, 366, 174, 315, 319, 111, 309, 87, 278, 276, 265],
+            backgroundColor: 'rgba(6, 62, 147, 1)',
+            borderColor: 'rgba(6, 62, 147, 1)',
+            borderWidth: .5,
+            borderRadius: 8,
+        },
+    ],
+    scales: {
+        x: {
+            grid: {
+                display: false, // Remove x-axis gridlines
+            },
+        },
+        y: {
+            grid: {
+                display: false, // Remove x-axis gridlines
+            },
+        }
+      }
+};
+
+const horizontalbarChartOptions = {
+    responsive: true,
+    maintainAspectRatio: true,
+    indexAxis: 'y'
+};
+
+new Chart(barCharthorizontal, {
+    type: 'bar',
+    data: barChartDataHorizontal,
+    options: horizontalbarChartOptions,
+});
+
 
 // line chart
 const lineChart = document.getElementById('lineChart1').getContext('2d');
@@ -166,4 +206,116 @@ new Chart(polarAreChart, {
     type: 'polarArea',
     data: polarAreChartData,
     options: polarAreChartOptions,
+});
+
+// stepped line chart
+const steppedlineChart = document.getElementById('steppedlineChart').getContext('2d');
+const steppedlineChartData = {
+    labels: ['बैशाख', 'जेठ', 'असार', 'श्रावण', 'भदौ', 'आश्विन', 'कार्तिक', 'मंसिर', 'पुष', 'माघ', 'फाल्गुन', 'चैत्र'],
+    datasets: [
+        {
+            label: 'जम्मा',
+            data: [263, 127, 366, 174, 315, 319, 111, 309, 87, 278, 276, 265],
+            backgroundColor: 'rgba(6, 62, 147, 0.2)', // Change to transparent
+            borderColor: 'rgba(6, 62, 147, 1)',
+            borderWidth: 1,
+            pointRadius: 0,
+            stepped: 'middle', // Use 'middle' for a stepped line chart
+        },
+        {
+            label: 'समाचार',
+            data: [43, 78, 328, 163, 274, 255, 104, 291, 86, 252, 168, 221],
+            backgroundColor: 'rgba(233, 1, 22, 0.2)', // Change to transparent
+            borderColor: 'rgba(233, 1, 22, 1)',
+            borderWidth: 1,
+            pointRadius: 0,
+            stepped: 'middle', // Use 'middle' for a stepped line chart
+        },
+        {
+            label: 'सूचना',
+            data: [220, 49, 38, 11, 41, 64, 7, 18, 1, 26, 108, 44],
+            backgroundColor: 'rgba(0, 145, 62, 0.2)', // Change to transparent
+            borderColor: 'rgba(0, 145, 62, 1)',
+            borderWidth: 1,
+            pointRadius: 0,
+            stepped: 'middle', // Use 'middle' for a stepped line chart
+        },
+    ],
+};
+
+const steppedlineChartOptions = {
+    scales: {
+        y: {
+            beginAtZero: true,
+        },
+    },
+    axis: 'x'
+};
+
+new Chart(steppedlineChart, {
+    type: 'line',
+    data: steppedlineChartData,
+    options: steppedlineChartOptions,
+});
+// bubble chart
+const bubbleChart = document.getElementById('bubbleChart').getContext('2d');
+const randomBubbleSizes = Array.from({ length: 12 }, () => Math.floor(Math.random() * 50) + 10);
+const bubbleChartData = {
+    datasets: [
+        {
+            label: 'जम्मा',
+            data: randomBubbleSizes.map((size, index) => (
+            { x: 'बैशाख', y: 263, r: 10 },
+            { x: 'जेठ', y: 127, r: 10 },
+            { x: 'असार', y: 366, r: 10 },
+            { x: 'श्रावण', y: 174, r: 10 },
+            { x: 'भदौ', y: 315, r: 10 },
+            { x: 'आश्विन', y: 319, r: 10 },
+            { x: 'कार्तिक', y: 111, r: 10 },
+            { x: 'मंसिर', y: 309, r: 10 },
+            { x: 'पुष', y: 87, r: 10 },
+            { x: 'माघ', y: 278, r: 10 },
+            { x: 'फाल्गुन', y: 276, r: 10 },
+            { x: 'चैत्र', y: 265, r: 10 })),
+            backgroundColor: 'rgba(6, 62, 147, 0.2)',
+            borderColor: 'rgba(6, 62, 147, 1)',
+        },
+        {
+            label: 'समाचार',
+            data: [
+                { x: 'बैशाख', y: 63, r: 10 },
+                { x: 'जेठ', y: 12, r: 10 },
+                { x: 'असार', y: 266, r: 10 },
+                { x: 'श्रावण', y: 274, r: 10 },
+                { x: 'भदौ', y: 215, r: 10 },
+                { x: 'आश्विन', y: 119, r: 10 },
+                { x: 'कार्तिक', y: 211, r: 10 },
+                { x: 'मंसिर', y: 209, r: 10 },
+                { x: 'पुष', y: 187, r: 10 },
+                { x: 'माघ', y: 78, r: 10 },
+                { x: 'फाल्गुन', y: 376, r: 10 },
+                { x: 'चैत्र', y: 165, r: 10 }
+            ],
+            backgroundColor: 'rgba(233, 1, 22, 0.2)',
+            borderColor: 'rgba(233, 1, 22, 1)',
+        },
+    ],
+};
+
+const bubbleChartOptions = {
+    scales: {
+        x: {
+            type: 'category', // Specify x-axis type as 'category'
+            beginAtZero: true,
+        },
+        y: {
+            beginAtZero: true,
+        },
+    },
+};
+
+new Chart(bubbleChart, {
+    type: 'bubble',
+    data: bubbleChartData,
+    options: bubbleChartOptions,
 });
