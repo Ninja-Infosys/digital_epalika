209b0dbe4e3a65abdaf5a6840017b038967494b1
Ninja PC 03 <tekam7755@gmail.com>
2023-10-31 17:13:31 +0545

    criteriadetailsetting

---
+++ Modules/EMap/Resources/views/admin/criteriaDetailSetting/create.blade.php
@@ -0,0 +1,136 @@
+@extends('admin.layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('emap.admin.dashboard') }}">
+                                <i class="fa fa-home"></i> गृहपृष्ठ
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active">मापदण्ड</li>
+                        <li class="breadcrumb-item active">नयाँ मापदण्ड</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">नयाँ मापदण्ड</h4>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+                        <h4 class="header-title">नयाँ मापदण्ड </h4>
+                        <a href="{{ route('emap.admin.criteriaDetailSetting.index','') }}"
+                           class="btn btn-sm btn-outline-primary">
+                            <i class="fa fa-list"></i> मापदण्ड सुची
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('emap.admin.criteriaDetailSetting.store')}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6 mb-2">
+                                <label for="title" class="form-label">शीर्षक *</label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    value="{{old('title')}}"
+                                    class="form-control @error('title') is-invalid @enderror"
+                                    id="title"
+                                    placeholder="शीर्षक"
+                                />
+                                @error('title')
+                                <div class="invalid-feedback">{{$message}}</div>
+                                @enderror
+                            </div>
+
+
+                            <div class="col-md-6 mb-2">
+                                <label for="area" class="form-label">क्षेत्र *</label>
+                                <input
+                                    type="number"
+                                    name="area"
+                                    value="{{old('area')}}"
+                                    min="0"
+                                    step="0.01"
+                                    class="form-control @error('area') is-invalid @enderror"
+                                    id="area"
+                                    placeholder="क्षेत्र"
+                                />
+                                @error('rate')
+                                <div class="invalid-feedback">{{$message}}</div>
+                                @enderror
+                            </div>
+
+
+                            <div class="col-md-4 mb-3">
+                                <label for="sign" class="form-label">संकेत</label>
+                                <select class="form-select @error('sign') is-invalid @enderror" name="sign"
+                                    id="sign">
+                                    <option value="">---संकेत छान्नुहोस् ---</option>
+                                    @foreach (\Modules\EMap\Enums\SignEnum::cases() as $sign)
+                                        <option value="{{ $sign->value }}"
+                                            {{ old('sign') == $sign->value ? 'selected' : '' }}>
+                                            {{ $sign->label() }}</option>
+                                    @endforeach
+                                </select>
+                                @error('sign')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-4 mb-2">
+                                <label for="gcr" class="form-label">GCR *</label>
+                                <input
+                                    type="number"
+                                    name="gcr"
+                                    value="{{old('gcr')}}"
+                                    min="0"
+                                    step="0.01"
+                                    class="form-control @error('gcr') is-invalid @enderror"
+                                    id="gcr"
+                                    placeholder="GCR"
+                                />
+                                @error('rate')
+                                <div class="invalid-feedback">{{$message}}</div>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-4 mb-2">
+                                <label for="far" class="form-label">FAR *</label>
+                                <input
+                                    type="number"
+                                    name="far"
+                                    value="{{old('far')}}"
+                                    min="0"
+                                    step="0.01"
+                                    class="form-control @error('far') is-invalid @enderror"
+                                    id="far"
+                                    placeholder="FAR"
+                                />
+                                @error('rate')
+                                <div class="invalid-feedback">{{$message}}</div>
+                                @enderror
+                            </div>
+
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">
+                            Save
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+@endsection
--- Modules/EMap/Routes/admin.php
+++ Modules/EMap/Routes/admin.php
@@ -11,6 +11,7 @@ use Modules\EMap\Http\Controllers\Admin\MapRegistrationController;
 use Modules\EMap\Http\Controllers\Admin\OrganizationController;
 use Modules\EMap\Http\Controllers\AdminStepController;
 use Modules\EMap\Http\Controllers\AttachDocumentController;
+use Modules\EMap\Http\Controllers\CriteriaDetailSettingController;
 use Modules\EMap\Http\Controllers\DynamicFormController;
 use Modules\EMap\Http\Controllers\MapSettingController;
 use Modules\EMap\Http\Controllers\OldMapController;
@@ -77,6 +78,7 @@ Route::prefix('setting')->group(function () {
     Route::get('dynamicForm/{dynamicForm}/template', [DynamicFormController::class, 'template'])->name('dynamicForm.template');
     Route::put('dynamicForm/{dynamicForm}/template', [DynamicFormController::class, 'templateStore'])->name('dynamicForm.template.store');
     Route::resource('dynamicForm', DynamicFormController::class);
+    Route::resource('criteriaDetailSetting', CriteriaDetailSettingController::class);
 });
 
 Route::prefix('files')->as('files.')->group(function () {
