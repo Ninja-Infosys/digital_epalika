c296ad2d4128434d29244e5f12924cc606e0b9cc
Binita <binita@ninjainfosys.com>
2023-10-31 16:27:47 +0545

    LandUseArea

--- Modules/EMap/Routes/admin.php
+++ Modules/EMap/Routes/admin.php
@@ -5,6 +5,7 @@ use Modules\EMap\Entities\New\MapPassGroup;
 use Modules\EMap\Http\Controllers\Admin\DashboardController;
 use Modules\EMap\Http\Controllers\Admin\DocumentAttachController;
 use Modules\EMap\Http\Controllers\Admin\EMapTemplateController;
+use Modules\EMap\Http\Controllers\LandUseAreaController;
 use Modules\EMap\Http\Controllers\Admin\MapController;
 use Modules\EMap\Http\Controllers\Admin\MapFeeController;
 use Modules\EMap\Http\Controllers\Admin\MapRegistrationController;
@@ -77,6 +78,7 @@ Route::prefix('setting')->group(function () {
     Route::get('dynamicForm/{dynamicForm}/template', [DynamicFormController::class, 'template'])->name('dynamicForm.template');
     Route::put('dynamicForm/{dynamicForm}/template', [DynamicFormController::class, 'templateStore'])->name('dynamicForm.template.store');
     Route::resource('dynamicForm', DynamicFormController::class);
+    Route::resource('landUseArea', LandUseAreaController::class);
 });
 
 Route::prefix('files')->as('files.')->group(function () {
@@ -90,3 +92,4 @@ Route::controller(ReportController::class)->prefix('reports')->as('report.')->gr
     Route::get('/', 'getRequiredData')->name('report');
     Route::post('report-data', 'report')->name('report-data');
 });
+
