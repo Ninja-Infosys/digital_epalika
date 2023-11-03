7a19cedbd7085aceb8765014414bf82d1a622533
nikeshstha <nikesh.shrestha@imark.com.np>
2023-11-03 15:48:28 +0545

    fix: 🐛 design fixes on emap on homepage and form fixes

--- Modules/EMap/Resources/views/admin/map/index.blade.php
+++ Modules/EMap/Resources/views/admin/map/index.blade.php
@@ -120,7 +120,7 @@
                         </tr>
                     @empty
                         <tr>
-                            <td class="text-center" colspan="7">तालिकामा कुनै डाटा उपलब्ध छैन !!!</td>
+                            <td class="text-center" colspan="12">तालिकामा कुनै डाटा उपलब्ध छैन !!!</td>
                         </tr>
                     @endforelse
                 </tbody>
--- Modules/EMap/Resources/views/frontend/e-map/form/form.blade.php
+++ Modules/EMap/Resources/views/frontend/e-map/form/form.blade.php
@@ -1,24 +1,22 @@
 @extends('frontend.layouts.master')
 @section('content')
-    <section class="inner-section mt-lg-5 ">
-        <div class="container">
-            <div class="row d-flex mt-5 ">
-                <div class="breadcrumb d-flex">
-                    <div class="breadcrumb-item">
-                        <a class="whitespace-nowrap text-primary-500" href="{{url('e-map')}}">ई-नक्सा</a>
-                        <i class="fa fa-angle-double-right text-light"></i>
-                        <a class=" text-primary-500 text-center">नक्सा दरखास्त फारम</a>
-                    </div>
+<section class="inner-section mt-lg-5 ">
+    <div class="container">
+        <div class="row d-flex mt-5 ">
+            <div class="breadcrumb d-flex">
+                <div class="breadcrumb-item">
+                    <a class="whitespace-nowrap text-primary-500" href="{{url('e-map')}}">ई-नक्सा</a>
+                    <i class="fa fa-angle-double-right text-light"></i>
+                    <a class=" text-primary-500 text-center">नक्सा दरखास्त फारम</a>
                 </div>
-                <h4 class="fw-semibold text-center">नक्सा दरखास्त फारम</h4>
-                <div class="row justify-content-center">
-                        <div class="card">
-                            <div class="card-body">
-                            <livewire:emap::map-application-form/>
-                            </div>
-                        </div>
+            </div>
+            <h4 class="fw-semibold text-center">नक्सा दरखास्त फारम</h4>
+            <div class="row justify-content-center">
+                <div class="p-4">
+                    <livewire:emap::map-application-form />
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
