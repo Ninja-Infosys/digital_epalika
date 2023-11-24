a841a9ecf68a5b8dd90c9bb92ea316da3b51e4b7
Shishir <raishishir774@gmail.com>
2023-11-24 14:29:15 +0545

    Front

--- resources/views/frontend/digital_board.blade.php
+++ resources/views/frontend/digital_board.blade.php
@@ -1,35 +1,3 @@
-{{-- @extends('frontend.layouts.master')
-@section('content')
-    <main>
-        <section class="news-section">
-            <x-frontend.scroll-news-component />
-        </section>
-        <section class="notice mt-3">
-            <div class="row">
-                <div class="col-md-8">
-                    <x-frontend.notice-vertical-slider-component />
-                </div>
-                <div class="col-md-4">
-                    <x-frontend.employee-section-component />
-                </div>
-            </div>
-        </section>
-        <section class="help mt-3">
-            <div class="row">
-                <div class="col-md-8">
-                    <x-frontend.digital-board-video-component />
-                </div>
-                <div class="col-md-4">
-                    <x-frontend.notice-section-component />
-                </div>
-            </div>
-        </section>
-        <section class="modules mt-3">
-            <x-frontend.module-info-component />
-        </section>
-    </main>
-@endsection --}}
-
 @extends('frontend.layouts.master')
 @section('content')
     <main>
--- resources/views/frontend/partials/header_middle.blade.php
+++ resources/views/frontend/partials/header_middle.blade.php
@@ -1,18 +1,3 @@
-{{-- <div class="background" style="background-image: url('{{$officeSetting->background_image_url??''}}')">
-    <div class="container-fluid d-lg-flex justify-content-between align-items-center">
-            <a href="{{route('welcome')}}" class="main-logo">
-                <img alt="nepal-government-logo" class="logo img-responsive center-block d-block mx-auto"
-                     src="{{ asset('assets/frontend/image/logo.png') }}"/>
-            </a>
-        <x-header-component/>
-            <a href="{{route('welcome')}}" class="main-logo">
-                <img alt="nepal-flag" class="logo-nep d-none d-lg-block"
-                     src="{{ asset('assets/frontend/image/nepal_flag.gif') }}"/>
-            </a>
-    </div>
-</div> --}}
-
-
 <div class="background" style="background-image: url('{{ officeSetting()->background_image_url ?? asset('images/bg.png') }}')">
     <div class="container-fluid d-lg-flex justify-content-between align-items-center">
         <div class="d-flex gap-2 align-items-center">
@@ -22,7 +7,8 @@
             </a>
             <x-header-component  :ward="$ward ?? null"/>
         </div>
-        <div>
+        <div class="d-flex align-items-center">
+            <a href="{{route('digital-service')}}" class="me-3  text-white ">विधुतीय शुसासन सेवा</a>
             <a href="{{ route('welcome') }}" class="main-logo d-flex align-items-center">
                 <img alt="nepal-flag" class="logo img-responsive center-block ms-2 "
                     src="{{ asset('assets/frontend/image/nepal_flag.gif') }}" />
