6ab1dba6c6487d129ef68476e65615d0044d6485
bhim bahadur shahi <70481323+bhim1243@users.noreply.github.com>
2023-11-27 10:41:54 +0545

    circular

--- Modules/Circular/Resources/views/admin/dashboard.blade.php
+++ Modules/Circular/Resources/views/admin/dashboard.blade.php
@@ -188,6 +188,6 @@
 
 @push('scripts')
 <script src="{{asset('assets/backend/js/chart.js')}}"></script>
-<script type="module" src="{{asset('assets/backend/js/circular.js')}}"></script>
+<script type="module" src="{{asset('assets/backend/js/acquisitions.js')}}"></script>
 @endpush
 @endsection
