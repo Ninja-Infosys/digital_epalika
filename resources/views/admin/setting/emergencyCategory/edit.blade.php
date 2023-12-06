461d2cee785c35b279c7c01b1c2e52e675066d65
Manoj Oli <121470468+manojoli667@users.noreply.github.com>
2023-12-06 15:34:15 +0545

    Dispatch mail is done and image is added in emergency category

--- resources/views/admin/setting/emergencyCategory/edit.blade.php
+++ resources/views/admin/setting/emergencyCategory/edit.blade.php
@@ -6,13 +6,13 @@
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item">
-                            <a href="{{route('admin.dashboard')}}">
-                               <img class="icon me-1" src="{{asset('assets/backend/images/home.svg')}}" alt="document-icon">
-                            गृहपृष्ठ
+                            <a href="{{ route('admin.dashboard') }}">
+                                <img class="icon me-1" src="{{ asset('assets/backend/images/home.svg') }}" alt="document-icon">
+                                गृहपृष्ठ
                             </a>
                         </li>
                         <li class="breadcrumb-item">
-                            <a href="{{route('admin.setting.dashboard')}}">सेटिङ</a>
+                            <a href="{{ route('admin.setting.dashboard') }}">सेटिङ</a>
                         </li>
                         <li class="breadcrumb-item active">आपतकालिन सेवा सम्पादन गर्नुहोस्</li>
                     </ol>
@@ -27,13 +27,15 @@
                 <div class="card-header">
                     <div class="d-flex justify-content-between">
                         <h4 class="header-title">नयाँ आपतकालिन सेवा थप्नुहोस्</h4>
-                        <a href="{{route('admin.generalSetting.emergencyCategory.index')}}" class="btn btn-sm btn-outline-primary">
+                        <a href="{{ route('admin.generalSetting.emergencyCategory.index') }}"
+                            class="btn btn-sm btn-outline-primary">
                             <i class="fa fa-list"></i> आपतकालिन सेवाको सूची
                         </a>
                     </div>
                 </div>
                 <div class="card-body">
-                    <form action="{{route('admin.generalSetting.emergencyCategory.update',$emergencyCategory)}}" method="post">
+                    <form action="{{ route('admin.generalSetting.emergencyCategory.update', $emergencyCategory) }}" enctype="multipart/form-data"
+                        method="post">
                         @csrf
                         @method('PUT')
 
@@ -43,13 +45,20 @@
                             <div class="col-md-4 mb-2">
                                 <label for="title" class="form-label">शिर्षक आबस्यक छ *</label>
                                 <input id="title" type="text" name="title" placeholder="शिर्षक आबस्यक छ"
-                                       class="form-control @error('title') is-invalid @enderror"
-                                       value="{{old('title', $emergencyCategory->title)}}">
+                                    class="form-control @error('title') is-invalid @enderror"
+                                    value="{{ old('title', $emergencyCategory->title) }}">
                                 @error('title')
-                                <div class="text-danger">{{$message}}</div>
+                                    <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
+                            <div class="col-md-6 mb-2">
+                                <label for="image">फोटो </label>
+                                <input type="file" name="image" id="image" class="form-control" >
+                                @error('image')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
 
+                            </div>
 
                         </div>
                         <button type="submit" class="btn btn-primary">
