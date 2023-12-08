6bf447ce700156b12b30656a458d9adfda87a4fd
Shishir <raishishir774@gmail.com>
2023-12-08 12:20:36 +0545

    Mobile User

--- Modules/EMap/Http/Controllers/AdminStepController.php
+++ Modules/EMap/Http/Controllers/AdminStepController.php
@@ -35,7 +35,7 @@ class AdminStepController extends Controller
         $form->load('formDataTypes.model', 'formDataTypes.appliedDocuments.appliedDocumentStatuses', 'formDataTypes.formStores.formStoreStatuses', 'group');
         $checkUser =  $form->group->users->pluck('id')->contains(auth()->user()->id);
         $MapGroups = DB::table('map_pass_group_user')->where('user_id', auth()->user()->id)->first() ?? null;
-        $ward_no =  $MapGroups ? explode(',', $MapGroups->ward_no) : [];
+        $ward_no =  $MapGroups ? explode(',', $MapGroups?->ward_no ?? '') : [];
         $checkAuthorization = in_array($mapApply->landDetail?->ward_no, $ward_no);
 
 
