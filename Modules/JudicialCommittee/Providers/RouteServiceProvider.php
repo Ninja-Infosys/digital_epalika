8a2ead53e784d9ce0be351d710b542a5096040ec
Shishir <raishishir774@gmail.com>
2023-12-17 14:51:31 +0545

    judicial committee

--- Modules/JudicialCommittee/Providers/RouteServiceProvider.php
+++ Modules/JudicialCommittee/Providers/RouteServiceProvider.php
@@ -24,27 +24,27 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
             ->prefix('judicial-committee')
             ->as('judicialCommittee.')
-            ->group(module_path('JudicialCommittee', '/Routes/web.php'));
+            ->group(module_path('JudicialCommittee', 'Routes/web.php'));
 
         Route::middleware(['web', 'auth.lock', 'auth:sanctum', 'checkRoleMiddleware','checkPinMiddleware'])
             ->prefix('admin/judicialcommittee')
             ->as('admin.judicialCommittee.')
-            ->group(module_path('JudicialCommittee', '/Routes/admin.php'));
+            ->group(module_path('JudicialCommittee', 'Routes/admin.php'));
     }
 
     protected function mapApiRoutes()
     {
         Route::prefix('api')
             ->middleware('api')
-            ->group(module_path('JudicialCommittee', '/Routes/api.php'));
+            ->group(module_path('JudicialCommittee', 'Routes/api.php'));
 
         Route::prefix('api/JudicialCommittee/user')
             ->middleware(['api', 'auth:sanctum'])
-            ->group(module_path('JudicialCommittee', '/Routes/api/private_api.php'));
+            ->group(module_path('JudicialCommittee', 'Routes/api/private_api.php'));
 
         Route::prefix('api/v1/complainRegistration')
             ->middleware('api')
-            ->group(module_path('JudicialCommittee', '/Routes/Api/public_api.php'));
+            ->group(module_path('JudicialCommittee', 'Routes/Api/public_api.php'));
 
     }
 }
