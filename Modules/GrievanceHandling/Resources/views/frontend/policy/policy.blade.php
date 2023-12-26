73f09c7ce14969895ef521af8a4c58d06b6da7ab
Manoj Adhikari <adhikarimanoj16@gmail.com>
2023-12-26 14:08:24 +0545

    digital epalika design started complete the grevience module

--- Modules/GrievanceHandling/Resources/views/frontend/policy/policy.blade.php
+++ Modules/GrievanceHandling/Resources/views/frontend/policy/policy.blade.php
@@ -1,18 +1,32 @@
 @extends('frontend.layouts.master')
 @section('content')
-    <section class="inner-section mt-lg-5 ">
+    <section class="inner-section">
+        <div class="breadcrumb d-flex pt-2">
         <div class="container">
-            <div class="row d-flex mt-5 ">
-                <div class="breadcrumb d-flex">
+            <div class="row">
+                <div class="col-md-12">
                     <div class="breadcrumb-item">
                         <a class="whitespace-nowrap text-primary-500" href="{{route('grievanceHandling.grievance')}}">गुनासो</a>
-                        <i class="fa fa-angle-double-right ml-lg-1 text-light"></i><a class="ml-1 text-primary-500">नीति सुची</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-chevron-double-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z" />
+                            <path fill-rule="evenodd"
+                                d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                        <a class="ml-1 text-primary-500">नीति सुची</a>
                     </div>
                 </div>
-                <h4 class="text-center">नीतिहरु</h4>
-                <p class="text-center">तल दिएको नीति फर्म पुरा पढनुहोस् र आफुले चाहेको नीति डाउनलोड गर्नुहोस्। </p>
-                <div class="bg-card shadow rounded overflow-hidden">
-                    <table class="table table-bordered">
+            </div>
+        </div>
+    </div>
+        <div class="container">
+            <div class="row d-flex mt-5 ">
+                <div class="mx-auto col-md-7">
+                <h3 class="text-left fw-bold">नीतिहरु</h3>
+                <p class="text-left fs-6 mb-3">तल दिएको नीति फर्म पुरा पढनुहोस् र आफुले चाहेको नीति डाउनलोड गर्नुहोस्। </p>
+                <div class="table-responsive">
+                    <table class="table table-custom">
                         <thead>
                         <tr class="fs-5">
                             <th scope="col">क्र.स.</th>
@@ -22,14 +36,24 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
+                        <tr valign="middle">
                             <th scope="row">१</th>
                             <td>विपन्‍न बस्तीमा मुख्यमन्त्री कार्यक्रम सञ्‍चालन मापदण्ड, २०७८</td>
                             <td>२०७९/०२/११</td>
                             <td>
                                 <div class="d-flex justify-content-around">
-                                    <button class="btn btn-sm btn-primary bg-primary text-white" href=""><i class="fa fa-eye"></i></button>
-                                    <button class="btn btn-sm btn-primary bg-primary text-white" href=""><i class="fa fa-download"></i></button>
+                                    <a class="btn btn-outline-dark btn-sm" href="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+</svg>
+</a>
+                                    <a class="btn btn-outline-dark btn-sm" href="">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg>
+</a>
                                 </div>
                             </td>
                         </tr>
@@ -37,6 +61,7 @@
                     </table>
                 </div>
             </div>
+</div>
         </div>
     </section>
 @endsection
