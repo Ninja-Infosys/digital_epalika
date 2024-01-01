f1af773b8d72a6724869b01558b8695343836916
Binita <binita@ninjainfosys.com>
2024-01-01 13:16:43 +0545

    change

--- Modules/Grant/Resources/views/admin/report/farmer/index.blade.php
+++ Modules/Grant/Resources/views/admin/report/farmer/index.blade.php
@@ -172,7 +172,6 @@
 
     @push('scripts')
         <script src="{{asset('assets/backend/js/ajaxCall.js')}}"></script>
-        <script src="{{ asset('assets/backend/print/print.min.js') }}"></script>
 
     @endpush
 @endsection
