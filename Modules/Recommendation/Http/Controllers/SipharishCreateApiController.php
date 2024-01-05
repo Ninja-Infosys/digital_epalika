2df82be7b9ac4ffa85f1912e1ceb41e1b7f590a8
Shishir Rai <raishishir774@gmail.com>
2024-01-05 12:00:10 +0545

    Changes

--- Modules/Recommendation/Http/Controllers/SipharishCreateApiController.php
+++ Modules/Recommendation/Http/Controllers/SipharishCreateApiController.php
@@ -70,11 +70,13 @@ class SipharishCreateApiController extends Controller
             ) {
 
                 foreach ($request->validated()['fields'] as $field) {
-
+                    $value = '';
                     if (!empty($field['type']) && $field['type'] == 'image') {
                         Log::debug($field);
-                        $value = Storage::disk('public')
-                            ->putFile('recommendation/files', $field['value']);
+                        if (!empty($field['value'])) {
+                            $value = Storage::disk('public')
+                                ->putFile('recommendation/files', $field['value']);
+                        }
                     } elseif (!empty($field['type']) && $field['type'] == 'table') {
                         $values = collect();
                         if (!empty($field['table'])) {
