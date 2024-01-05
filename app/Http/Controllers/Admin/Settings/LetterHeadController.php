e97d6a08241160fdfbccafe4f0c71720ccbf192d
Shishir Rai <raishishir774@gmail.com>
2024-01-05 12:27:25 +0545

    Revert "Changes from setting to global"
    This reverts commit 44a825a77ad374a1cfc97db7ee410114dcd8735b.

--- app/Http/Controllers/Admin/Settings/LetterHeadController.php
+++ app/Http/Controllers/Admin/Settings/LetterHeadController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Setting;
+namespace App\Http\Controllers\Admin\Settings;
 
 use App\Http\Controllers\Controller;
 use App\Models\Settings\LetterHead;
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -2,8 +2,7 @@
     <div class="container-fluid">
         <ul class="list-unstyled topnav-menu float-end mb-0 d-flex justify-content-center align-items-center">
             <li class="d-none d-xl-block">
-                <h4 class="text-light top-heading mb-0" id="fiscalyear-tour"><span class="arthik-barsa-badge"><i
-                            class="lnr lnr-calendar-full"></i>आर्थिक
+                <h4 class="text-light top-heading mb-0" id="fiscalyear-tour"><span class="arthik-barsa-badge"><i class="lnr lnr-calendar-full"></i>आर्थिक
                     वर्ष: {{$officeSetting->fiscalYear->title??''}}</span></h4>
             </li>
             <li class="dropdown d-none d-lg-inline-block">
@@ -34,8 +33,7 @@
                                 <h6 class="m-0 font-16 fw-semibold"> नोटिफिकेसन</h6>
                             </div>
                             <div class="col-auto">
-                                <a href="{{route('admin.notification.readAllNotification')}}"
-                                   class="text-dark text-decoration-underline">
+                                <a href="{{route('admin.notification.readAllNotification')}}" class="text-dark text-decoration-underline">
                                     <small>सबै खाली गर्नुहोस्</small>
                                 </a>
                             </div>
@@ -106,7 +104,7 @@
                         <i class="fa fa-tasks"></i>
                         <span>गतिविधिहरू</span>
                     </a>
-                    <a href="{{route('admin.global.dashboard')}}" class="dropdown-item notify-item">
+                    <a href="{{route('admin.setting.dashboard')}}" class="dropdown-item notify-item">
                         <i class="fa fa-spin fa-cog"></i>
                         <span>सेटिङ</span>
                     </a>
@@ -173,11 +171,11 @@
                 </h3>
             </li>
             @if(config('app.app_is_demo'))
-                <li class="d-none d-xl-block ms-2">
-                    <p class="top-heading text-danger">
-                        नोट: यो डेमो भर्जनमा सबै फिचर उपलब्ध गराइएको छैन ।
-                    </p>
-                </li>
+            <li class="d-none d-xl-block ms-2">
+                <p class="top-heading text-danger">
+                    नोट: यो डेमो भर्जनमा सबै फिचर उपलब्ध गराइएको छैन ।
+                </p>
+            </li>
             @endif
             <li class="d-none d-xl-block ms-1">
                 <p class="top-heading text-danger">
