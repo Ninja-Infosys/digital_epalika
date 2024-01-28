a60c3bbd84140cca709edeb0956d1e5a972f58d5
sush1l <sushilkc188@gmail.com>
2024-01-28 21:22:10 +0545

    change

--- Modules/EMap/Routes/organization/admin.php
+++ Modules/EMap/Routes/organization/admin.php
@@ -1,35 +1,13 @@
 <?php
 
-use App\Http\Controllers\Admin\Global\OrganizationAuthController;
-use App\Http\Controllers\Admin\Global\OrganizationDashboardController;
-use App\Http\Controllers\Admin\Global\RenewedController;
-use App\Http\Controllers\Admin\Global\TaxClearanceController;
+
 use Illuminate\Support\Facades\Route;
 use Modules\EMap\Http\Controllers\AttachDocumentController;
 use Modules\EMap\Http\Controllers\Clients\MapApplyController;
 use Modules\EMap\Http\Controllers\OrganizationNotificationController;
 
 // Route::get('dashboard', OrganizationDashboardController::class)->name('dashboard');
-Route::get('dashboard', OrganizationDashboardController::class)->name('dashboard');
-Route::prefix('organization')->as('organization.')->group(function () {    
-    // Route::get('login', [OrganizationAuthController::class, 'showOrganizationLoginForm'])->name('login.form');
-    Route::post('login', [OrganizationAuthController::class, 'organizationLogin'])->name('login');
-    Route::get('register', [OrganizationAuthController::class, 'showOrganizationRegisterForm'])->name('register.form');
-    Route::get('register-person', [OrganizationAuthController::class, 'showOrganizationRegisterFormPerson'])->name('register.formPerson');
-    
-    Route::get('{organization}/invitation', [OrganizationAuthController::class, 'invitation'])->name('invitation');
-    Route::get('password/create', [OrganizationAuthController::class, 'create'])->name('password.create')->middleware(['password.check']);
-    Route::post('password/store', [OrganizationAuthController::class, 'store'])->name('password.store')->middleware(['password.check']);
-    Route::prefix('profile')->group(function () {
-        Route::get('/', [OrganizationAuthController::class, 'profile'])->name('auth-organization.profile');
-    });
-    Route::middleware("auth:organization")->group(function(){
-        Route::post('logout', [OrganizationAuthController::class, 'logout'])->name('logout');
-        Route::resource('taxClearance', TaxClearanceController::class);
-        Route::resource('renewed', RenewedController::class);
-    });
-   
-});
+
 
 
 Route::controller(MapApplyController::class)->group(function () {
