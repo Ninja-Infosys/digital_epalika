7223ea13f5b313ef7b6c953a834e0a88d28b6557
anjali sharma <114386240+Anjali12234@users.noreply.github.com>
2024-02-03 14:12:21 +0545

    lw

--- Modules/EMap/Resources/views/livewire/form-data-type-livewire.blade.php
+++ Modules/EMap/Resources/views/livewire/form-data-type-livewire.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div class="col-md-3 mb-2">
             <label for="map_pass_group_id" class="form-label">फारम भर्ने समूह</label>
-            <select id="map_pass_group_id" name="map_pass_group_id" wire:model="form.map_pass_group_id" class="form-select" required>
+            <select id="map_pass_group_id" name="map_pass_group_id" wire:model="form.map_pass_group_id" class="form-select">
                 <option value="">-- छान्नुहोस् --</option>
                 @foreach($mapPassGroups as $mapPassGroup)
                 <option value="{{$mapPassGroup->id}}" {{old('map_pass_group_id') == $mapPassGroup->id ? 'selected' : ''}}>{{$mapPassGroup->title}}</option>
