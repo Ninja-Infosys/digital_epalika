324e0c471cfc02a7ff8aa3091f20858fdb4f69f2
altaafali092 <altafali092@gmail.com>
2024-02-09 16:54:46 +0545

    commit

--- Modules/Estimate/Routes/admin.php
+++ Modules/Estimate/Routes/admin.php
@@ -5,3 +5,22 @@ use Modules\Estimate\Http\Controllers\Admin\DashboardController;
 
 Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
 Route::get('dashboard/ajax', [DashboardController::class, 'ajaxData'])->name('dashboard.ajax');
+Route::prefix('estimateSetting')->group(function () {
+    Route::resource('labour', LabourController::class);
+    Route::resource('labourRate', LabourRateController::class);
+    Route::resource('fuel', FuelController::class);
+    Route::resource('fuelRate', FuelRateController::class);
+    Route::resource('equipment', EquipmentController::class);
+    Route::get('equipmentAdditionalCost/create', [EquipmentAdditionalCostController::class, 'create'])->name('equipmentAdditionalCost.create');
+    Route::get('equipmentAdditionalCost', [EquipmentAdditionalCostController::class, 'index'])->name('equipmentAdditionalCost.index');
+    Route::get('equipment/{equipment}', [EquipmentAdditionalCostController::class, 'edit'])->name('equipmentAdditionalCost.edit');
+    Route::delete('equipment/{equipment}/delete', [EquipmentAdditionalCostController::class, 'destroy'])->name('equipmentAdditionalCost.delete');
+
+    Route::resource('fuelDemand', FuelDemandController::class);
+    Route::resource('crewRate', CrewRateController::class);
+    Route::resource('materialType', MaterialTypeController::class);
+    Route::resource('material', MaterialController::class);
+    Route::resource('materialRate', MaterialRateController::class);
+    Route::resource('materialCollection', MaterialCollectionController::class);
+    Route::resource('cargoHandling', CargoHandlingController::class);
+});
--- Modules/Plan/Routes/admin.php
+++ Modules/Plan/Routes/admin.php
@@ -66,25 +66,7 @@ Route::prefix('setting')->group(function () {
     Route::resource('expenseHead', ExpenseHeadController::class);
 });
 
-Route::prefix('estimateSetting')->group(function () {
-    Route::resource('labour', LabourController::class);
-    Route::resource('labourRate', LabourRateController::class);
-    Route::resource('fuel', FuelController::class);
-    Route::resource('fuelRate', FuelRateController::class);
-    Route::resource('equipment', EquipmentController::class);
-    Route::get('equipmentAdditionalCost/create', [EquipmentAdditionalCostController::class, 'create'])->name('equipmentAdditionalCost.create');
-    Route::get('equipmentAdditionalCost', [EquipmentAdditionalCostController::class, 'index'])->name('equipmentAdditionalCost.index');
-    Route::get('equipment/{equipment}', [EquipmentAdditionalCostController::class, 'edit'])->name('equipmentAdditionalCost.edit');
-    Route::delete('equipment/{equipment}/delete', [EquipmentAdditionalCostController::class, 'destroy'])->name('equipmentAdditionalCost.delete');
 
-    Route::resource('fuelDemand', FuelDemandController::class);
-    Route::resource('crewRate', CrewRateController::class);
-    Route::resource('materialType', MaterialTypeController::class);
-    Route::resource('material', MaterialController::class);
-    Route::resource('materialRate', MaterialRateController::class);
-    Route::resource('materialCollection', MaterialCollectionController::class);
-    Route::resource('cargoHandling', CargoHandlingController::class);
-});
 
 //report
 Route::controller(ReportController::class)->prefix('report')->as('report.')->group(function () {
