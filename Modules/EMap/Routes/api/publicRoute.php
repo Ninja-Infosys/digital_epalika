ac31bc46a60a41ae81dd314306f9015d0a1e71dd
ninjainfosys78 <technical.ninjainfosys@gmail.com>
2024-02-19 14:09:56 +0545

    Front Map Apply Form

--- Modules/EMap/Routes/api/publicRoute.php
+++ Modules/EMap/Routes/api/publicRoute.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\EMap\Http\Controllers\Admin\Api\MapApplyFormApiController;
+use Modules\EMap\Http\Controllers\Api\MapApplicationController;
 
 Route::get("mapApplySetting", [MapApplyFormApiController::class, 'getMapApplySetting'])->name('get-map-apply-setting');
 
 //register map application from frontend
-Route::post('map-application', [\Modules\EMap\Http\Controllers\Api\MapApplicationController::class,'registerApplication'])->name('register-application');
+Route::post('map-application', [MapApplicationController::class,'registerApplication'])->name('register-application');
