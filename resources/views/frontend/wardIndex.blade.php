616864eee585b7da0d9380c8df99c93c9a834970
Sush1l <sushilkc188@gmail.com>
2024-02-23 10:25:11 +0545

    change

--- resources/views/frontend/wardIndex.blade.php
+++ resources/views/frontend/wardIndex.blade.php
@@ -1,37 +1,33 @@
 @extends('frontend.layouts.master')
 @section('content')
-<main>
-    <section>
-        <x-frontend.scroll-news-component />
-    </section>
-    <section class="notice mt-1">
-        <div class="row">
-            <div class="col-md-9">
-                <div class="table-1">
-                    <h2 class="heading">नागरिक वडापत्र</h2>
-                    <x-frontend.citizen-charter-component :ward="$ward" />
+    <main>
+        <section>
+            <x-frontend.scroll-news-component/>
+        </section>
+        <section class="notice mt-1">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="table-1">
+                        <h2 class="heading text-white px-2 text-center">नागरिक वडापत्र</h2>
+                        <x-frontend.citizen-charter-component/>
+                    </div>
                 </div>
-                {{-- <!-- <div class="table-2">
-                        <h2 class="heading">करका दायराहरु</h2>
-                        <x-frontend.revenue-component  :ward="$ward"/>
-                    </div> --> --}}
-            </div>
-            <div class="col-md-3">
-                <div class="row">
-                    <div class=" mb-2 video-container">
-                        <x-frontend.digital-board-video-component :ward="$ward" />
-                    </div>
-                    <div class="">
-                        <h2 class="heading">सूचना र परिपत्र</h2>
-                        <x-frontend.notice-vertical-slider-component :ward="$ward" />
-                    </div>
-                    <div class="">
-                        <h2 class="heading">जनप्रतिनिधि/कर्मचारी</h2>
-                        <x-frontend.employee-section-component :ward="$ward" />
+                <div class="col-md-4">
+                    <div class="row">
+                        <div class="video-container">
+                            <x-frontend.digital-board-video-component/>
+                        </div>
+                        <div class="">
+                            <h2 class="sub-heading text-white text-center px-2 mb-0">कार्यक्रमहरु</h2>
+                            <x-frontend.program-component/>
+                        </div>
+                        <div class="">
+                            <h2 class="jana text-center text-white px-2 mt-1 mb-0">जिम्मेवार पदाधिकारी/कर्मचारी</h2>
+                            <x-frontend.employee-section-component/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-</main>
+        </section>
+    </main>
 @endsection
