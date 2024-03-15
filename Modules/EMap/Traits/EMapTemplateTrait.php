ba6882c11c4d4611efa30ca43ccb6d49698b4f78
anjali sharma <114386240+Anjali12234@users.noreply.github.com>
2024-03-15 15:34:04 +0545

    commit

--- Modules/EMap/Traits/EMapTemplateTrait.php
+++ Modules/EMap/Traits/EMapTemplateTrait.php
@@ -326,7 +326,7 @@ trait EMapTemplateTrait
             '[@houseOwner.ward_no]' => $this->get_nepali_number($this->houseOwner?->ward_no) ?? '',
             '[@houseOwner.district]' => $this->get_nepali_number($this->houseOwner?->district?->district) ?? '',
             '[@houseOwner.tole]' => $this->get_nepali_number($this->houseOwner?->tole) ?? '',
-            '[@houseOwner.photo]' => '<img src="' . $this->get_nepali_number($this->houseOwner?->photo) . '" alt="House Owner Photo">' ?? '',
+            '[@houseOwner.photo]' => '<img src="' . ($this->houseOwner?->photo) . '" alt="House Owner Photo">' ?? '',
         ];
     }
 
