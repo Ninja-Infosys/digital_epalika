0ae5e5c45f4e5c2e03e7f692c66d752b558af208
Anjali12234 <anjisharma649@gmail.com>
2024-03-15 14:57:21 +0545

    commit

--- Modules/EMap/Traits/EMapTemplateTrait.php
+++ Modules/EMap/Traits/EMapTemplateTrait.php
@@ -90,6 +90,7 @@ trait EMapTemplateTrait
                 'पालिका' => '[@houseOwner.local_body]',
                 'वडा नं' => '[@houseOwner.ward_no]',
                 'टोल' => '[@houseOwner.tole]',
+                'फोटो' => '[@houseOwner.photo]',
             ],
         ],
         [
@@ -325,6 +326,7 @@ trait EMapTemplateTrait
             '[@houseOwner.ward_no]' => $this->get_nepali_number($this->houseOwner?->ward_no) ?? '',
             '[@houseOwner.district]' => $this->get_nepali_number($this->houseOwner?->district?->district) ?? '',
             '[@houseOwner.tole]' => $this->get_nepali_number($this->houseOwner?->tole) ?? '',
+            '[@houseOwner.photo]' => '<img src="' . $this->get_nepali_number($this->houseOwner?->photo) . '" alt="House Owner Photo">' ?? '',
         ];
     }
 
