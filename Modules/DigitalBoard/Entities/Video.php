6cd34c14dfa8737dc05cad95fd53eac3b072462e
Bin1ta <beenitarzmii456@gmail.com>
2024-03-18 13:12:28 +0545

    index page

--- Modules/DigitalBoard/Entities/Video.php
+++ Modules/DigitalBoard/Entities/Video.php
@@ -36,11 +36,11 @@ class Video extends Model
         $builder->where('is_displayed', $display);
     }
 
-//    protected function ward(): Attribute
-//    {
-//        return Attribute::make(
-//            get: fn (string $value) => explode(',', $value),
-//            set: fn (string|array|null $value) => !empty($value) ? is_array($value) ? implode(',', $value) : $value : null,
-//        );
-//    }
+    protected function ward(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => explode(',', $value),
+            set: fn (string|array|null $value) => !empty($value) ? is_array($value) ? implode(',', $value) : $value : null,
+        );
+    }
 }
