1942db51fe35013e24c114be756190f2f663eb3a
Bin1ta <beenitarzmii456@gmail.com>
2024-03-21 15:40:43 +0545

    search

--- Modules/EMap/Entities/OldMap.php
+++ Modules/EMap/Entities/OldMap.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\EventObserveTrait;
 use Modules\EMap\Enums\CategorizationEnum;
@@ -49,5 +50,6 @@ class OldMap extends Model
     {
         return $this->belongsToMany(HouseOwner::class);
     }
+
 
 }
