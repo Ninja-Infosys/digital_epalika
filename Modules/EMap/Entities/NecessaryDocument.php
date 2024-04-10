aafd4adb96980a3775604f6751948cf95985d843
altaafali092 <altafali092@gmail.com>
2024-04-10 12:27:08 +0545

    document

--- Modules/EMap/Entities/NecessaryDocument.php
+++ Modules/EMap/Entities/NecessaryDocument.php
@@ -7,57 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Traits\EventObserveTrait;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class NecessaryDocument extends Model
 {
-    use HasFactory, SoftDeletes, EventObserveTrait;
+    use HasFactory,SoftDeletes,EventObserveTrait;
 
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'deleted_at'
-    ];
+   protected $dates = [
+       'created_at',
+       'updated_at',
+       'deleted_at'
+   ];
 
-    protected $fillable = [
-
-        'title',
-        'description',
-       
-    ];
-
-    public function files():MorphMany
-    {
-        return $this->MorphMany(File::class,'model');
-    }
-
-
-    // public function setAttribute($key, $value)
-    // {
-    //     if ($key === 'files' && is_array($value)) {
-    //         // Store file paths as JSON string
-    //         $value = json_encode($this->uploadFiles($value));
-    //     }
-
-    //     parent::setAttribute($key, $value);
-    // }
-
-    // // Method to handle file upload and return file paths
-    // private function uploadFiles(array $files): array
-    // {
-    //     $filePaths = [];
-
-    //     foreach ($files as $file) {
-    //         $filePaths[] = $file->store('public/necessaryDocument');
-    //     }
-
-    //     return $filePaths;
-    // }
-
-    // // Accessor to retrieve file URLs
-    // public function getFilesAttribute($value): array
-    // {
-    //     return json_decode($value, true) ?? [];
-    // }
+   protected $fillable = [
+    'title',
+    'description',
+   ];
+   public function files():MorphMany
+   {
+       return $this->MorphMany(File::class,'model');
+   }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -116,14 +116,14 @@ class User extends Authenticatable
 
     public function getSignaturePhotoPathUrlAttribute(): string
     {
-        return $this->attributes['signature_photo_path']
-
+        return $this->attributes['signature_photo_path'];
+    }
     public function getSignaturePhotoUrlAttribute(): string
     {
         return $this->attributes['profile_photo_path']
 
             ? Storage::disk('public')->url($this->attributes['signature_photo_path'])
-            : '';
+            : '' ;
     }
 
     public function setSignaturePhotoPathAttribute($value): void
