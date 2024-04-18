d5e1c08041bfdc18e9b5befb186c8635b90ca73e
Sush1l <sushilkc188@gmail.com>
2024-04-18 13:23:24 +0545

    emap

--- Modules/EMap/Database/Migrations/2022_09_28_112132_create_storey_details_table.php
+++ Modules/EMap/Database/Migrations/2022_09_28_112132_create_storey_details_table.php
@@ -11,10 +11,10 @@ return new class () extends Migration {
             $table->id();
             $table->foreignId('map_apply_id')->constrained()->cascadeOnDelete();
             $table->foreignId('map_fee_id')->constrained()->cascadeOnDelete();
-            $table->string('area_of_proposed_construction', 12, 2)->default(0)->comment('प्रस्तावित निर्माण क्षेत्र');
-            $table->string('area_of_former_construction', 12, 2)->default(0)->comment('पूर्व निर्माण क्षेत्र');
-            $table->string('total_area', 12, 2)->default(0)->comment('कुल क्षेत्रफल');
-            $table->string('height', 12, 2)->default(0)->comment('उचाइ');
+            $table->string('area_of_proposed_construction')->nullable()->comment('प्रस्तावित निर्माण क्षेत्र');
+            $table->string('area_of_former_construction')->nullable()->comment('पूर्व निर्माण क्षेत्र');
+            $table->string('total_area')->nullable()->comment('कुल क्षेत्रफल');
+            $table->string('height')->nullable()->comment('उचाइ');
             $table->timestamps();
             $table->softDeletes();
         });
