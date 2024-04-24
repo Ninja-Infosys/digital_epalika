828c50b475f0253ab37f80bfdb374907845b77ca
Anjali12234 <anjisharma649@gmail.com>
2024-04-24 14:51:19 +0545

    commit

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -59,7 +59,7 @@ class FrontController extends Controller
     }
 
 
-    public function notice($ward)
+    public function notice()
     {
         $notices = Notice::where('type', 'Notice')->orderBy('date')->get();
 
--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -7,7 +7,7 @@
             </div>
         @endforeach
     </div>
-    @if ($hasClock)
+    @if($hasClock)
         <div class="main-date d-none d-lg-block">
             <h6><i class="fa fa-calendar"></i>
                 <x-convert-to-unicode number="{{$year}}" id="today_year"></x-convert-to-unicode>
@@ -24,16 +24,16 @@
 
 <div class="main-heading">
     <h2 class="sub-title mb-1">
-        @foreach ($headers as $header)
-            @if ($loop->first)
-                {{ $header->title }}
-            @else
-                <span class="d-block">{{ $header->title }}</span>
+        @foreach($headers as $header)
+            @if($loop->first)
+                    {{$header->title}}
+                @else
+                <span class="d-block">{{$header->title}}</span>
             @endif
         @endforeach
 
-    </h2>
-    {{--        <a class="link-btn mt-2 text-decoration-none" href="tel:{{officeSetting($ward ?? null)?->phone ?? ''}}"> --}}
-    {{--            फोन नं - {{officeSetting($ward ?? null)?->phone ?? ''}} --}}
-    {{--        </a> --}}
+        </h2>
+{{--        <a class="link-btn mt-2 text-decoration-none" href="tel:{{officeSetting($ward ?? null)?->phone ?? ''}}">--}}
+{{--            फोन नं - {{officeSetting($ward ?? null)?->phone ?? ''}}--}}
+{{--        </a>--}}
 </div>
