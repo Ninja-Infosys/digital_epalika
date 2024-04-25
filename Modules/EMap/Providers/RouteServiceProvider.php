31b8145d79039f5f57fef1398d178257fbd3eaf7
ninjainfosys78 <technical.ninjainfosys@gmail.com>
2024-04-25 10:29:15 +0545

    Logging

--- Modules/EMap/Providers/RouteServiceProvider.php
+++ Modules/EMap/Providers/RouteServiceProvider.php
@@ -47,7 +47,10 @@ class RouteServiceProvider extends ServiceProvider
             ->prefix('ebps/api/v1/user')
             ->group(module_path('EMap', '/Routes/api/privateRoute.php'));
 
-        Route::middleware(['api', 'auth:organization'])
+        Route::middleware([
+            'api',
+//            'auth:organization'
+        ])
             ->prefix('api/organization/admin')
             ->as('api.organization.admin.')
             ->group(module_path('EMap', '/Routes/organization/api.php'));
