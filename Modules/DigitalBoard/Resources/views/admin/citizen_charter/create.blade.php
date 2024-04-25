31437c93565b253935416d6486bbca766809828b
Anjali12234 <anjisharma649@gmail.com>
2024-04-25 12:34:24 +0545

    commit

--- Modules/DigitalBoard/Resources/views/admin/citizen_charter/create.blade.php
+++ Modules/DigitalBoard/Resources/views/admin/citizen_charter/create.blade.php
@@ -111,12 +111,12 @@
                                 @enderror
                             </div>
                             <div class="col-md-6 mb-2">
-                                @if (auth()->user()->role->type == 'Super')
+                                @if (auth()?->user()?->role?->type == 'Super')
                                 <label for="ward" class="form-label">वडा</label>
                                 <select name="ward[]" id="ward" class="form-select"
                                         @if(!empty(auth()->user()->ward_no)) disabled @endif multiple>
                                     <option value="">---वडा छान्नुहोस्---</option>
-                                    @foreach(officeSetting()->localbody->ward_no as $ward)
+                                    @foreach(officeSetting()?->localbody?->ward_no as $ward)
                                         <option value="{{$ward}}" {{in_array($ward, old('ward',!empty(auth()->user()->ward_no) ? [auth()->user()->ward_no]:[])) ? 'selected' : ''}}>{{$ward}}</option>
                                     @endforeach
                                 </select>
@@ -136,7 +136,7 @@
                                     value="1"
                                     class="form-check-input @error('is_displayed') is-invalid @enderror"
                                     id="is_displayed"
-                                    @if(!empty(auth()->user()->ward_no))disabled @else checked @endif
+                                    @if(!empty(auth()?->user()?->ward_no))disabled @else checked @endif
                                 />
                                 <label for="is_displayed" class="form-label">पालिकामा पनि देखाउनु होस्</label>
 
