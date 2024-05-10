500103c79c1167fd0972e4afa079855f95c42515
Anjali12234 <anjisharma649@gmail.com>
2024-05-10 16:19:29 +0545

    comit

--- Modules/BusinessRegistration/Transformers/OrganizationReport/OrganizationRegistrationResource.php
+++ Modules/BusinessRegistration/Transformers/OrganizationReport/OrganizationRegistrationResource.php
@@ -1,20 +1,23 @@
 <?php
 
 
+namespace Modules\BusinessRegistration\Transformers\OrganizationReport;
+
 use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\BusinessRegistration\Transformers\OrganizationReport\OrganizationRenewResource;
+use Modules\BusinessRegistration\Transformers\Report\PartnerResource;
 
 class OrganizationRegistrationResource extends JsonResource
 {
     public function toArray($request): array
     {
-        $request_columns = $request->input('columns')['business_details'] ?? [];
+        $request_columns = $request->input('columns')['organization_registrations'] ?? [];
         return [
             'सबमिशन नम्बर' => $this->when(in_array('submission_no', $request_columns), $this->submission_no ?? ''),
             'व्यवसाय ठेगाना ' => $this->when((bool)array_intersect(['province_id', 'district_id', 'local_body_id', 'ward_no', 'tole','way'], $request_columns), function () use ($request_columns) {
                 return $this->resolveAddress($request_columns);
             }),
             'आर्थिक बर्ष' => $this->when(in_array('fiscal_year_id', $request_columns), $this->fiscalYear->title ?? ''),
-            'व्यवसायको प्रकृति ' => $this->when(in_array('business_nature_id', $request_columns), $this->businessNature->title ?? ''),
             'बिल नं.' => $this->when(in_array('bill_no', $request_columns), $this->bill_no ?? ''),
             'बिल मिति बि स.' => $this->when(in_array('bill_date_bs', $request_columns), $this->bill_date_bs ?? ''),
             'बिल मिति ई स.' => $this->when(in_array('bill_date_ad', $request_columns), $this->bill_date_ad ?? ''),
@@ -27,29 +30,18 @@ class OrganizationRegistrationResource extends JsonResource
             'व्यवसायको नाम(अंग्रेजीमा)' => $this->when(in_array('name_en', $request_columns), $this->name_en ?? ''),
             'ठेगाना' => $this->when(in_array('address', $request_columns), $this->address ?? ''),
             'ठेगाना(अंग्रेजीमा)' => $this->when(in_array('address_en', $request_columns), $this->address_en ?? ''),
+            'सम्पर्क नं.' => $this->when(in_array('phone', $request_columns), $this->phone ?? ''),
+            'ईमेल' => $this->when(in_array('email', $request_columns), $this->email ?? ''),
             'उद्देश्य' => $this->when(in_array('purpose', $request_columns), $this->purpose ?? ''),
-            'व्यवसायको कारोबार गर्ने मुख्य सेवा वा बस्तु' => $this->when(in_array('object_transaction_id', $request_columns), $this->objectTransaction->title ?? ''),
-            'चालु पूँजी' => $this->when(in_array('working_capital', $request_columns), $this->working_capital ?? ''),
-            'स्थिर पूँजी' => $this->when(in_array('fixed_capital', $request_columns), $this->fixed_capital ?? ''),
-            'पूँजीगत लगानी' => $this->when(in_array('investment', $request_columns), $this->investment ?? ''),
-            'घर मालिकको नाम' => $this->when(in_array('house_owner_name', $request_columns), $this->house_owner_name ?? ''),
-            'घर मालिकको फोन' => $this->when(in_array('house_owner_phone', $request_columns), $this->house_owner_phone ?? ''),
-            'घर मालिकको ठेगाना' => $this->when(in_array('house_owner_address', $request_columns), $this->house_owner_address ?? ''),
-            'घर मालिक मासिक भाडा' => $this->when(in_array('house_owner_monthly_rent', $request_columns), $this->house_owner_monthly_rent ?? ''),
-            'लम्बाई' => $this->when(in_array('length', $request_columns), $this->length ?? ''),
-            'चौडाई' => $this->when(in_array('width', $request_columns), $this->width ?? ''),
+            'आर्थिक स्रोत' => $this->when(in_array('financial_source', $request_columns), $this->financial_source ?? ''),
             'आवेदन मिति बि. सं.' => $this->when(in_array('application_date', $request_columns), $this->application_date ?? ''),
             'आवेदन मिति ई. सं.' => $this->when(in_array('application_date_en', $request_columns), $this->application_date_en ?? ''),
-            'भाडा सम्झौता' => $this->when(in_array('rent_agreement', $request_columns), $this->rent_agreement ?? ''),
-            'आफ्नै घर जग्गा भए जग्गा धनि प्रमाणपत्र' => $this->when(in_array('land_ownership_certificate', $request_columns), $this->land_ownership_certificate ?? ''),
             'वार्ड सिफारिस' => $this->when(in_array('ward_recommendation', $request_columns), $this->ward_recommendation ?? ''),
-            'राजदूतावासको कागजात' => $this->when(in_array('embassy_document', $request_columns), $this->embassy_document ?? ''),
             'दर्ता प्रमाणपत्र' => $this->when(in_array('registration_document', $request_columns), $this->registration_document ?? ''),
-            'इजाजत पत्र' => $this->when(in_array('license', $request_columns), $this->license ?? ''),
-            'कर तिरेको प्रमाणपत्र' => $this->when(in_array('tax_document', $request_columns), $this->tax_document ?? ''),
+            'कर तिरेको प्रमाणपत्र नं.' => $this->when(in_array('taxpayer_number', $request_columns), $this->taxpayer_number ?? ''),
             'व्यवसायी विवरण' => PartnerResource::collection($this->whenLoaded('partners')),
-            'अघि दर्ता भएका व्यवसाय' => RegisterBusinessResource::collection($this->whenLoaded('registeredBusinesses')),
-            'व्यवसाय नवीकरण' => BusinessRenewResource::collection($this->whenLoaded('businessRenew')),
+
+            'व्यवसाय नवीकरण' => OrganizationRenewResource::collection($this->whenLoaded('organizationRenew')),
         ];
     }
 
