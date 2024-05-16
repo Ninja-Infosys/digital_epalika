621acccba93b7f1c378311976262b1dd8954bf01
Bin1ta <beenitarzmii456@gmail.com>
2024-05-16 14:18:45 +0545

    forum

--- Modules/BusinessRegistration/Http/Livewire/RegistrationForm.php
+++ Modules/BusinessRegistration/Http/Livewire/RegistrationForm.php
@@ -10,6 +10,7 @@ use Livewire\Component;
 use Livewire\WithFileUploads;
 use Modules\BusinessRegistration\Entities\BusinessDetail;
 use Modules\BusinessRegistration\Entities\BusinessNature;
+use Modules\BusinessRegistration\Entities\CommitteeName;
 use Modules\BusinessRegistration\Entities\ObjectTransaction;
 use Modules\BusinessRegistration\Entities\Partner;
 use Modules\BusinessRegistration\Entities\RegisteredBusiness;
@@ -327,16 +328,8 @@ class RegistrationForm extends Component
     private function saveBusinessDetailsData($businessDetail)
     {
         foreach ($this->form['partners'] as $partner) {
-            Partner::updateOrCreate(
-                ['business_detail_id' => $businessDetail->id, 'id' => $partner['id'] ?? null],
-                $partner
-            );
-        }
-        foreach ($this->form['registeredBusinesses'] as $registeredBusiness) {
-            RegisteredBusiness::updateOrCreate(
-                ['business_detail_id' => $businessDetail->id, 'id' => $registeredBusiness['id'] ?? null],
-                $registeredBusiness
-            );
+            $partner = new Partner($partner);
+            $businessDetail->partners()->save($partner);
         }
         foreach ($this->form['other_document'] ?? [] as $document) {
             $businessDetail->files()->create([
