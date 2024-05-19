3bea3a6d59a657254dbe08789fc760b7d34520b2
Anjali12234 <anjisharma649@gmail.com>
2024-05-19 15:01:45 +0545

    commit

--- Modules/EMap/Resources/views/admin/step/formList.blade.php
+++ Modules/EMap/Resources/views/admin/step/formList.blade.php
@@ -118,29 +118,15 @@
                             </a>
                         </li>
 
-                        {{-- <li class="nav-item">
-                            <a href="#tab-checker" data-bs-toggle="tab" aria-expanded="false" class="nav-link">
-                                जाँच गर्नुपर्ने (
-                                {{
-                                    $forms->filter(function ($form) use ($mapGroups) {
-                                        return $form->map_group_user_id == optional($mapGroups)->id;
-                                    })->count()
-                                }})
-                            </a>
-                        </li> --}}
                         <li class="nav-item">
                             <a href="#tab-checker" data-bs-toggle="tab" aria-expanded="false" class="nav-link">
                                 जाँच गर्नुपर्ने (
                                 {{
-                                    $forms->filter(function ($form) use ($mapGroups) {
-                                        return $form->map_group_user_id == optional($mapGroups)->id &&
-                                               optional($form->mapUserGroup?->user?->first())->user_id == auth()->user()->id;
-                                    })->count()
+                                    $forms->whereNotNull('map_group_user_id')->where('map_group_user_id',auth()->user()->id)?->count()
+
                                 }})
                             </a>
                         </li>
-                        
-                        
 
                         <li class="nav-item">
                             <a href="#tab-approval" data-bs-toggle="tab" aria-expanded="false" class="nav-link">
