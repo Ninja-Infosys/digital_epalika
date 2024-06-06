a85edb64eee1b3db563566ed1fd488bf29320ad3
Bin1ta <binita@ninjainfosys.com>
2024-06-06 17:51:18 +0545

    building documentation

--- Modules/EMap/Resources/views/admin/buildingDocumentation/template/recommendation.blade.php
+++ Modules/EMap/Resources/views/admin/buildingDocumentation/template/recommendation.blade.php
@@ -1,91 +1,107 @@
-<!doctype html>
-<html lang="en">
+@extends('admin.layouts.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="stylesheet" href="{{ asset('assets/backend/css/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/backend/css/icons.min.css') }}">
-    <title>{{ $buildingDocumentation->name }}को फर्म दर्ता आवेदन</title>
+@section('content')
+    <div class="row m-3">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item">
+                            <a href="{{ route('admin.dashboard') }}">
+                                <img class="icon me-1" src="{{ asset('assets/backend/images/home.svg') }}" alt="document-icon">
+                                गृहपृष्ठ
+                            </a>
+                        </li>
 
-</head>
+                        <li class="breadcrumb-item active">अभिलेखीकरण</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">सिफारिस पत्रको ढाँचा</h4>
+            </div>
+        </div>
+    </div>
 
-<body class="container bg-white">
-    <section class="row justify-content-center my-4 ">
-        <div class="card col-md-8 border">
-            <div class="card-body">
-                <p class="text-danger">नोट: आवेदन अनिवार्य प्रिन्ट गरि कार्यालयमा हाजिर हुनुहोला</p>
-                <x-print-button target-element="printData" title="{{ $buildingDocumentation->name }}" />
-                <div Id="printData">
-                    <div class="text-center fw-bolder">
-                        <div><span style="font-size:14px"><strong>अनुसूची ४</strong></span><br />
-                            <span style="font-size:14px"><strong>निर्देशिकाको दफा ५ संग सम्वन्धिता</strong></span><br />
-                            <span style="font-size:14px"><strong>सिफारिस पत्रको ढाँचा</strong></span>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card p-0">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h4 class="header-title">सिफारिस पत्रको ढाँचा </h4>
+                        <div class="d-flex justify-content-between">
+                            <x-print-button title="७ दिने सूचना" target-element="printData" />
+
+                            <a href="{{ route('emap.admin.buildingDocumentation.index') }}"
+                               class="btn btn-sm btn-outline-primary">
+                                <i class="fa fa-list"> दर्ता भएका अभिलेखीकरण</i>
+                            </a>
                         </div>
                     </div>
+                </div>
+                <div class="card-body px-0">
+                    <div Id="printData">
+                        <div class="text-center fw-bolder">
+                            <div><span style="font-size:14px"><strong>अनुसूची ४</strong></span><br />
+                                <span style="font-size:14px"><strong>निर्देशिकाको दफा ५ संग सम्वन्धिता</strong></span><br />
+                                <span style="font-size:14px"><strong>सिफारिस पत्रको ढाँचा</strong></span>
+                            </div>
+                        </div>
 
-                    <div class="subject text-justify-center lh-lg px-5 ">
-                        <h5><strong> {{ $officeSetting->localBody->local_body ?? '' }}</strong></h5>
-                        <h5><strong>........ वडा कार्यालय</strong></h5>
+                        <div class="subject text-justify-center lh-lg px-5 ">
+                            <h5><strong> {{ $officeSetting->localBody->local_body ?? '' }}</strong></h5>
+                            <h5><strong>........ वडा कार्यालय</strong></h5>
 
-                    </div>
-                    <div class="d-flex text-justify-center lh-lg px-5">
-                        <p>प.स.:</p>
-                        <p style="margin-left: 500px;">मिति :....................</p>
-                    </div>
-                    <p class="text-justify-center lh-lg px-5"> चालनी नं. :</p>
-                    <div class="text-justify-center lh-lg px-5">
-                        <p>श्री {{ $officeSetting->localBody->local_body ?? '' }}को कार्यालय</p>
-                        <p>{{$officeSetting->site_address}} ।</p>
-                    </div>
-                    <p class="text-justify-center text-center lh-lg px-5"> <strong>बिषय : घर जग्गा अभिलेखिकरणको सिफारिस
-                            पठाइएको बारे ।</strong></p>
-                    <p class="text-justify-center  lh-lg px-5">
-                        प्रस्तुत बिषयमा {{ $officeSetting->localBody->local_body ?? '' }} वडा नं.
-                        {{get_nepali_number($buildingDocumentation->land_ward_no)}} साविक जिल्ला {{ $buildingDocumentation->former_district }}
-                        {{ $buildingDocumentation->former_local_body }} वडा नं
-                        {{ get_nepali_number($buildingDocumentation->former_ward_no) }} कित्ता नं. {{ get_nepali_number($buildingDocumentation->plot_no) }} मा {{ get_nepali_number($buildingDocumentation->land_area) }} क्षेत्रफलमा घर
-                        निर्माण गरेको घरधनि श्री. {{ $buildingDocumentation->house_owner_name }} ले यस कार्यालयमा घर अभिलेखिकरणका लागी सिफारिस गरिपाउँ भनि दिएको निवेदन माथि जाँचबुझ
-                        गर्दा निजले पेश गरेको घरको अभिलेखिकरण गर्न तोकिएको मापदण्ड हरु सबै पुरा भएको देखिएकाले घर
-                        अभिलेखिकरण गरिदिनुहुन सिफारिस साथ अनुरोध छ
-                    </p>
-                    <p class="text-justify-center  lh-lg px-5">
-                        ...................<br>
-                        वडा अध्यक्ष
-                    </p>
-                    <p class="text-justify-center lh-lg px-5"> <strong>(नगरपालिकामा सिफारिस गर्दा तपसिल बमोजिमका कागजात
-                            संलग्न हुनपर्नेछ)</strong></p>
+                        </div>
+                        <div class="d-flex text-justify-center lh-lg px-5">
+                            <p>प.स.:</p>
+                            <p style="margin-left: 500px;">मिति :....................</p>
+                        </div>
+                        <p class="text-justify-center lh-lg px-5"> चालनी नं. :</p>
+                        <div class="text-justify-center lh-lg px-5">
+                            <p>श्री {{ $officeSetting->localBody->local_body ?? '' }}को कार्यालय</p>
+                            <p>{{$officeSetting->site_address}} ।</p>
+                        </div>
+                        <p class="text-justify-center text-center lh-lg px-5"> <strong>बिषय : घर जग्गा अभिलेखिकरणको सिफारिस
+                                पठाइएको बारे ।</strong></p>
+                        <p class="text-justify-center  lh-lg px-5">
+                            प्रस्तुत बिषयमा {{ $officeSetting->localBody->local_body ?? '' }} वडा नं.
+                            {{get_nepali_number($buildingDocumentation->land_ward_no)}} साविक जिल्ला {{ $buildingDocumentation->former_district }}
+                            {{ $buildingDocumentation->former_local_body }} वडा नं
+                            {{ get_nepali_number($buildingDocumentation->former_ward_no) }} कित्ता नं. {{ get_nepali_number($buildingDocumentation->plot_no) }} मा {{ get_nepali_number($buildingDocumentation->land_area) }} क्षेत्रफलमा घर
+                            निर्माण गरेको घरधनि श्री. {{ $buildingDocumentation->house_owner_name }} ले यस कार्यालयमा घर अभिलेखिकरणका लागी सिफारिस गरिपाउँ भनि दिएको निवेदन माथि जाँचबुझ
+                            गर्दा निजले पेश गरेको घरको अभिलेखिकरण गर्न तोकिएको मापदण्ड हरु सबै पुरा भएको देखिएकाले घर
+                            अभिलेखिकरण गरिदिनुहुन सिफारिस साथ अनुरोध छ
+                        </p>
+                        <p class="text-justify-center  lh-lg px-5">
+                            ...................<br>
+                            वडा अध्यक्ष
+                        </p>
+                        <p class="text-justify-center lh-lg px-5"> <strong>(नगरपालिकामा सिफारिस गर्दा तपसिल बमोजिमका कागजात
+                                संलग्न हुनपर्नेछ)</strong></p>
 
-                    <div class="subject text-justify-center lh-lg px-5">
-                        <ol>
-                            <li>
-                                सम्बन्धीत वडा कार्यालयको सिफारिस पत्र (१ प्रति)
-                            </li>
-                            <li>
-                                नगरपालिकामा सूचिकृत भएको कन्सल्टेन्सीबाट तयार भई सहिछाप भएको घरको नक्सा (२ प्रति)
-                            </li>
+                        <div class="subject text-justify-center lh-lg px-5">
+                            <ol>
+                                <li>
+                                    सम्बन्धीत वडा कार्यालयको सिफारिस पत्र (१ प्रति)
+                                </li>
+                                <li>
+                                    नगरपालिकामा सूचिकृत भएको कन्सल्टेन्सीबाट तयार भई सहिछाप भएको घरको नक्सा (२ प्रति)
+                                </li>
 
-                            <li>नेपाली नागरिकताको प्रमाण पत्रको प्रतिलिपी (१ प्रति)</li>
+                                <li>नेपाली नागरिकताको प्रमाण पत्रको प्रतिलिपी (१ प्रति)</li>
 
-                            <li>चालु आ.व को मालपोत कर तिरेको प्रमाण (१ प्रति)</li>
-                            <li>पासपोर्ड साइजको फोटो (४ प्रति)</li>
+                                <li>चालु आ.व को मालपोत कर तिरेको प्रमाण (१ प्रति)</li>
+                                <li>पासपोर्ड साइजको फोटो (४ प्रति)</li>
 
-                            <li>जग्गा धनी दर्ता प्रमाण पूर्जाको प्रतिलिपी (१ प्रति)</li>
-                            <li>घर बनेको जग्गाको ब्लु प्रिन्ट, फाईल वा ट्रेस नक्साको सक्कल प्रतिलिपी (१ प्रति)</li>
-                            <li>चार किल्ला प्रमाणित सिफारिसको प्रतिलिपी (१ प्रति)</li>
-                            <li>निर्मित घर टहरा तथा पक्की भवनको चौतर्फी फोटो (१/१ प्रति)</li>
+                                <li>जग्गा धनी दर्ता प्रमाण पूर्जाको प्रतिलिपी (१ प्रति)</li>
+                                <li>घर बनेको जग्गाको ब्लु प्रिन्ट, फाईल वा ट्रेस नक्साको सक्कल प्रतिलिपी (१ प्रति)</li>
+                                <li>चार किल्ला प्रमाणित सिफारिसको प्रतिलिपी (१ प्रति)</li>
+                                <li>निर्मित घर टहरा तथा पक्की भवनको चौतर्फी फोटो (१/१ प्रति)</li>
 
-                        </ol>
+                            </ol>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-    <script src="{{ asset('assets/backend/print/print.min.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-</body>
 
-</html>
+@endsection
