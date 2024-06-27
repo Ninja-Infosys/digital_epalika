4da028abaee412e30c1758ddbc359b1bac54ea6a
altaafali092 <altaafali092@gmail.com>
2024-06-27 15:01:41 +0545

    commit

--- Modules/DigitalBoard/Resources/views/admin/notice/edit.blade.php
+++ Modules/DigitalBoard/Resources/views/admin/notice/edit.blade.php
@@ -85,21 +85,11 @@
                                 <div class="col-md-4 mb-2">
                                     @if (auth()->user()->role->type == 'Super')
                                         <label for="ward" class="form-label">वडा</label>
-                                        {{-- <select name="ward[]" id="ward" class="form-select"
-                                        @if (!empty(auth()->user()->ward_no)) disabled @endif multiple>
-                                        <option value="">---वडा छान्नुहोस्---</option>
-                                        @foreach (officeSetting()->localbody->ward_no as $ward)
-                                            <option value="{{ $ward }}"
-                                                {{ in_array($ward, old('ward', !empty(auth()->user()->ward_no) ? [auth()->user()->ward_no] : [])) ? 'selected' : '' }}>
-                                                {{ $ward }}</option>
-                                        @endforeach
-                                    </select> --}}
-
                                         <select class="form-control @error('ward') is-invalid @enderror" name="ward[]"
                                             id="ward" {{ !empty(auth()->user()->ward_no) ? 'disabled' : '' }}
                                             multiple>
                                             <option value=""> वडा छान्नुहोस्</option>
-                                            @foreach (officeSetting()?->localbody?->ward_no ??[] as $ward)
+                                            @foreach (officeSetting()?->localbody?->ward_no ?? [] as $ward)
                                                 <option value="{{ $ward }}"
                                                     {{ in_array($ward, old('ward', $notice->ward)) ? 'selected' : '' }}>
                                                     {{ $ward }}
@@ -116,16 +106,18 @@
                                     @enderror
                                 </div>
 
-                                <div class="col-md-4 mb-2">
-                                    <input type="checkbox" name="is_displayed" value="1"
-                                        class="form-check-input @error('is_displayed') is-invalid @enderror"
-                                        id="is_displayed" @if (!empty(auth()->user()->ward_no)) disabled @else checked @endif />
-                                    <label for="is_displayed" class="form-label">पालिकामा पनि देखाउनु होस्</label>
+                                    <div class="col-md-4 mb-2">
+                                        <input type="checkbox" name="is_displayed" value="1"
+                                            class="form-check-input @error('is_displayed') is-invalid @enderror"
+                                            id="is_displayed"
+                                            @if (!empty(auth()->user()->ward_no)) disabled @else checked @endif />
+                                        <label for="is_displayed" class="form-label">पालिकामा पनि देखाउनु होस्</label>
 
-                                    @error('is_displayed')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
+                                        @error('is_displayed')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+
 
                             </div>
                         </fieldset>
