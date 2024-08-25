3b85787c0957939eefc4c39c8d51e824a8ec9d29
altaafali092 <altaafali092@gmail.com>
2024-08-25 14:24:21 +0545

    comit

--- Modules/Revenue/Http/Controllers/ReportController.php
+++ Modules/Revenue/Http/Controllers/ReportController.php
@@ -100,34 +100,7 @@ class ReportController extends Controller
         return view('revenue::admin.report.invoice', compact('fiscalYears'));
     }
 
-    // public function invoiceReport(Request $request)
-    // {
-    //     $request->validate([
-    //         'from_date' => ['nullable'],
-    //         'to_date' => ['nullable'],
-    //         'payment_method' => ['nullable'],
-    //         'fiscal_year' => ['nullable', 'array'],
-    //         'fiscal_year.*' => [Rule::exists('fiscal_years', 'id')],
-    //     ]);
 
-    //     $invoices = Invoice::where(function ($query) use ($request) {
-    //         $this->filterDataFromUser($query, $request);
-    //     })->get();
-
-    //     $wardData = [];
-    //     foreach (officeSetting()->localBody->ward_no as $ward_no) {
-    //         $wardData[] = $invoices->where('ward', $ward_no)->count();
-    //     }
-
-    //     // Calculate the count of invoices where ward is null
-    //     $palikaCount = $invoices->whereNull('ward')->count();
-
-    //     return response()->json([
-    //         'fiscal_years' => !empty($request->input('fiscal_year')) ? FiscalYear::select('title')->whereIn('id', Arr::wrap($request->input('fiscal_year')))->pluck('title') : FiscalYear::pluck('title'),
-    //         'wardsData' => $wardData,
-    //         'palikaCount' => $palikaCount,
-    //     ]);
-    // }
 
     public function invoiceReport(Request $request)
 {
@@ -208,52 +181,7 @@ class ReportController extends Controller
         return \view('revenue::admin.report.ward-wise-invoice', compact('fiscalYears'));
     }
 
-    // public function wordWiseInvoiceReport(Request $request)
-    // {
-    //     $request->validate([
-    //         'from_date' => ['nullable'],
-    //         'to_date' => ['nullable'],
-    //         'tax_payer_type' => ['nullable', 'array'],
-    //         'tax_payer_type.*' => [Rule::exists('tax_payer_types', 'id')],
-    //         'fiscal_year' => ['nullable', 'array'],
-    //         'fiscal_year.*' => [Rule::exists('fiscal_years', 'id')],
-    //     ]);
 
-    //     $invoices = Invoice::with('InvoiceParticulars')->where(function ($query) use ($request) {
-    //         $this->filterDataFromUser($query, $request);
-    //     })->get()->map(function ($invoice) {
-    //         return [
-    //             'ward' => $invoice->ward,
-    //             'invoice' => $invoice->is_cash_invoice,
-    //             'grand_total_amount' => $invoice->InvoiceParticulars->sum('grand_total_amount')
-    //         ];
-    //     });
-
-    //     $data = collect();
-
-    //     // Handle wards with actual ward numbers
-    //     foreach (officeSetting()->localBody->ward_no as $ward_no) {
-    //         $data->push([
-    //             'ward' => 'वडा नं ' . $ward_no,
-    //             'land_invoice' => $invoices->where('ward', $ward_no)->where('invoice', 0)->sum('grand_total_amount'),
-    //             'invoice' => $invoices->where('ward', $ward_no)->where('invoice', 1)->sum('grand_total_amount'),
-    //             'total' => $invoices->where('ward', $ward_no)->where('invoice', 0)->sum('grand_total_amount') + $invoices->where('ward', $ward_no)->where('invoice', 1)->sum('grand_total_amount'),
-    //         ]);
-    //     }
-
-    //     // Handle invoices where the ward is null (पालिका)
-    //     $data->push([
-    //         'ward' => 'पालिका',
-    //         'land_invoice' => $invoices->whereNull('ward')->where('invoice', 0)->sum('grand_total_amount'),
-    //         'invoice' => $invoices->whereNull('ward')->where('invoice', 1)->sum('grand_total_amount'),
-    //         'total' => $invoices->whereNull('ward')->where('invoice', 0)->sum('grand_total_amount') + $invoices->whereNull('ward')->where('invoice', 1)->sum('grand_total_amount'),
-    //     ]);
-
-    //     return response()->json([
-    //         'data' => $data,
-    //         'view' => (string)View::make('revenue::admin.report.inc.ward-wise-invoice', compact('data'))
-    //     ]);
-    // }
 
     public function wordWiseInvoiceReport(Request $request)
     {
