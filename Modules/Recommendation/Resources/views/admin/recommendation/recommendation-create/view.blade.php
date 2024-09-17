c8a5e3a7d656e36ca0868e847695cff0872d1431
Shishir Rai <91421157+shishirrai98@users.noreply.github.com>
2024-09-17 08:25:06 +0545

    recommendation show

--- Modules/Recommendation/Resources/views/admin/recommendation/recommendation-create/view.blade.php
+++ Modules/Recommendation/Resources/views/admin/recommendation/recommendation-create/view.blade.php
@@ -84,6 +84,7 @@
                                             <td>
                                                 <div>
                                                     @if($recommendationValue->type == 'image')
+                                                    <img src="{{ $recommendationValue->value ?? '' }}" alt="">
                                                     @elseif($recommendationValue->type == 'table')
                                                     @else
                                                         {{ $recommendationValue->value ?? '' }}
