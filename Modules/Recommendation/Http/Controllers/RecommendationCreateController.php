e196342c2cdb27f796d333428cdfb45e9d1d52d7
altaafali092 <altaafali092@gmail.com>
2024-10-07 11:16:12 +0545

    commit

--- Modules/Recommendation/Http/Controllers/RecommendationCreateController.php
+++ Modules/Recommendation/Http/Controllers/RecommendationCreateController.php
@@ -81,6 +81,7 @@ class RecommendationCreateController extends Controller
 
         $recommendationCreates = $recommendation->paginate(10);
 
+
         return view('recommendation::admin.recommendation.recommendation-create.index', compact('recommendationCreates'));
     }
 
