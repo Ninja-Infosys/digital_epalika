53569a79656b6804dddf70e5d86e33fabd6f7739
altaafali092 <altaafali092@gmail.com>
2024-10-29 14:09:14 +0545

    commmit

--- Modules/Recommendation/Http/Requests/RecommendationCreate/StoreRecommendationCreateRequest.php
+++ Modules/Recommendation/Http/Requests/RecommendationCreate/StoreRecommendationCreateRequest.php
@@ -12,7 +12,7 @@ class StoreRecommendationCreateRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return Gate::allows(' recommendation_access');
+        return Gate::allows('recommendation_access');
 
     }
 
